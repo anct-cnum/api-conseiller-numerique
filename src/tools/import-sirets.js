c507b25a816c0e73fc28a22cb593cdee720ae252
Jean-Yves Stervinou <jy@stervinou.com>
2021-01-08 23:43:25 +0100

    Mise à jour du siret si on trouve un mail qui correspond

--- src/tools/import-sirets.js
+++ src/tools/import-sirets.js
@@ -43,10 +43,19 @@ const readCSV = async (filePath) => {
   }
 };
 
-const updateDB = async (email, entreprise) => {
-  //const { rows } = await pool.query('SELECT * FROM djapp_hostorganization WHERE contact_email = $1', [email])
-  const { rows } = await pool.query('SELECT NOW() AS DATE');
-  //console.dir(rows);
+const updateDB = async (email, entreprise, conseillers) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM djapp_hostorganization WHERE LOWER(contact_email) LIKE LOWER('%' || $1 || '%')", [email])
+    if (rows.length > 0) {
+      console.log(`Email trouvé : ${rows[0].contact_email} ${rows[0].id}`);
+      //const { result } = await pool.query("UPDATE djapp_hostorganization SET siret=$1, coach_desired=$2 WHERE id=$3", [entreprise.siret_siege_social, conseillers, rows[0].id])
+      const { result } = await pool.query("UPDATE djapp_hostorganization SET siret=$1 WHERE id=$2", [entreprise.siret_siege_social, rows[0].id])
+    } else {
+      console.log(`Email inconnu : ${email}`);
+    }
+  } catch (error) {
+    console.log(`Erreur DB : ${error.message} pour l'adresse ${email}`);
+  }
 }
 
 readCSV(program.csv).then(async (replies) => {
@@ -54,6 +63,7 @@ readCSV(program.csv).then(async (replies) => {
     const siret = reply['Quel est votre numéro SIRET ?'].replace(/\s/g,'');
     const id = reply['ID de la réponse'];
     const email = reply['Pouvez-vous nous rappeler l’adresse mail avec laquelle vous avez reçu ce message ?'];
+    const conseillers = reply['Combien de conseillers numériques souhaitez-vous accueillir ?'];
 
     if (/\d{14}/.test(siret)) {
       const siren = siret.substring(0,9);
@@ -63,7 +73,7 @@ readCSV(program.csv).then(async (replies) => {
         // result.siret_siege_social
         // result.raison_sociale
         console.log(`OK ${id} ${siret} ${result.raison_sociale}`);
-        await updateDB(email,result);
+        await updateDB(email, result, conseillers);
       } catch (error) {
         console.log(error);
       }
