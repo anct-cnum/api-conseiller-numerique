6fa8fe6b6543036e30915ad6335630afee1d872e
Loic GUILLOIS <contact@loicguillois.fr>
2021-01-27 22:18:08 +0100

    configuration par variables d'environnement pour la production

--- src/app.js
+++ src/app.js
@@ -19,9 +19,9 @@ const mongodb = require('./mongodb');
 
 const app = express(feathers());
 
-// Load app configuration
+
 app.configure(configuration());
-// Enable security, CORS, compression and body parsing
+
 app.use(helmet({
   contentSecurityPolicy: false
 }));
@@ -36,22 +36,15 @@ app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Set up Plugins and providers
 app.configure(express.rest());
-
 
 app.configure(mongodb);
 
-
-// Configure other middleware (see `middleware/index.js`)
 app.configure(middleware);
 app.configure(authentication);
-// Set up our services (see `services/index.js`)
 app.configure(services);
-// Set up event channels (see channels.js)
 app.configure(channels);
 
-// Configure a middleware for 404s and the error handler
 app.use(express.notFound());
 app.use(express.errorHandler({ logger }));
 
