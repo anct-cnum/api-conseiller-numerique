982e0c536efe890f8bd459d8c3f719f842baaab2
Loic GUILLOIS <contact@loicguillois.fr>
2021-02-02 16:07:47 +0100

    API de récupération des mises en relations pour une structure donnée

--- src/services/index.js
+++ src/services/index.js
@@ -2,12 +2,12 @@ const users = require('./users/users.service.js');
 const siret = require('./siret/siret.service.js');
 const conseillers = require('./conseillers/conseillers.service.js');
 const structures = require('./structures/structures.service.js');
-const correspondances = require('./correspondances/correspondances.service.js');
+const misesEnRelation = require('./mises-en-relation/misesEnRelation.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function(app) {
   app.configure(users);
   app.configure(siret);
   app.configure(conseillers);
   app.configure(structures);
-  app.configure(correspondances);
+  app.configure(misesEnRelation);
 };
--- src/services/structures/structures.class.js
+++ src/services/structures/structures.class.js
@@ -1,11 +1,35 @@
+const { ObjectID } = require('mongodb');
+
 const { Service } = require('feathers-mongodb');
 
 exports.Structures = class Structures extends Service {
   constructor(options, app) {
     super(options);
-    
-    app.get('mongoClient').then(db => {
+
+    let db;
+
+    app.get('mongoClient').then(mongoDB => {
+      db = mongoDB;
       this.Model = db.collection('structures');
+    });
+
+    app.get('/structures/:id/misesEnRelation', async (req, res) => {
+      const misesEnRelationService = app.service('misesEnRelation');
+      const conseillersService = app.service('conseillers');
+      const conseillers = await misesEnRelationService.find();
+
+      const findConseiller = async miseEnRelation => {
+        return conseillersService.find({ query: { _id: new ObjectID(miseEnRelation.conseiller.oid) } });
+      };
+
+      const getData = async () => {
+        return Promise.all(conseillers.data.map(miseEnRelation => findConseiller(miseEnRelation)));
+      };
+
+      getData().then(data => {
+        conseillers.data = data;
+        res.send(conseillers);
+      });
     });
   }
 };
