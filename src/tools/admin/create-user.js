b61adb59da7ee10e59c54c40be021fdf0cd70d20
Loic GUILLOIS <contact@loicguillois.fr>
2021-02-02 17:13:13 +0100

    association compte utilisateur <> entitée (structure / conseiller) (#22)

--- src/tools/admin/create-user.js
+++ src/tools/admin/create-user.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 
+const ObjectID = require('mongodb').ObjectID;
 const { program } = require('commander');
 
 require('dotenv').config();
@@ -9,15 +10,17 @@ const { execute } = require('../utils');
 
 execute(async ({ feathers, db, logger, exit }) => {
 
-  program.option('-n, --username <username>', 'username');
+  program.option('-u, --username <username>', 'username');
   program.option('-p, --password <password>', 'password');
   program.option('-r, --role <role>', 'role : choisir entre admin, structure, conseiller');
+  program.option('-i, --id <id>', 'id');
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
 
   const username = program.username;
   const password = program.password;
   const role = program.role;
+  const id = program.id;
 
   if (!username || !password || !role) {
     exit('Paramètres invalides');
@@ -27,14 +30,29 @@ execute(async ({ feathers, db, logger, exit }) => {
     exit('Rôle non reconnu');
   }
 
+  if (role === 'admin') {
+    if (id) {
+      exit('Paramètre id interdit pour le rôle admin');
+    }
+  } else if (!id) {
+    exit('Paramètre id obligatoire pour ce rôle');
+  }
+
   const count = await db.collection('users').countDocuments({ name: username});
   if (count > 0) {
     exit('Un utilisateur avec nom existe déjà');
   }
+  const dbName = db.serverConfig.s.options.dbName;
   await feathers.service('users').create({
     name: username,
     password: password,
-    roles: Array(role)
+    roles: Array(role),
+    entity: {
+      '$ref': `${role}s`,
+      '$id': new ObjectID('5126bc054aed4daf9e2ab772'),
+      '$db': dbName
+    },
+    createdAt: new Date()
   });
   logger.info('Utilisateur créé');
 });
