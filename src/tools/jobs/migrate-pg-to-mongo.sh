04b562899e7ab85523747345387cd0b06b0c9c84
Jean-Yves Stervinou <jy@stervinou.com>
2021-03-25 17:10:14 +0100

    CRON pour la synchro des données et les mises en relation

--- src/tools/jobs/migrate-pg-to-mongo.sh
+++ src/tools/jobs/migrate-pg-to-mongo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -l
 
 cd ${APP_HOME}
-node src/tools/scripts/migrate-pg-to-mongo.js
+
+echo "Migration Postgresql vers MongoDB : START"
+node src/tools/scripts/migrate-pg-to-mongo.js -l 100000
+echo "Migration Postgresql vers MongoDB : END"
+echo "Création des mises en relation : START"
+node src/tools/admin/mise-en-relation.js
+echo "Création des mises en relation : END"
 
