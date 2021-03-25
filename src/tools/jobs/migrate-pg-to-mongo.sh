cb097c266dd7ad65a27f89cd2fb326bed81bc1b6
Jean-Yves Stervinou <jy@stervinou.com>
2021-03-25 17:12:46 +0100

    Retour à la ligne

--- src/tools/jobs/migrate-pg-to-mongo.sh
+++ src/tools/jobs/migrate-pg-to-mongo.sh
@@ -2,10 +2,10 @@
 
 cd ${APP_HOME}
 
-echo "Migration Postgresql vers MongoDB : START"
+echo "Migration Postgresql vers MongoDB : START\n"
 node src/tools/scripts/migrate-pg-to-mongo.js -l 100000
-echo "Migration Postgresql vers MongoDB : END"
-echo "Création des mises en relation : START"
+echo "Migration Postgresql vers MongoDB : END\n"
+echo "Création des mises en relation : START\n"
 node src/tools/admin/mise-en-relation.js
-echo "Création des mises en relation : END"
+echo "Création des mises en relation : END\n"
 
