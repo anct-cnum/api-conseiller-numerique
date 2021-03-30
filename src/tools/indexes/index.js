0f2967de9388d8a6fd654ea063bf30e7677b78ed
Loic GUILLOIS <contact@loicguillois.fr>
2021-03-30 18:48:21 +0200

    Ajout des principaux index MongoDB

---
+++ src/tools/indexes/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+'use strict';
+
+const cli = require('commander');
+const { execute } = require('../utils');
+const dropIndexes = require('./tasks/dropIndexes');
+const createIndexes = require('./tasks/createIndexes');
+const findUnusedIndexes = require('./tasks/findUnusedIndexes');
+
+cli.description('Manage indexes')
+.option('-f, --find', 'Find unused indexex')
+.option('-d, --drop', 'Drop all indexesx')
+.parse(process.argv);
+
+execute(async ({ db, logger }) => {
+
+  if (cli.find) {
+    return await findUnusedIndexes(db);
+  }
+
+  if (cli.drop) {
+    logger.info('Dropping indexes...');
+    await dropIndexes(db);
+  }
+
+  logger.info('Creating indexes...');
+  return createIndexes(db);
+});
