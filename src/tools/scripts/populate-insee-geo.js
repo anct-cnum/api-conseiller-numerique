f9f4b711dca86feb238e453ef4d268faa00be6a6
Jean-Yves Stervinou <jy@stervinou.com>
2021-04-07 11:58:24 +0200

    Récupère les coordonnées géographiques des structures à partir de leur adresse INSEE

---
+++ src/tools/scripts/populate-insee-geo.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+'use strict';
+const axios = require('axios');
+const { execute } = require('../utils');
+const { program } = require('commander');
+
+program.parse(process.argv);
+
+execute(async ({ db, logger, exit }) => {
+  const store = async (s, adresse) => {
+    const filter = {
+      '_id': s._id,
+    };
+
+    const updateDoc = {
+      $set: {
+        coordonneesInsee: adresse.features[0].geometry
+      }
+    };
+
+    const options = { };
+
+    const result = await db.collection('structures').updateOne(filter, updateDoc, options);
+
+    logger.info(
+      `geo,OK,${s._id},${s.idPG},${s.nom},${s.siret},` +
+      `${result.matchedCount},${result.modifiedCount}`
+    );
+  };
+
+  const getGeo = async adresse => {
+    const adressePostale = encodeURI(`${adresse.numero_voie} ${adresse.nom_voie} ${adresse.code_postal} ${adresse.localite}`);
+    const urlAPI = `https://api-adresse.data.gouv.fr/search/?q=${adressePostale}&citycode=${adresse.code_insee_localite}`;
+
+    const params = {};
+
+    try {
+      const result = await axios.get(urlAPI, { params: params });
+      return result.data;
+    } catch (e) {
+      throw new Error(`API Error : ${e}`);
+    }
+  };
+
+  const match = await db.collection('structures').find({
+    siret: { $ne: null },
+    insee: { '$exists': true }
+  });
+
+  let s;
+  while ((s = await match.next())) {
+    if (!s.insee || !s.insee.etablissement || !s.insee.etablissement.adresse) {
+      logger.info(`geo,KOINSEE,${s._id},${s.idPG},${s.nom},${s.siret},`);
+      continue;
+    }
+
+    try {
+      let adresse = await getGeo(s.insee.etablissement.adresse);
+      console.log(JSON.stringify(adresse));
+      await store(s, adresse);
+    } catch (e) {
+      logger.info(`geo,KOAPI,${s._id},${s.idPG},${s.nom},${s.siret},`);
+    }
+  }
+
+  exit();
+});
