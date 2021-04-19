170b572c4046812fb7ba5d438f2ef35a62ec1f84
Loic GUILLOIS <contact@loicguillois.fr>
2021-04-19 17:38:52 +0200

    active le paramètre de limite

--- src/tools/jobs/send-email-prefets.sh
+++ src/tools/jobs/send-email-prefets.sh
@@ -3,5 +3,5 @@
 cd ${APP_HOME}
 
 echo "Sending email to prefets: START\n"
-node src/tools/admin/prefets/account
+node src/tools/admin/prefets/account --limit 100
 echo "Sending email to prefets: END\n"
