ee7cce439ce9d8447c33e49c154ce5bc145eb8b2
Jean-Yves Stervinou <jy@stervinou.com>
2021-04-19 16:44:35 +0200

    CRON pour la synchro PG/Mongo et les mises en relation synchro chaque heure mises en relation toutes les nuits à 00:30

--- src/tools/jobs/migrate-pg-to-mongo.sh
+++ src/tools/jobs/migrate-pg-to-mongo.sh
@@ -2,17 +2,7 @@
 
 cd ${APP_HOME}
 
-DATECOURANTE=`date +"%Y-%m-%d-%H-%M"`
-
 echo "Migration Postgresql vers MongoDB : START\n"
-node src/tools/scripts/migrate-pg-to-mongo.js -l 100000 > ${APP_HOME}/batchlogs/migrate-pg-to-mongo-${DATECOURANTE}.txt
-gzip -9 ${APP_HOME}/batchlogs/migrate-pg-to-mongo-${DATECOURANTE}.txt
+node src/tools/scripts/migrate-pg-to-mongo.js -l 100000
 echo "Migration Postgresql vers MongoDB : END\n"
-
-DATECOURANTE=`date +"%Y-%m-%d-%H-%M"`
-
-echo "Création des mises en relation : START\n"
-node src/tools/admin/mise-en-relation.js > ${APP_HOME}/batchlogs/mise-en-relation-${DATECOURANTE}.txt
-gzip -9 ${APP_HOME}/batchlogs/mise-en-relation-${DATECOURANTE}.txt
-echo "Création des mises en relation : END\n"
 
