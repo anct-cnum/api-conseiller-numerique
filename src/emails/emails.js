4605ffbd3c78ff7ac76e09b88dcd0e8a71f98ba9
Loic GUILLOIS <contact@loicguillois.fr>
2021-04-19 12:55:19 +0200

    mécanisme d'invitation des utilisateurs admins par email

--- src/emails/emails.js
+++ src/emails/emails.js
@@ -1,11 +1,13 @@
 const creationCompteStructure = require('./structures/creationCompteStructure');
 const creationComptePrefet = require('./prefets/creationComptePrefet');
+const creationCompteAdmin = require('./admins/creationCompteAdmin');
 
 module.exports = (db, mailer) => {
 
   let emails = [
     creationCompteStructure(db, mailer),
-    creationComptePrefet(db, mailer)
+    creationComptePrefet(db, mailer),
+    creationCompteAdmin(db, mailer)
   ];
 
   return {
