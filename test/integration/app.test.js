58e74e06d6451848835e5032d8d90453a0ef1102
Sébastien Quirrion <sebastien@fitlab.fr>
2021-04-27 17:57:09 +0200

    initialisation service cras

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -14,12 +14,12 @@ const getUrl = pathname => url.format({
 describe('Feathers application tests', () => {
   let server;
 
-  before(function (done) {
+  before(function(done) {
     server = app.listen(port);
     server.once('listening', () => done());
   });
 
-  after(function (done) {
+  after(function(done) {
     server.close(done);
   });
 
@@ -34,7 +34,7 @@ describe('Feathers application tests', () => {
     }
   });
 
-  describe('404', function () {
+  describe('404', function() {
     it('shows a 404 HTML page', async () => {
       try {
         await axios.get(getUrl('path/to/nowhere'), {
