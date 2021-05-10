85503234053af4c769f7a65b867d0d722d0ee761
Loic GUILLOIS <contact@loicguillois.fr>
2021-05-10 15:29:27 +0200

    meilleure gestion des promesses

--- src/tools/scripts/username-lowercase.js
+++ src/tools/scripts/username-lowercase.js
@@ -8,13 +8,18 @@ const { execute } = require('../utils');
 execute(__filename, async ({ db, logger, exit }) => {
   logger.info('Passes les noms d\'utilisateur en minuscule...');
   let count = 0;
+  let promises = [];
   await db.collection('users').find({}, { 'name': 1 }).forEach(function(doc) {
-    count++;
-    db.collection('users').updateOne(
-      { _id: doc._id },
-      { $set: { 'name': doc.name.toLowerCase() } }
-    );
+    promises.push(new Promise(async resolve => {
+      await db.collection('users').updateOne(
+        { _id: doc._id },
+        { $set: { 'name': doc.name.toLowerCase() } }
+      );
+      count++;
+      resolve();
+    }));
   });
+  await Promise.all(promises);
   logger.info(`${count} utilisateurs mis à jour`);
-  //exit();
+  exit();
 });
