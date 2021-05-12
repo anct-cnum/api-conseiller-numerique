37159ca3224b880388bb204ec329b094ad6b4cb8
Loic GUILLOIS <contact@loicguillois.fr>
2021-05-12 12:27:37 +0200

    mise à jour cron

--- src/tools/jobs/migrate-pg-to-mongo.sh
+++ src/tools/jobs/migrate-pg-to-mongo.sh
@@ -3,6 +3,6 @@
 cd ${APP_HOME}
 
 echo "Migration Postgresql vers MongoDB : START\n"
-node src/tools/scripts/migrate-pg-to-mongo.js -l 100000
+node src/tools/migration/migrate-pg-to-mongo.js -l 100000
 echo "Migration Postgresql vers MongoDB : END\n"
 
