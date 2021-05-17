9bd623fa03e3e6a53db5264bc8f523eef8d80926
Sébastien Quirrion <sebastien@fitlab.fr>
2021-05-17 17:50:07 +0200

    envoi mail apres choix password conseiller

--- src/emails/emails.js
+++ src/emails/emails.js
@@ -6,6 +6,7 @@ const bienvenueComptePrefet = require('./prefets/bienvenueComptePrefet');
 const bienvenueCompteAdmin = require('./admins/bienvenueCompteAdmin');
 const motDePasseOublie = require('./commun/motDePasseOublie');
 const renouvellementCompte = require('./commun/renouvellementCompte');
+const bienvenueCompteConseiller = require('./conseillers/bienvenueCompteConseiller');
 
 module.exports = (db, mailer, app) => {
 
@@ -18,6 +19,7 @@ module.exports = (db, mailer, app) => {
     bienvenueCompteAdmin(db, mailer, app),
     motDePasseOublie(db, mailer, app),
     renouvellementCompte(db, mailer, app),
+    bienvenueCompteConseiller(db, mailer, app),
   ];
 
   return {
