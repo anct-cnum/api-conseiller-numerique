2a8cb8a19bfa0d7964a62fcfd29cadb7ca023389
Jean-Yves Stervinou <jy@stervinou.com>
2021-05-25 17:21:09 +0200

    Script pour mettre à jour l'adresse email de contact d'une structure, et les autres infos du contact

---
+++ src/tools/admin/structures/users/change-email.js
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+'use strict';
+
+const { program } = require('commander');
+const { Pool } = require('pg');
+
+const { execute } = require('../../../utils');
+
+const pool = new Pool();
+
+execute(__filename, async ({ feathers, db, logger, exit, Sentry }) => {
+  const getStructure = async id => {
+    try {
+      const { rows } = await pool.query(`
+        SELECT
+          id,
+          name,
+          contact_first_name,
+          contact_last_name,
+          contact_job,
+          contact_email,
+          contact_phone,
+          siret
+        FROM djapp_hostorganization
+        WHERE id = $1`,
+      [id]);
+      return rows;
+    } catch (error) {
+      logger.info(`Erreur DB : ${error.message}`);
+    }
+  };
+
+  const updateStructurePG = async (id, contact) => {
+    try {
+      const { rows } = await pool.query(`
+        UPDATE djapp_hostorganization
+        SET (
+          contact_first_name,
+          contact_last_name,
+          contact_job,
+          contact_email,
+          contact_phone)
+          =
+          ($2,$3,$4,$5,$6)
+        WHERE id = $1`,
+      [id, contact.first_name,
+        contact.last_name,
+        contact.job,
+        contact.email,
+        contact.phone]);
+      return rows;
+    } catch (error) {
+      logger.info(`Erreur DB : ${error.message}`);
+    }
+  };
+
+  program.option('-e, --email <email>', 'email: nouvelle adresse email pour la structure');
+  program.option('-n, --nom <nom>', 'nom: nom du nouveau contact');
+  program.option('-p, --prenom <prenom>', 'prenom: prénom du nouveau contact');
+  program.option('-f, --fonction <fonction>', 'fonction: fonction du nouveau contact');
+  program.option('-t, --telephone <telephone>', 'telephone: téléphone du nouveau contact');
+  program.option('-i, --id <id>', 'id: id PG de la structure');
+  program.helpOption('-e', 'HELP command');
+  program.parse(process.argv);
+
+  let id = ~~program.id;
+  let email = program.email;
+
+  const dbName = db.serverConfig.s.options.dbName;
+
+  if (id === 0 || !email) {
+    exit('Paramètres invalides. Veuillez préciser un id et une adresse email');
+    return;
+  }
+
+  const structures = await db.collection('structures').find({ idPG: id }).toArray();
+
+  if (structures.length === 0) {
+    exit('id PG inconnu dans MongoDB');
+    return;
+  }
+  const structure = structures[0];
+
+  const structuresPG = await getStructure(id);
+
+  if (!structuresPG || structuresPG.length === 0) {
+    exit('id PG inconnu dans PostgreSQL');
+    return;
+  }
+  const structurePG = structuresPG[0];
+
+  let contact = {};
+  contact.first_name = program.prenom || structurePG.contact_first_name;
+  contact.last_name = program.nom || structurePG.contact_last_name;
+  contact.job = program.fonction || structurePG.contact_job;
+  contact.email = program.email || structurePG.contact_email;
+  contact.phone = program.telephone || structurePG.contact_phone;
+
+  updateStructurePG(id, contact);
+
+  await db.collection('structures').updateOne({ idPG: id }, { $set: {
+    contact: {
+      prenom: contact.first_name,
+      nom: contact.last_name,
+      fonction: contact.job,
+      email: contact.email,
+      telephone: contact.phone
+    } } }, {});
+
+  await db.collection('users').updateOne({ name: structurePG.contact_email }, { $set: {
+    name: contact.email } }, {});
+
+  exit('Email mis à jour');
+});
