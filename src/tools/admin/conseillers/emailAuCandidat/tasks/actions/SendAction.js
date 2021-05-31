678609bf7a268be2f496ac9d1cf014e8e0b2fb6d
Loic GUILLOIS <contact@loicguillois.fr>
2021-05-31 22:14:56 +0200

    fix vérification date d'email de sondage

--- src/tools/admin/conseillers/emailAuCandidat/tasks/actions/SendAction.js
+++ src/tools/admin/conseillers/emailAuCandidat/tasks/actions/SendAction.js
@@ -2,7 +2,7 @@ class SendAction {
 
   getQuery(limit) {
     return [
-      { '$match': { 'sondageSentAt': null, 'conseillerObj.disponible': true, 'statut': { $ne: 'recrutee' } } },
+      { '$match': { 'conseillerObj.disponible': true, 'statut': { $ne: 'recrutee' } } },
       { $group: { _id: '$conseillerObj._id', email: { $first: '$conseillerObj.email' } } },
       { $limit: limit }
     ];
