36226750de7da2f2b7518bc64c81c9b86fbaa092
Sébastien Quirrion <sebastien@fitlab.fr>
2021-06-14 17:50:16 +0200

    avancement des stats en one shot

--- src/services/stats/stats.class.js
+++ src/services/stats/stats.class.js
@@ -129,19 +129,20 @@ exports.Stats = class Stats extends Service {
         stats.nbUsagersBeneficiantSuivi = await db.collection('cras').aggregate(
           [
             { $match: { ...query, 'cra.accompagnement': { $ne: null } } },
-            { $group: { _id: null, totalSuivi: { $sum: 1 } } }
+            { $group: { _id: null, totalSuivi: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } }
           ]
         ).toArray();
         stats.nbUsagersBeneficiantSuivi = stats.nbUsagersBeneficiantSuivi.length !== 0 ? stats.nbUsagersBeneficiantSuivi[0].totalSuivi : 0;
-
-        //Taux accompagnement
-        stats.tauxTotalUsagersAccompagnes = stats.nbAccompagnement > 0 ? ~~(stats.nbUsagersBeneficiantSuivi / stats.nbAccompagnement * 100) : 0;
 
         //Accompagnement poursuivi en individuel
         stats.nbUsagersAccompagnementIndividuel = await db.collection('cras').aggregate(
           [
             { $match: { ...query, 'cra.accompagnement': { $eq: 'individuel' } } },
-            { $group: { _id: null, totalSuiviIndividuel: { $sum: 1 } } }
+            { $group: { _id: null, totalSuiviIndividuel: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } }
           ]
         ).toArray();
         // eslint-disable-next-line max-len
@@ -151,7 +152,9 @@ exports.Stats = class Stats extends Service {
         stats.nbUsagersAtelierCollectif = await db.collection('cras').aggregate(
           [
             { $match: { ...query, 'cra.accompagnement': { $eq: 'atelier' } } },
-            { $group: { _id: null, totalSuiviAtelier: { $sum: 1 } } }
+            { $group: { _id: null, totalSuiviAtelier: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } }
           ]
         ).toArray();
         stats.nbUsagersAtelierCollectif = stats.nbUsagersAtelierCollectif.length !== 0 ? stats.nbUsagersAtelierCollectif[0].totalSuiviAtelier : 0;
@@ -160,53 +163,79 @@ exports.Stats = class Stats extends Service {
         stats.nbReconduction = await db.collection('cras').aggregate(
           [
             { $match: { ...query, 'cra.accompagnement': { $eq: 'redirection' } } },
-            { $group: { _id: null, totalSuiviRedirection: { $sum: 1 } } }
+            { $group: { _id: null, totalSuiviRedirection: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } }
           ]
         ).toArray();
         stats.nbReconduction = stats.nbReconduction.length !== 0 ? stats.nbReconduction[0].totalSuiviRedirection : 0;
 
-        //Thèmes (total de chaque)
-        stats.statsThemes = [];
+        //Taux accompagnement
+        // eslint-disable-next-line max-len
+        stats.tauxTotalUsagersAccompagnes = stats.nbAccompagnement > 0 ? ~~(stats.nbUsagersBeneficiantSuivi / (stats.nbTotalParticipant + stats.nbAccompagnementPerso + stats.nbDemandePonctuel) * 100) : 0;
+
+        //Thèmes (total de chaque catégorie)
+        stats.statsThemes = [
+          { nom: 'equipement informatique', valeur: 0 },
+          { nom: 'internet', valeur: 0 },
+          { nom: 'courriel', valeur: 0 },
+          { nom: 'smartphone', valeur: 0 },
+          { nom: 'contenus numeriques', valeur: 0 },
+          { nom: 'vocabulaire', valeur: 0 },
+          { nom: 'traitement texte', valeur: 0 },
+          { nom: 'echanger', valeur: 0 },
+          { nom: 'trouver emploi', valeur: 0 },
+          { nom: 'accompagner enfant', valeur: 0 },
+          { nom: 'tpe/pme', valeur: 0 },
+          { nom: 'demarche en ligne', valeur: 0 },
+          { nom: 'autre', valeur: 0 },
+        ];
         let statsThemes = await db.collection('cras').aggregate(
           [
             { $unwind: '$cra.themes' },
             { $match: { ...query } },
             { $group: { _id: '$cra.themes', count: { $sum: 1 } } },
             { $group: { _id: null, listThemes: { $push: {
-              'theme': '$_id',
-              'total': '$count',
+              'nom': '$_id',
+              'valeur': '$count',
             } } } },
             { $project: { '_id': 0, 'listThemes': 1 } }
           ]
         ).toArray();
         if (statsThemes.length > 0) {
-          for (let theme of Object.values(statsThemes[0].listThemes)) {
-            stats.statsThemes[theme.theme] = theme.total;
-          }
+          stats.statsThemes = stats.statsThemes.map(theme1 => statsThemes[0].listThemes.find(theme2 => theme1.nom === theme2.nom) || theme1);
         }
 
-        //Canaux (total de chaque)
-        stats.statsLieux = [];
+        //Canaux (total de chaque catégorie)
+        stats.statsLieux = [
+          { nom: 'domicile', valeur: 0 },
+          { nom: 'distance', valeur: 0 },
+          { nom: 'rattachement', valeur: 0 },
+          { nom: 'autre', valeur: 0 },
+        ];
         let statsLieux = await db.collection('cras').aggregate(
           [
             { $unwind: '$cra.canal' },
             { $match: { ...query } },
             { $group: { _id: '$cra.canal', count: { $sum: 1 } } },
             { $group: { _id: null, listLieux: { $push: {
-              'canal': '$_id',
-              'total': '$count',
+              'nom': '$_id',
+              'valeur': '$count',
             } } } },
             { $project: { '_id': 0, 'listLieux': 1 } }
           ]
         ).toArray();
         if (statsLieux.length > 0) {
-          for (let canal of Object.values(statsLieux[0].listLieux)) {
-            stats.statsLieux[canal.canal] = canal.total;
-          }
+          stats.statsLieux = stats.statsLieux.map(canal1 => statsLieux[0].listLieux.find(canal2 => canal1.nom === canal2.nom) || canal1);
         }
 
-        //Duree (total de chaque)
-        stats.statsDurees = [];
+        //Duree (total de chaque catégorie)
+        stats.statsDurees = [
+          { nom: '0-30', valeur: 0 },
+          { nom: '30-60', valeur: 0 },
+          { nom: '60-120', valeur: 0 },
+          { nom: '120+', valeur: 0 },
+        ];
         //Gestion des categories 0-30 / 30-60
         let statsDurees = await db.collection('cras').aggregate(
           [
@@ -214,16 +243,14 @@ exports.Stats = class Stats extends Service {
             { $match: { ...query, 'cra.duree': { $in: ['0-30', '30-60'] } } },
             { $group: { _id: '$cra.duree', count: { $sum: 1 } } },
             { $group: { _id: null, listDurees: { $push: {
-              'duree': '$_id',
-              'total': '$count',
+              'nom': '$_id',
+              'valeur': '$count',
             } } } },
             { $project: { '_id': 0, 'listDurees': 1 } }
           ]
         ).toArray();
         if (statsDurees.length > 0) {
-          for (let duree of Object.values(statsDurees[0].listDurees)) {
-            stats.statsDurees[duree.duree] = duree.total;
-          }
+          stats.statsDurees = stats.statsDurees.map(duree1 => statsDurees[0].listDurees.find(duree2 => duree1.nom === duree2.nom) || duree1);
         }
         //Ajout du cas spécifique 90 à 120 minutes
         let duree60 = await db.collection('cras').aggregate(
@@ -243,7 +270,7 @@ exports.Stats = class Stats extends Service {
             { $group: { _id: null, total: { $sum: 1 } } }
           ]
         ).toArray();
-        stats.statsDurees['60-120'] = duree60.length !== 0 ? duree60[0].total : 0;
+        stats.statsDurees[stats.statsDurees.findIndex(duree => duree.nom === '60-120')].valeur = duree60.length !== 0 ? duree60[0].total : 0;
 
         //Ajout du cas spécifique > 120 minutes
         let duree120 = await db.collection('cras').aggregate(
@@ -259,9 +286,72 @@ exports.Stats = class Stats extends Service {
             { $group: { _id: null, total: { $sum: 1 } } }
           ]
         ).toArray();
-        stats.statsDurees['120+'] = duree120.length !== 0 ? duree120[0].total : 0;
+        stats.statsDurees[stats.statsDurees.findIndex(duree => duree.nom === '120+')].valeur = duree120.length !== 0 ? duree120[0].total : 0;
 
-        console.log(stats);
+        //Catégorie d'âges (total de chaque catégorie en %)
+        stats.statsAges = [
+          { nom: '-12', valeur: 0 },
+          { nom: '12-18', valeur: 0 },
+          { nom: '18-35', valeur: 0 },
+          { nom: '35-60', valeur: 0 },
+          { nom: '+60', valeur: 0 },
+        ];
+        let statsAges = await db.collection('cras').aggregate(
+          [
+            { $unwind: '$cra.age' },
+            { $match: { ...query } },
+            { $group: { _id: '$cra.age', count: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } },
+            { $group: { _id: null, listAges: { $push: {
+              'nom': '$_id',
+              'valeur': '$count',
+            } } } },
+            { $project: { '_id': 0, 'listAges': 1 } }
+          ]
+        ).toArray();
+        if (statsAges.length > 0) {
+          stats.statsAges = stats.statsAges.map(age1 => statsAges[0].listAges.find(age2 => age1.nom === age2.nom) || age1);
+          //Conversion en % total
+          stats.statsAges = stats.statsAges.map(age => {
+            // eslint-disable-next-line max-len
+            age.valeur = stats.nbAccompagnement > 0 ? ~~(age.valeur / (stats.nbTotalParticipant + stats.nbAccompagnementPerso + stats.nbDemandePonctuel) * 100) : 0;
+            return age;
+          });
+        }
+
+        //Statut des usagers (total de chaque catégorie en %)
+        stats.statsUsagers = [
+          { label: ' test', nom: 'etudiant', valeur: 0 },
+          { nom: 'sans emploi', valeur: 0 },
+          { nom: 'en emploi', valeur: 0 },
+          { nom: 'retraite', valeur: 0 },
+          { nom: 'heterogene', valeur: 0 },
+        ];
+        let statsUsagers = await db.collection('cras').aggregate(
+          [
+            { $unwind: '$cra.statut' },
+            { $match: { ...query } },
+            { $group: { _id: '$cra.statut', count: { $sum: {
+              $cond: [{ '$gt': ['$cra.nbParticipants', 0] }, '$cra.nbParticipants', 1] //Si nbParticipants alors c'est collectif sinon 1
+            } } } },
+            { $group: { _id: null, listStatuts: { $push: {
+              'nom': '$_id',
+              'valeur': '$count',
+            } } } },
+            { $project: { '_id': 0, 'listStatuts': 1 } }
+          ]
+        ).toArray();
+        if (statsUsagers.length > 0) {
+          stats.statsUsagers = stats.statsUsagers.map(statut1 => statsUsagers[0].listStatuts.find(statut2 => statut1.nom === statut2.nom) || statut1);
+          //Conversion en % total
+          stats.statsUsagers = stats.statsUsagers.map(statut => {
+            // eslint-disable-next-line max-len
+            statut.valeur = stats.nbAccompagnement > 0 ? ~~(statut.valeur / (stats.nbTotalParticipant + stats.nbAccompagnementPerso + stats.nbDemandePonctuel) * 100) : 0;
+            return statut;
+          });
+        }
+
         res.send(stats);
       });
     });
