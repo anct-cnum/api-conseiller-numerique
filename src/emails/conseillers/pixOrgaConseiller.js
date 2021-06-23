e5fc7c1002594289228a9fd4ccfdb842f794b621
morgan <morgan@fitlab.fr>
2021-06-23 11:17:48 +0200

    ajout du mail pour pix orga

--- src/emails/conseillers/pixOrgaConseiller.js
+++ src/emails/conseillers/pixOrgaConseiller.js
@@ -2,26 +2,24 @@ module.exports = (db, mailer) => {
   const templateName = 'pixOrgaConseiller';
   let { utils } = mailer;
 
-  let render = async conseiller => {
+  let render = async (user, conseiller) => {
 
     return mailer.render(__dirname, templateName, {
-      link: utils.getPixUrl(`?control1714940=${conseiller?.prenom}&control1714939=${conseiller?.nom}&control1714941=${conseiller?.email}`),
+      link: utils.getPixUrl(`?control1714940=${conseiller?.prenom}&control1714939=${conseiller?.nom}&control1714941=${user?.name}`),
     });
   };
 
   return {
     templateName,
     render,
-    send: async conseiller => {
-      console.log(conseiller);
-      let onSuccess = () => {
-      };
+    send: async (user, conseiller) => {
+      let onSuccess = () => { };
 
       let onError = async err => {
         throw err;
       };
       return mailer.createMailer().sendEmail(
-        conseiller.email,
+        user.name,
         {
           subject: 'Nouvel outil dans votre espace Coop : vous êtes invité(e) à rejoindre Pix Orga',
           body: await render(conseiller),
