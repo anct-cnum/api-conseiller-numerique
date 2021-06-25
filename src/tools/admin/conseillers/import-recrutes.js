dc7cb00a8452481bdfffa2e6b88476d9a5eaf2d2
Loic GUILLOIS <contact@loicguillois.fr>
2021-06-25 15:50:15 +0200

    creation utilisateur et mise à jour des mises en relation

--- src/tools/admin/conseillers/import-recrutes.js
+++ src/tools/admin/conseillers/import-recrutes.js
@@ -1,5 +1,7 @@
 const CSVToJSON = require('csvtojson');
 const { program } = require('commander');
+const moment = require('moment');
+const { v4: uuidv4 } = require('uuid');
 
 program
 .option('-c, --csv <path>', 'CSV file path');
@@ -18,7 +20,7 @@ const readCSV = async filePath => {
 
 const { execute } = require('../../utils');
 
-execute(__filename, async ({ db, logger, exit, Sentry }) => {
+execute(__filename, async ({ feathers, db, logger, exit, Sentry }) => {
 
   logger.info('Import des conseillers recrutés');
   let count = 0;
@@ -35,14 +37,48 @@ execute(__filename, async ({ db, logger, exit, Sentry }) => {
             errors++;
             reject();
           } else {
+
+            const structureId = conseiller['ID structure (plateforme)'];
+
             const result = await db.collection('conseillers').updateOne({ email }, {
               $set: {
                 statut: 'RECRUTE',
                 disponible: false,
                 estRecrute: true,
-                recrutedAt: new Date()
-                // à voir pour les statuts: estEnEmploi, estEnFormation, estDemandeurEmploi...
+                datePrisePoste: moment(conseiller['Date de prise de poste / départ en formation'], 'DD/MM/YYYY'),
+                dateFinFormation: moment(conseiller['Date de fin de formation'], 'DD/MM/YYYY'),
+                structureId
               }
+            });
+
+            await db.collection('miseEnRelation').updateOne({ 'conseillerObj.email': email, 'structureObj.idPG': structureId }, {
+              $set: {
+                statut: 'finalisee',
+              }
+            });
+
+            await db.collection('miseEnRelation').updateOne({ 'conseillerObj.email': email, 'structureObj.idPG': { $ne: structureId } }, {
+              $set: {
+                statut: 'finalisee_non_disponible',
+              }
+            });
+
+            const role = 'conseiller';
+            const dbName = db.serverConfig.s.options.dbName;
+            const conseillerDoc = await db.collection('conseillers').findOne({ email });
+            await feathers.service('users').create({
+              name: email,
+              password: null,
+              roles: Array(role),
+              entity: {
+                '$ref': `${role}s`,
+                '$id': conseillerDoc._id,
+                '$db': dbName
+              },
+              token: uuidv4(),
+              mailSentDate: null, // on stock la date du dernier envoi de mail de création pour le mécanisme de relance
+              passwordCreated: false,
+              createdAt: new Date(),
             });
 
             if (result.matchedCount === 1) {
