ad92c15917d23c0cb5f667509e4183fd81278a6a
Loic GUILLOIS <contact@loicguillois.fr>
2021-07-02 16:08:11 +0200

    script creation compte mattermost

---
+++ src/tools/admin/conseillers/mattermost.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+'use strict';
+
+const { program } = require('commander');
+const slugify = require('slugify');
+const { ObjectID } = require('mongodb');
+
+require('dotenv').config();
+
+const { execute } = require('../../utils');
+const { createAccount } = require('../../../utils/mattermost');
+
+execute(__filename, async ({ logger, exit, app, db, Sentry }) => {
+
+  program.option('-p, --password <password>', 'password: clear text');
+  program.option('-i, --id <id>', 'id: MongoDB ObjecID');
+  program.option('-a, --operation <operation>', 'operation: create');
+  program.helpOption('-e', 'HELP command');
+  program.parse(process.argv);
+
+  const password = program.password;
+  const operation = program.operation;
+  const id = program.id;
+
+  if (!password || !operation || !id) {
+    exit('Paramètres invalides');
+    return;
+  }
+
+  if (!['create'].includes(operation)) {
+    exit('Action non reconnu');
+    return;
+  }
+
+  const conseiller = await db.collection('conseillers').findOne({
+    _id: new ObjectID(id)
+  });
+
+  if (conseiller === null) {
+    exit('Conseiller introuvable');
+    return;
+  }
+
+  const login = slugify(`${conseiller.prenom}.${conseiller.nom}`, { replacement: '.', lower: true, strict: true });
+
+  const mattermost = app.get('mattermost');
+  if (operation === 'create') {
+    createAccount({ mattermost, conseiller, login, password, db, logger, Sentry });
+  }
+});
