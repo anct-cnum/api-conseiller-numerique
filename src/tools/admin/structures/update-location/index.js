6e20360c1bb07593c4d72000ad79e0f770ee6ecd
Jean-Yves Stervinou <jy@stervinou.com>
2021-08-02 19:41:05 +0200

    Mise à jour du champ location (type PointField de PostGIS) d'une structure en fonction de son code commune INSEE

---
+++ src/tools/admin/structures/update-location/index.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+/* eslint-disable guard-for-in */
+'use strict';
+
+require('dotenv').config();
+
+const axios = require('axios');
+const { Pool } = require('pg');
+const { execute } = require('../../../utils');
+const { program } = require('commander');
+
+program
+.option('-i, --id <id>', 'id PG de la structure');
+
+program.parse(process.argv);
+
+const pool = new Pool();
+
+execute(__filename, async ({ logger, exit }) => {
+
+  const getStructure = async id => {
+    try {
+      const { rows } = await pool.query(`SELECT * FROM djapp_hostorganization WHERE id=$1`, [id]);
+      if (rows.length > 0) {
+        logger.info(`Nombre : ${rows.length}`);
+        return rows;
+      } else {
+        logger.info(`Structure introuvable`);
+      }
+    } catch (error) {
+      logger.error(`Erreur DB : ${error.message}`);
+    }
+  };
+
+  const getGeo = async codeCommune => {
+    if (!/^\d{5}$/.test(codeCommune)) {
+      logger.info(`Code commune invalide : ${codeCommune}`);
+      return;
+    }
+
+    const urlAPI = `https://geo.api.gouv.fr/communes/${codeCommune}?format=geojson&geometry=centre`;
+    const params = {};
+
+    try {
+      const result = await axios.get(urlAPI, { params: params });
+      return result.data;
+    } catch (e) {
+      throw new Error(`API Error : ${e}`);
+    }
+  };
+
+  const storeGeo = async (s, geo) => {
+    try {
+      await pool.query(`UPDATE djapp_hostorganization SET location = ST_GeomFromGeoJSON
+            ($2) WHERE id=$1`,
+      [s.id, geo.geometry]);
+    } catch (error) {
+      logger.error(`Erreur DB : ${error.message}`);
+    }
+  };
+
+  logger.info('Recherche de la structure...');
+
+  let count = 0;
+
+  const structures = await getStructure(program.id);
+
+  for (const s of structures) {
+    const geo = await getGeo(s.commune_code);
+
+    if (geo.length === 0) {
+      logger.info(`Aucun résultat pour ${s.commune_code}`);
+    } else {
+      await storeGeo(s, geo);
+      count++;
+      logger.info(`${geo.properties.nom}`);
+    }
+  }
+
+  logger.info(`${count} structures mises à jour`);
+
+  exit();
+});
