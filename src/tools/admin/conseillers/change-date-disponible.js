7dbd9d5998de9edc1091e2288ea2bbfbb68638e9
Ornella Ourfi <ornella93@outlook.fr>
2021-08-11 15:03:08 +0200

    ajout du dayjs + sentry error + logger

--- src/tools/admin/conseillers/change-date-disponible.js
+++ src/tools/admin/conseillers/change-date-disponible.js
@@ -3,17 +3,18 @@ const { execute } = require('../../utils');
 const { ObjectID } = require('mongodb');
 const { Pool } = require('pg');
 const pool = new Pool();
+const dayjs = require('dayjs');
 
-execute(__filename, async ({ db, logger, exit }) => {
+execute(__filename, async ({ db, logger, Sentry, exit }) => {
 
-  program.option('-d, --disponible <type>', 'disoponible : entrer  la date de disponibilité du conseiller');
+  program.option('-d, --disponible <type>', 'disponible : entrer  la date de disponibilité du conseiller sous ce format AAAA/MM/DD');
   program.option('-i, --id <id>', 'id: id PG du conseiller');
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
 
   let id = ~~program.id;
-  let disponible = program.disponible;
-  disponible = new Date(disponible);
+  let dispo = program.disponible;
+  let disponible = dayjs(dispo, 'YYYY-MM-DD').toDate();
 
   if (id === 0 || !disponible) {
     exit('Paramètres invalides. Veuillez préciser un id et une date de disponibilité');
@@ -26,24 +27,31 @@ execute(__filename, async ({ db, logger, exit }) => {
     exit('id PG inconnu dans MongoDB');
     return;
   }
+  //Update dans MongoDb
+  try {
+    await db.collection('conseillers').updateOne({ idPG: id }, { $set: { dateDisponibilite: disponible } });
+    await db.collection('misesEnRelation').updateMany(
+      { 'conseillerObj._id': new ObjectID(conseiller._id) },
+      { $set: { 'conseillerObj.dateDisponibilite': disponible }
+      });
 
-  await db.collection('conseillers').updateOne({ idPG: id }, { $set: { dateDisponibilite: disponible } });
-
-  await db.collection('misesEnRelation').updateMany(
-    { 'conseillerObj._id': new ObjectID(conseiller._id) },
-    { $set: { 'conseillerObj.dateDisponibilite': disponible }
-    });
-
+  } catch (error) {
+    logger.error(`Erreur MongoDB : ${error.message}`);
+    Sentry.captureException(error);
+    return;
+  }
+  // Update dans PG
   try {
     await pool.query(`UPDATE djapp_coach
       SET start_date = $2 WHERE id = $1`,
     [conseiller.idPG, disponible]);
 
   } catch (error) {
-    logger.error(`Erreur DB : ${error.message}`);
-    //a mettre erreur sentry ?
+    logger.error(`Erreur PG : ${error.message}`);
+    Sentry.captureException(error);
+    return;
   }
 
-  logger.info(`date mis à jour pour le conseiller avec l'idPG: ${conseiller.idPG}`);
+  logger.info(`date mis à jour pour le conseiller avec l'idPG: ${conseiller.idPG} pour une date de disponibilté à partir du ${dispo} (format: AAAA/MM/DD)`);
   exit();
 });
