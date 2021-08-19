f7fa854d0f1ffbbc2e2efc42e14171b27291f120
Ornella Ourfi <ornella93@outlook.fr>
2021-08-19 15:51:35 +0200

    orthographe + condition simplifié pour le résultat des infos

--- src/tools/exports/listeInviteCoop.js
+++ src/tools/exports/listeInviteCoop.js
@@ -1,31 +1,30 @@
 #!/usr/bin/env node
 'use strict';
-const { ObjectID } = require('mongodb');
 const path = require('path');
 const fs = require('fs');
 
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseiller = await db.collection('users').find({ roles: 'conseiller' }).toArray();
+  const conseillers = await db.collection('users').find({ roles: 'conseiller' }).toArray();
   let promises = [];
 
   logger.info(`Generating CSV file...`);
 
-  logger.info(`Il y a en tout ${conseiller.length} compte coop créer`);
-  let csvFile = path.join(__dirname, '../../../data/exports', `liste_invite_coop.csv`);
+  logger.info(`${conseillers.length} comptes coop créés`);
+  let csvFile = path.join(__dirname, '../../../data/exports', `liste_emails_coop.csv`);
 
   let file = fs.createWriteStream(csvFile, {
     flags: 'w'
   });
 
-  file.write('Email_Coop;Email personelle du conseiller;Email de la structure\n');
-  conseiller.forEach(infoUser => {
+  file.write('Email CN;Email personelle du conseiller;Email de la structure\n');
+  conseillers.forEach(infoUser => {
     promises.push(new Promise(async resolve => {
-      let conseiller = await db.collection('conseillers').findOne({ _id: new ObjectID(infoUser.entity.oid) });
+      let conseiller = await db.collection('conseillers').findOne({ _id: infoUser.entity.oid });
       let structure = await db.collection('structures').findOne({ _id: conseiller?.structureId });
       // eslint-disable-next-line max-len
-      file.write(`${!infoUser?.name ? 'Non renseigné' : infoUser.name};${!conseiller?.email ? 'Non renseigné' : conseiller.email};${!structure?.contact?.email ? `${structure?.nom} n'a pas renseigné d'email de contact` : structure?.contact?.email}\n`);
+      file.write(`${infoUser?.name ?? 'Non renseigné'};${conseiller?.email ?? 'Non renseigné'};${structure?.contact?.email ?? `${structure?.nom} n'a pas renseigné d'email de contact`}\n`);
       resolve();
     }));
   });
