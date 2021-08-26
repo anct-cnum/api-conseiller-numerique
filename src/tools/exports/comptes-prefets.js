7522c7a616a757812c3411926e6e48c137e8fde3
Loïc Guillois <contact@loicguillois.fr>
2021-08-26 13:00:01 +0200

    script export des préfectures qui ont créé leur compte

---
+++ src/tools/exports/comptes-prefets.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+'use strict';
+const { ObjectID } = require('mongodb');
+const path = require('path');
+const fs = require('fs');
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ logger, db }) => {
+  const departements = await db.collection('users').aggregate([
+    { $match: { roles: { $elemMatch: { $eq: 'prefet' } } } }, { $group: { _id: '$departement' } }
+  ]).toArray();
+  let promises = [];
+
+  logger.info(`Generating CSV file...`);
+  let csvFile = path.join(__dirname, '../../../data/exports', 'comptes_prefets.csv');
+
+  let file = fs.createWriteStream(csvFile, {
+    flags: 'w'
+  });
+
+  // eslint-disable-next-line max-len
+  file.write('departement\n');
+  departements.forEach(departement => {
+    promises.push(new Promise(async resolve => {
+      file.write(`${departement._id}\n`);
+      resolve();
+    }));
+  });
+  await Promise.all(promises);
+  file.close();
+});
