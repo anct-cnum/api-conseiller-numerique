0bc89b0db4ca227d805ac9144d3217dab8c8af2a
Sébastien Quirrion <sebastien@fitlab.fr>
2021-08-30 17:19:46 +0200

    archivage script metabase et suppression doublon index

--- src/tools/indexes/tasks/mongoIndexes.js
+++ src/tools/indexes/tasks/mongoIndexes.js
@@ -29,7 +29,6 @@ module.exports = {
       db.collection('misesEnRelation').createIndex({ 'structure.$id': 1 }),
       db.collection('misesEnRelation').createIndex({ 'structure.oid': 1 }),
       db.collection('misesEnRelation').createIndex({ 'conseiller.$id': 1 }),
-      db.collection('misesEnRelation').createIndex({ 'statut': 1 }),
       db.collection('misesEnRelation').createIndex({ 'structureObj.codePostal': 1 }),
       db.collection('misesEnRelation').createIndex({ 'conseillerObj.disponible': 1 }),
       db.collection('misesEnRelation').createIndex({
