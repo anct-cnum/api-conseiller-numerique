52fab671497e117340a251d4b129743192a7fd89
Ornella Ourfi <ornella93@outlook.fr>
2021-09-07 15:04:57 +0200

    script pour modifier la clé distanceMax d'un conseiller/candidat

---
+++ src/tools/admin/conseillers/change-distance-km.js
@@ -0,0 +1,51 @@
+const { program } = require('commander');
+const { execute } = require('../../utils');
+const { Pool } = require('pg');
+const pool = new Pool();
+
+execute(__filename, async ({ db, logger, Sentry, exit }) => {
+
+  program.option('-distance, --distance <distance>', 'distance max en km');
+  program.option('-i, --id <id>', 'id: id PG du conseiller');
+  program.helpOption('-e', 'HELP command');
+  program.parse(process.argv);
+
+  let id = ~~program.id;
+  let distance = program.distance;
+
+  if (id === 0 || !distance) {
+    exit('Paramètres invalides. Veuillez préciser un id et une date de disponibilité');
+    return;
+  }
+
+  const conseiller = await db.collection('conseillers').findOne({ idPG: id });
+
+  if (conseiller === null) {
+    exit('id PG inconnu dans MongoDB');
+    return;
+  }
+
+  try {
+    await db.collection('conseillers').updateOne({ idPG: id }, { $set: { distanceMax: distance } });
+    await db.collection('misesEnRelation').updateMany(
+      { 'conseillerObj._id': conseiller._id },
+      { $set: { 'conseillerObj.distanceMax': distance }
+      });
+  } catch (error) {
+    logger.error(`Erreur MongoDB : ${error.message}`);
+    Sentry.captureException(error);
+    return;
+  }
+  try {
+    await pool.query(`UPDATE djapp_coach
+      SET max_distance = $2 WHERE id = $1`,
+    [id, distance]);
+  } catch (error) {
+    logger.error(`Erreur PG : ${error.message}`);
+    Sentry.captureException(error);
+    return;
+  }
+
+  logger.info(`distance géographique mis à jour de ${conseiller.distanceMax} km par ${distance} km pour le conseiller idPG: ${id}`);
+  exit();
+});
