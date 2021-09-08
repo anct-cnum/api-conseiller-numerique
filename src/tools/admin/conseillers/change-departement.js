d60fddb8716a0a4f20ef8a60113134dabb3f1e58
Ornella Ourfi <ornella93@outlook.fr>
2021-09-08 16:17:39 +0200

    ajout de la partie PG + rectif de l'emplacement pour la partie mongo

--- src/tools/admin/conseillers/change-departement.js
+++ src/tools/admin/conseillers/change-departement.js
@@ -1,5 +1,7 @@
 const { program } = require('commander');
 const { execute } = require('../../utils');
+const { Pool } = require('pg');
+const pool = new Pool();
 const axios = require('axios');
 
 execute(__filename, async ({ db, logger, Sentry, exit }) => {
@@ -24,27 +26,27 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
     exit('id PG inconnu dans MongoDB');
     return;
   }
+  const params = {};
+  const urlAPI2 = `https://geo.api.gouv.fr/communes/${codeCommune}?format=geojson&geometry=centre`;
+  const { data } = await axios.get(urlAPI2, { params: params });
+  const miseAJour = {
+    location: data.geometry,
+    codePostal: codePostal,
+    nomCommune: data.properties.nom,
+    codeCommune: data.properties.code,
+    codeDepartement: data.properties.codeDepartement,
+    codeRegion: data.properties.codeRegion
+  };
+  const miseAJourMiseEnRelation = {
+    'conseillerObj.location': data.geometry,
+    'conseillerObj.codePostal': codePostal,
+    'conseillerObj.nomCommune': data.properties.nom,
+    'conseillerObj.codeCommune': data.properties.code,
+    'conseillerObj.codeDepartement': data.properties.codeDepartement,
+    'conseillerObj.codeRegion': data.properties.codeRegion
+  };
 
   try {
-    const params = {};
-    const urlAPI2 = `https://geo.api.gouv.fr/communes/${codeCommune}?format=geojson&geometry=centre`;
-    const { data } = await axios.get(urlAPI2, { params: params });
-    const miseAJour = {
-      location: data.geometry,
-      codePostal: codePostal,
-      nomCommune: data.properties.nom,
-      codeCommune: data.properties.code,
-      codeDepartement: data.properties.codeDepartement,
-      codeRegion: data.properties.codeRegion
-    };
-    const miseAJourMiseEnRelation = {
-      'conseillerObj.location': data.geometry,
-      'conseillerObj.codePostal': codePostal,
-      'conseillerObj.nomCommune': data.properties.nom,
-      'conseillerObj.codeCommune': data.properties.code,
-      'conseillerObj.codeDepartement': data.properties.codeDepartement,
-      'conseillerObj.codeRegion': data.properties.codeRegion
-    };
     await db.collection('conseillers').updateOne({ idPG: id }, { $set: miseAJour });
     await db.collection('misesEnRelation').updateMany({ 'conseiller.$id': conseiller._id }, { $set: miseAJourMiseEnRelation });
   } catch (error) {
@@ -52,7 +54,34 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
     Sentry.captureException(error);
     return;
   }
+  try {
+    await pool.query(`UPDATE djapp_coach
+      SET (
+      zip_code,
+      geo_name,
+      commune_code,
+      departement_code,
+      region_code
+      ) = ($2,$3,$4,$5,$6)
+       WHERE id = $1`,
+    [
+      id,
+      codePostal,
+      data.properties.nom,
+      data.properties.code,
+      data.properties.codeDepartement,
+      data.properties.codeRegion
+    ]);
+    await pool.query(`UPDATE djapp_coach SET location = ST_GeomFromGeoJSON
+            ($2) WHERE id=$1`,
+    [id, data.geometry]);
 
-  logger.info(`ok`);
+  } catch (error) {
+    logger.error(`Erreur PG : ${error.message}`);
+    Sentry.captureException(error);
+    return;
+  }
+
+  logger.info(`le nouveau code Postal est bien le ${codePostal} => conseiller avec l'idPG: ${id}`);
   exit();
 });
