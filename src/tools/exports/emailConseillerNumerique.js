4e456dbc9330ae4a750e3c1e858589000ba1b3e9
Ornella Ourfi <ornella93@outlook.fr>
2021-09-16 10:35:13 +0200

    export des emails de conseiller numérique à  la demande de Lauren

---
+++ src/tools/exports/emailConseillerNumerique.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict';
+const path = require('path');
+const fs = require('fs');
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ logger, db }) => {
+  const conseillers = await db.collection('users').find({ roles: 'conseiller', passwordCreated: true }).toArray();
+
+  let promises = [];
+
+  logger.info(`${conseillers.length} conseiller qui ont crée leurs email @conseiller-numerique.fr`);
+  logger.info(`Generating CSV file...`);
+  let csvFile = path.join(__dirname, '../../../data/exports', 'email_conseiller_numerique.csv');
+
+  let file = fs.createWriteStream(csvFile, {
+    flags: 'w'
+  });
+
+  file.write('Nom; Prenom; Email conseiller-numerique.fr; Structure associé; SIRET\n');
+  conseillers.forEach(conseiller => {
+    promises.push(new Promise(async resolve => {
+      const infoConseiller = await db.collection('conseillers').findOne({ _id: conseiller?.entity?.oid });
+      const structure = await db.collection('structures').findOne({ _id: infoConseiller?.structureId });
+      // eslint-disable-next-line max-len
+      file.write(`${infoConseiller?.nom ?? 'Non renseigné'};${infoConseiller?.prenom ?? 'Non renseigné'};${conseiller?.name ?? 'Non renseigné'};${structure?.nom ?? 'Non renseigné'};${structure?.siret ?? 'Non renseigné'}\n`);
+      resolve();
+    }));
+  });
+  await Promise.all(promises);
+  file.close();
+});
