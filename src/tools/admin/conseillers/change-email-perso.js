b80ca57fe878630f6b4b785de57333811d0b46ad
Ornella Ourfi <ornella93@outlook.fr>
2021-10-12 11:41:56 +0200

    script pour changer l'email perso d'un conseiller

---
+++ src/tools/admin/conseillers/change-email-perso.js
@@ -0,0 +1,52 @@
+const { program } = require('commander');
+const { execute } = require('../../utils');
+const { Pool } = require('pg');
+const pool = new Pool();
+
+execute(__filename, async ({ db, logger, Sentry, exit }) => {
+
+  program.option('-e, --email <email>', 'email: nouvelle adresse mail');
+  program.option('-i, --id <id>', 'id: id PG du conseiller');
+  program.helpOption('-e', 'HELP command');
+  program.parse(process.argv);
+
+  let id = ~~program.id;
+  let email = program.email;
+
+  if (id === 0 || !email) {
+    exit('Paramètres invalides. Veuillez préciser un id et le nouveau email à changer');
+    return;
+  }
+
+  const conseiller = await db.collection('conseillers').findOne({ idPG: id });
+
+  if (conseiller === null) {
+    exit('id PG inconnu dans MongoDB');
+    return;
+  }
+
+  try {
+    await db.collection('conseillers').updateOne({ idPG: id }, { $set: { email: email } });
+    await db.collection('misesEnRelation').updateMany(
+      { 'conseiller.$id': conseiller._id },
+      { $set: { 'conseillerObj.email': email }
+      });
+  } catch (error) {
+    logger.error(error.message);
+    Sentry.captureException(error);
+    return;
+  }
+
+  try {
+    await pool.query(`UPDATE djapp_coach
+      SET email = $2 WHERE id = $1`,
+    [id, email]);
+  } catch (error) {
+    logger.error(error.message);
+    Sentry.captureException(error);
+    return;
+  }
+
+  logger.info(`Email perso : ${conseiller.email} changer par => ${email} pour le conseiller avec l'id ${id}`);
+  exit();
+});
