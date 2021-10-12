b802fe5ad2bc7b0244b226da76caf1037ad25fdb
Ornella Ourfi <ornella93@outlook.fr>
2021-10-12 15:44:00 +0200

    rectif message + orthographe + condition

--- src/tools/admin/conseillers/change-email-perso.js
+++ src/tools/admin/conseillers/change-email-perso.js
@@ -7,32 +7,34 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
 
   program.option('-e, --email <email>', 'email: nouvelle adresse mail');
   program.option('-i, --id <id>', 'id: id PG du conseiller');
-  program.option('-t, --type <id>', `id: déficfinir si c'est un candidat ou un conseiller`);
+  program.option('-t, --typo <typo>', `typo: candidat ou un conseiller`);
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
 
   let id = ~~program.id;
   let email = program.email;
-  let type = program.type;
+  let typo = program.typo;
 
   if (id === 0 || !email) {
     exit('Paramètres invalides. Veuillez préciser un id et le nouveau email à changer');
     return;
   }
-  if (!['candidat', 'conseiller'].includes(type)) {
+  if (!['candidat', 'conseiller'].includes(typo)) {
     exit('Paramètres invalides. Veuillez un type : candidat ou conseiller');
     return;
   }
   const conseiller = await db.collection('conseillers').findOne({ idPG: id });
-  const { roles, name } = await db.collection('users').findOne({ 'entity.$id': conseiller._id });
-  let role = roles[0];
+  const { roles } = await db.collection('users').findOne({ 'entity.$id': conseiller._id });
 
   if (conseiller === null) {
     exit('id PG inconnu dans MongoDB');
     return;
   }
 
-
+  if (roles[0] !== typo) {
+    exit(`Le conseiller id=${id} n'est pas de type ${typo} mais ${roles[0]}`);
+    return;
+  }
   try {
     await pool.query(`UPDATE djapp_coach
     SET email = $2 WHERE id = $1`,
@@ -49,12 +51,10 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
       { 'conseiller.$id': conseiller._id },
       { $set: { 'conseillerObj.email': email }
       });
-    if (role !== type) {
-      // eslint-disable-next-line max-len
-      exit(`Nous avons pas changer le nouveau email dans la collection users car le conseiller avec l'id ${id} à un role : ${roles} avec comme name ${name} ! Ce qui n'a pas empecher le changement dans la collection conseillers/misesEnRelation & dans PG ( ${conseiller.email} par => ${email})`);
-      return;
+    if (typo === 'candidat') {
+      await db.collection('users').updateOne({ 'entity.$id': conseiller._id }, { $set: { name: email } });
     }
-    await db.collection('users').updateOne({ 'entity.$id': conseiller._id }, { $set: { name: email } });
+
   } catch (error) {
     logger.error(error.message);
     Sentry.captureException(error);
