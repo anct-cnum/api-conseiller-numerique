f3bdd3e5564b3ec82c53aad640236e13f0db269b
Sébastien Quirrion <sebastien@fitlab.fr>
2021-10-14 15:43:24 +0200

    Adaptation export liste CVs avec la gestion des doublons

--- src/tools/exports/cvFilesList.js
+++ src/tools/exports/cvFilesList.js
@@ -6,7 +6,11 @@ const fs = require('fs');
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseillers = await db.collection('conseillers').find({ cv: { $exists: true } }).toArray();
+  const conseillers = await db.collection('conseillers').find({ cv: { $exists: true } }).sort({ 'cv.file': 1 }).toArray();
+
+  // Conserver uniquement la liste des CVs et sans doublon
+  let cvFiles = conseillers.map(conseiller => conseiller['cv'].file);
+  cvFiles = [...new Set(cvFiles)];
 
   let promises = [];
 
@@ -19,9 +23,9 @@ execute(__filename, async ({ logger, db }) => {
   });
 
   file.write('CV\n');
-  conseillers.forEach(conseiller => {
+  cvFiles.forEach(cv => {
     promises.push(new Promise(async resolve => {
-      file.write(`${conseiller.cv.file}\n`);
+      file.write(`${cv}\n`);
       resolve();
     }));
   });
