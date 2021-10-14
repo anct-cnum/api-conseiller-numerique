d3caa27364f032b9222a0817d47ad771317c321f
morgan <morgan@fitlab.fr>
2021-10-14 17:41:27 +0200

    séparation de puppeteer

--- src/services/stats/stats.class.js
+++ src/services/stats/stats.class.js
@@ -6,7 +6,7 @@ const statsCras = require('./cras');
 const Joi = require('joi');
 const dayjs = require('dayjs');
 const logger = require('../../logger');
-const puppeteer = require('puppeteer');
+const statsPdf = require('./stats.pdf');
 
 exports.Stats = class Stats extends Service {
   constructor(options, app) {
@@ -196,42 +196,11 @@ exports.Stats = class Stats extends Service {
           return;
         }
 
-        let finUrl = type + '/' + idType + '/' + dateDebut + '/' + dateFin;
+        let finUrl = '/' + type + '/' + idType + '/' + dateDebut + '/' + dateFin;
 
         /** Ouverture d'un navigateur en headless afin de générer le PDF **/
         try {
-          const browser = await puppeteer.launch();
-          browser.on('targetchanged', async target => {
-            const targetPage = await target.page();
-            const client = await targetPage.target().createCDPSession();
-            await client.send('Runtime.evaluate', {
-              expression: `localStorage.setItem('user', '{"accessToken":"${accessToken}",` +
-              `"authentication":{` +
-                `"strategy":"local",` +
-                `"accessToken":"${accessToken}"},` +
-              `"user":${JSON.stringify(user)}}')`
-            });
-          });
-
-          const page = await browser.newPage();
-
-          await Promise.all([
-            page.goto(app.get('espace_coop_hostname') + '/statistiques/' + finUrl, { waitUntil: 'networkidle0' }),
-          ]);
-
-          await page.waitForTimeout(500);
-
-          let pdf;
-          await Promise.all([
-            page.addStyleTag({ content: '#burgerMenu { display: none} .no-print { display: none }' }),
-            pdf = page.pdf({ format: 'A4', printBackground: true })
-          ]);
-
-          await browser.close();
-
-          res.contentType('application/pdf');
-          pdf.then(buffer => res.send(buffer));
-
+          await statsPdf.generatePdf(app, res, accessToken, user, finUrl);
           return;
         } catch (error) {
           app.get('sentry').captureException(error);
