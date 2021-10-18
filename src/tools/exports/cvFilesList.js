509fa8da7825bc66098fc6c7eb19cf7d0e9f4ceb
Sébastien Quirrion <sebastien@fitlab.fr>
2021-10-18 10:16:24 +0200

    Passage en stream

--- src/tools/exports/cvFilesList.js
+++ src/tools/exports/cvFilesList.js
@@ -6,29 +6,39 @@ const fs = require('fs');
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseillers = await db.collection('conseillers').find({ cv: { $exists: true } }).sort({ 'cv.file': 1 }).toArray();
+  const conseillers = await db.collection('conseillers').find({ cv: { $exists: true } }).sort({ 'cv.file': 1 }).stream();
 
-  // Conserver uniquement la liste des CVs et sans doublon
-  let cvFiles = conseillers.map(conseiller => conseiller['cv'].file);
-  cvFiles = [...new Set(cvFiles)];
+  const generateCsv = async cvFiles => {
+    let promises = [];
 
-  let promises = [];
+    logger.info(`${cvFiles.length} conseillers avec CV`);
+    logger.info(`[Début] Génération du csv liste des CVs`);
+    let csvFile = path.join(__dirname, '../../../data/exports', 'listeCVs.csv');
 
-  logger.info(`${conseillers.length} conseillers avec CV`);
-  logger.info(`Generating CSV file...`);
-  let csvFile = path.join(__dirname, '../../../data/exports', 'listeCVs.csv');
+    let file = fs.createWriteStream(csvFile, {
+      flags: 'w'
+    });
 
-  let file = fs.createWriteStream(csvFile, {
-    flags: 'w'
+    file.write('CV\n');
+    cvFiles.forEach(cv => {
+      promises.push(new Promise(async resolve => {
+        file.write(`${cv}\n`);
+        resolve();
+      }));
+    });
+    await Promise.all(promises);
+    file.close();
+    logger.info(`[Fin] Génération du csv liste des CVs`);
+  };
+
+  // Conservation de la liste des fichiers CVs uniquement
+  let cvFiles = [];
+  conseillers.on('data', conseiller => {
+    cvFiles.push(conseiller.cv.file);
   });
 
-  file.write('CV\n');
-  cvFiles.forEach(cv => {
-    promises.push(new Promise(async resolve => {
-      file.write(`${cv}\n`);
-      resolve();
-    }));
+  conseillers.on('end', () => {
+    cvFiles = [...new Set(cvFiles)]; //sans doublon
+    generateCsv(cvFiles);
   });
-  await Promise.all(promises);
-  file.close();
 });
