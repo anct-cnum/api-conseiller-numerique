e925f0a8d8e1fa7585272e853ddf3ea9d4bf600c
morgan <morgan@fitlab.fr>
2021-10-19 09:41:30 +0200

    Essayer de catcher l'erreur de génération PDF

--- src/services/stats/stats.pdf.js
+++ src/services/stats/stats.pdf.js
@@ -1,40 +1,64 @@
 
 const puppeteer = require('puppeteer');
 
-const generatePdf = async (app, res, accessToken, user, finUrl = null) => {
+const generatePdf = async (app, res, logger, accessToken, user, finUrl = null) => {
 
   const browser = await puppeteer.launch();
-  browser.on('targetchanged', async target => {
-    const targetPage = await target.page();
-    const client = await targetPage.target().createCDPSession();
-    await client.send('Runtime.evaluate', {
-      expression: `localStorage.setItem('user', '{"accessToken":"${accessToken}",` +
-      `"authentication":{` +
-        `"strategy":"local",` +
-        `"accessToken":"${accessToken}"},` +
-      `"user":${JSON.stringify(user)}}')`
+
+  try {
+    browser.on('targetchanged', async target => {
+      const targetPage = await target.page();
+      const client = await targetPage.target().createCDPSession();
+      await client.send('Runtime.evaluate', {
+        expression: `localStorage.setItem('user', '{"accessToken":"${accessToken}",` +
+        `"authentication":{` +
+          `"strategy":"local",` +
+          `"accessToken":"${accessToken}"},` +
+        `"user":${JSON.stringify(user)}}')`
+      });
     });
-  });
+  } catch (error) {
+    app.get('sentry').captureException(error);
+    logger.error(error);
+  }
 
-  const page = await browser.newPage();
+  try {
+    const page = await browser.newPage();
+    await Promise.all([
+      page.goto(app.get('espace_coop_hostname') + '/statistiques' + finUrl, { waitUntil: 'networkidle0' }),
+    ]);
+    await page.waitForTimeout(500);
+    let pdf;
+    try {
+      await Promise.all([
+        page.addStyleTag({ content: '#burgerMenu { display: none} .no-print { display: none }' }),
+        pdf = page.pdf({ format: 'A4', printBackground: true })
+      ]);
+    } catch (error) {
+      app.get('sentry').captureException(error);
+      logger.error(error);
+    }
 
-  await Promise.all([
-    page.goto(app.get('espace_coop_hostname') + '/statistiques' + finUrl, { waitUntil: 'networkidle0' }),
-  ]);
+    try {
+      await browser.close();
+    } catch (error) {
+      app.get('sentry').captureException(error);
+      logger.error(error);
+    }
 
-  await page.waitForTimeout(500);
+    try {
+      res.contentType('application/pdf');
+      pdf.then(buffer => res.send(buffer));
+    } catch (error) {
+      app.get('sentry').captureException(error);
+      logger.error(error);
+    }
+  } catch (error) {
+    app.get('sentry').captureException(error);
+    logger.error(error);
+  }
 
-  let pdf;
-  await Promise.all([
-    page.addStyleTag({ content: '#burgerMenu { display: none} .no-print { display: none }' }),
-    pdf = page.pdf({ format: 'A4', printBackground: true })
-  ]);
-
-  await browser.close();
-  res.contentType('application/pdf');
-  pdf.then(buffer => res.send(buffer));
   return;
-
 };
 
 module.exports = { generatePdf };
