3885724396caa3d8109f29a8c6fd47e72f199bca
Loïc Guillois <contact@loicguillois.fr>
2021-10-27 11:02:28 +0200

    Mattermost: team hub numerique

--- src/tools/scripts/mattermost/createHubs.js
+++ src/tools/scripts/mattermost/createHubs.js
@@ -2,35 +2,58 @@
 'use strict';
 
 const { execute } = require('../../utils');
-const { loginAPI, createChannel, joinChannel, deleteArchivedChannels } = require('../../../utils/mattermost');
+const { loginAPI, createChannel, joinChannel, deleteArchivedChannels, joinTeam } = require('../../../utils/mattermost');
 const { findDepartement } = require('../../../utils/geo');
 
-execute(__filename, async ({ app, db, logger }) => {
+execute(__filename, async ({ app, db, logger, Sentry }) => {
   const mattermost = app.get('mattermost');
   const token = await loginAPI({ mattermost });
 
   const sleep = ms => new Promise(r => setTimeout(r, ms));
-  const hubs = require('../../../../data/imports/hubs.json');
+  /*const hubs = require('../../../../data/imports/hubs.json');
   await deleteArchivedChannels(mattermost, token);
   for (const hub of hubs) {
     const result = await createChannel(mattermost, token, hub.name);
     hub.channelId = result.data.id;
     await db.collection('hubs').insertOne(hub);
-  }
+  }*/
 
   let count = 0;
-  const conseillers = await db.collection('conseillers').find({ mattermost: { $ne: null } }).toArray();
+  const conseillers = await db.collection('conseillers').find({
+    'mattermost': { $ne: null },
+    'mattermost.error': { $ne: true },
+    'mattermost.hubJoined': { $ne: true }
+  }).toArray();
+
   for (const conseiller of conseillers) {
     const structure = await db.collection('structures').findOne({ _id: conseiller.structureId });
-    const regionName = findDepartement(structure.codeDepartement).region_name;
 
-    let hub = await db.collection('hubs').findOne({ region_names: { $elemMatch: { $eq: regionName } } });
-    if (hub === null) {
-      hub = await db.collection('hubs').findOne({ departements: { $elemMatch: { $eq: `${structure.codeDepartement}` } } });
-    }
-    if (hub !== null) {
-      count++;
-      joinChannel(mattermost, token, hub.channelId, conseiller.mattermost.id);
+    try {
+      const regionName = findDepartement(structure.codeDepartement).region_name;
+
+      let hub = await db.collection('hubs').findOne({ region_names: { $elemMatch: { $eq: regionName } } });
+
+      if (hub === null) {
+        hub = await db.collection('hubs').findOne({ departements: { $elemMatch: { $eq: `${structure.codeDepartement}` } } });
+      }
+    
+      if (hub !== null) {
+        try {
+          await joinTeam(mattermost, token, mattermost.hubTeamId, conseiller.mattermost.id);
+          await joinChannel(mattermost, token, hub.channelId, conseiller.mattermost.id);
+          db.collection('conseillers').updateOne({ _id: conseiller._id }, {
+            $set: { 'mattermost.hubJoined': true }
+          });
+          count++;
+        } catch (e) {
+          console.log(conseiller._id, hub);
+          Sentry.captureException(e);
+          logger.error(e);//.response.data
+        }
+      }
+    } catch (e) {
+      Sentry.captureException(e);
+      logger.error(e);
     }
 
     // To avoid overload Mattermost API
