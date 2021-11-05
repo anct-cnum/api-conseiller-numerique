8e33fa44463d3775d6d429f7d8e31c362be7959a
Ornella Ourfi <ornella93@outlook.fr>
2021-11-05 17:30:20 +0100

    script pour mettre à jour les misesEnRelation

--- src/tools/admin/structures/fix-statut-email-structure.js
+++ src/tools/admin/structures/fix-statut-email-structure.js
@@ -1,9 +1,11 @@
 const { execute } = require('../../utils');
+const { isEqual } = require('lodash');
 
 execute(__filename, async ({ db, logger, exit }) => {
   let promises = [];
   let countTotalStructure = 0;
   let countActionMAJ = 0;
+  let countNonMAJ = 0;
 
   const structures = await db.collection('structures').find().toArray();
 
@@ -11,25 +13,22 @@ execute(__filename, async ({ db, logger, exit }) => {
     promises.push(new Promise(async resolve => {
       const verifMisesEnRelation = await db.collection('misesEnRelation').findOne({ 'structure.$id': structure._id });
       if (verifMisesEnRelation !== null) {
-        if (verifMisesEnRelation?.structureObj?.contact !== undefined) {
-          // eslint-disable-next-line max-len
-          if ((verifMisesEnRelation.structureObj.statut !== structure?.statut) || (verifMisesEnRelation.structureObj.contact.email !== structure.contact.email)) {
-            await db.collection('misesEnRelation').updateMany(
-              { 'structure.$id': structure._id },
-              { $set: { 'structureObj': structure }
-              });
-            countActionMAJ++;
-          }
+        if (!isEqual(verifMisesEnRelation?.structureObj, structure)) {
+          await db.collection('misesEnRelation').updateMany(
+            { 'structure.$id': structure._id },
+            { $set: { 'structureObj': structure }
+            });
+          countNonMAJ++;
         }
+        countActionMAJ++;
       }
       countTotalStructure++;
       resolve();
     }));
   });
   await Promise.all(promises);
-
   // eslint-disable-next-line max-len
-  logger.info(`${countTotalStructure} structures ont été traitées, ${countActionMAJ} structures qui ont au moins une misesEnRelation et qui n'était pas à jour dans les misesEnRelations à été modifié`);
+  logger.info(`${countTotalStructure} structures ont été traitées, ${countActionMAJ} structures qui ont au moins une misesEnRelation && ${countNonMAJ} qui n'était pas à jour dans les misesEnRelations `);
 
   exit();
 });
