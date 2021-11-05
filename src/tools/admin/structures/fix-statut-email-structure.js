6dbfe2ec4892842fe8303e7a4903379bf5bf27e3
Ornella Ourfi <ornella93@outlook.fr>
2021-11-05 10:27:22 +0100

    mise à jour complete de l'objet structure pour etre sure que tout est bon dans les mises En Relation

--- src/tools/admin/structures/fix-statut-email-structure.js
+++ src/tools/admin/structures/fix-statut-email-structure.js
@@ -3,10 +3,7 @@ const { execute } = require('../../utils');
 execute(__filename, async ({ db, logger, exit }) => {
   let promises = [];
   let countTotalStructure = 0;
-  let countNonMAJStatut = 0;
-  let countOKMAJStatut = 0;
-  let countNonMAJEmail = 0;
-  let countOKMAJEmail = 0;
+  let countActionMAJ = 0;
 
   const structures = await db.collection('structures').find().toArray();
 
@@ -14,28 +11,11 @@ execute(__filename, async ({ db, logger, exit }) => {
     promises.push(new Promise(async resolve => {
       const verifMisesEnRelation = await db.collection('misesEnRelation').findOne({ 'structure.$id': structure._id });
       if (verifMisesEnRelation !== null) {
-        //Partie pour mettre à jour le statut
-        if (verifMisesEnRelation?.structureObj?.statut !== structure?.statut) {
-          await db.collection('misesEnRelation').updateMany(
-            { 'structure.$id': structure._id },
-            { $set: { 'structureObj.statut': structure.statut }
-            });
-          countNonMAJStatut++;
-        } else {
-          countOKMAJStatut++;
-        }
-        // Partie pour mettre à jour l'email de contact
-        if (verifMisesEnRelation?.structureObj?.contact !== undefined) {
-          if (verifMisesEnRelation?.structureObj?.contact.email !== structure.contact.email) {
-            await db.collection('misesEnRelation').updateMany(
-              { 'structure.$id': structure._id },
-              { $set: { 'structureObj.contact.email': structure.contact.email }
-              });
-            countNonMAJEmail++;
-          } else {
-            countOKMAJEmail++;
-          }
-        }
+        await db.collection('misesEnRelation').updateMany(
+          { 'structure.$id': structure._id },
+          { $set: { 'structureObj': structure }
+          });
+        countActionMAJ++;
       }
       countTotalStructure++;
       resolve();
@@ -43,9 +23,7 @@ execute(__filename, async ({ db, logger, exit }) => {
   });
   await Promise.all(promises);
 
-  // eslint-disable-next-line max-len
-  logger.info(`${countTotalStructure} structures ont été traités, ${countNonMAJStatut} structures ont été modifié et ${countOKMAJStatut} ont le meme statut dans la collection misesEnRelation`);
-  // eslint-disable-next-line max-len
-  logger.info(`${countTotalStructure}structures ont été traités, ${countNonMAJEmail} structures ont été modifié et ${countOKMAJEmail} ont le meme email de contact dans la collection misesEnRelation`);
+  logger.info(`${countTotalStructure} structures ont été traitées, ${countActionMAJ} structures qui ont au moins une misesEnRelation à été modifié`);
+
   exit();
 });
