851217cf5a4d53444548ec70401fa6dd9c3537dd
Ornella Ourfi <ornella93@outlook.fr>
2021-11-26 17:51:41 +0100

    debut de script pour envoyer l'email

---
+++ src/tools/admin/conseillers/change-email-pro.js
@@ -0,0 +1,67 @@
+const { program } = require('commander');
+const { execute } = require('../../utils');
+const slugify = require('slugify');
+const createEmails = require('../../../emails/emails');
+const createMailer = require('../../../mailer');
+
+execute(__filename, async ({ app, db, logger, Sentry, exit, gandi }) => {
+
+  program.option('-n, --nom <nom>', 'nom: nouveau nom');
+  program.option('-p, --prenom <prenom>', 'prenom: nouveau prenom');
+  program.option('-i, --id <id>', 'id: id PG du conseiller');
+  program.helpOption('-h', 'HELP command');
+  program.parse(process.argv);
+
+  const id = ~~program.id;
+  let nom = program.nom;
+  let prenom = program.prenom;
+  let login;
+  if (id === 0) {
+    exit('Paramètres invalides. Veuillez préciser un id et un nom ou prenom');
+    return;
+  }
+
+  const conseiller = await db.collection('conseillers').findOne({ idPG: id });
+  if (conseiller === null) {
+    exit('idPG inconnu, conseiller non trouvé');
+    return;
+  }
+  const compteUser = await db.collection('users').findOne({ 'entity.$id': conseiller._id });
+  if (compteUser === null) {
+    exit('idPG inconnu, conseiller non trouvé');
+    return;
+  }
+  const condition1 = prenom ? prenom : conseiller.prenom;
+  const condition2 = nom ? nom : conseiller.nom;
+  prenom = slugify(`${condition1}`, { replacement: '-', lower: true, strict: true });
+  nom = slugify(`${condition2}`, { replacement: '-', lower: true, strict: true });
+  login = `${prenom}.${nom}`;
+  console.log('login:', login);
+
+  conseiller.message_email = {
+    email_actuelle: conseiller.emailCN.address,
+    email_future: `${login}@${gandi.domain}`
+  };
+  conseiller.support_cnfs = {
+    login,
+    nouveauEmail: `${login}@${gandi.domain}`
+  };
+
+  try {
+    // Envoi email pour que l'utilisateur entre son mp lui meme !
+    let mailer = createMailer(app);
+    const emails = createEmails(db, mailer, app, logger);
+    let message = emails.getEmailMessageByTemplateName('conseillerChangeEmailCnfs');
+    await message.send(conseiller);
+    await db.collection('users').updateOne({ 'entity.$id': conseiller._id }, { $set: { 'support_cnfs': conseiller.support_cnfs } });
+
+  } catch (error) {
+    logger.error(error);
+    Sentry.captureException(error);
+    return;
+  }
+
+  // eslint-disable-next-line max-len
+  logger.info(`Envoi e-mail pour la demande de changement d'email professionnel : ${conseiller.emailCN.address} par => ${login}@${gandi.domain} pour le conseiller avec l'id ${id}`);
+  exit();
+});
