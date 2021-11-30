913696e7edbc8e0b9a60747e583af00b8133482a
Loïc Guillois <contact@loicguillois.fr>
2021-11-30 16:01:38 +0100

    scripts me remise à niveau des data côté mongodb + création des comptes pour ceux en erreur

---
+++ src/tools/scripts/mattermost/createMissingAccount.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+'use strict';
+
+const { execute } = require('../../utils');
+const { loginAPI, searchUser, createAccount } = require('../../../utils/mattermost');
+const slugify = require('slugify');
+const { v4: uuidv4 } = require('uuid');
+
+// On crée les comptes pour les 21 conseillers en erreurs
+// Ils pourront utiliser la fonction "mot de passe oublié" pour se changer leur mot de passe et se connecter
+execute(__filename, async ({ app, db, logger, Sentry }) => {
+  const mattermost = app.get('mattermost');
+  const token = await loginAPI({ mattermost });
+
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
+  let count = 0;
+  const conseillers = await db.collection('conseillers').find({
+    'statut': { $ne: 'RUPTURE' },
+    'mattermost.error': { $eq: true }
+  }).toArray();
+
+  for (const conseiller of conseillers) {
+    try {
+      const result = await searchUser(mattermost, token, conseiller);
+      if (result.data.length === 0) {
+        const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
+        const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
+
+        const login = `${prenom}.${nom}`;
+        const gandi = app.get('gandi');
+        const email = `${login}@${gandi.domain}`;
+        const password = uuidv4() + 'AZEdsf;+:';
+
+        //console.log(login);
+
+        createAccount({ mattermost, conseiller, email, login, password, db, logger, Sentry });
+
+        count++;
+      }
+    } catch (e) {
+      Sentry.captureException(e);
+      logger.error(e);
+    }
+
+    // To avoid overload Mattermost API
+    await sleep(500);
+  }
+
+  logger.info(`[MATTERMOST] ${count} comptes conseillers créés`);
+});
