7ef792c60592a740804502e2a13f69c173c9fe6f
Ornella Ourfi <ornella93@outlook.fr>
2021-12-14 20:05:41 +0100

    rectif syntaxe du script qui est maintenant corrigé (56 mailbox créé)

--- src/tools/scripts/fixCreationBoiteMail.js
+++ src/tools/scripts/fixCreationBoiteMail.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 'use strict';
 
-require('dotenv').config();
-
 const { execute } = require('../utils');
 const { getMailBox, createMailbox } = require('../../utils/mailbox');
 const { v4: uuidv4 } = require('uuid');
@@ -23,43 +21,42 @@ execute(__filename, async ({ logger, db, gandi, Sentry }) => {
   let errorEmailBoxGandi = 0;
   let idPGErrorEmailBoxGandi = [];
   let idPGSuccessEmailBoxGandi = [];
-  const conseillers = await db.collection('conseillers').find({ emailCNError: { $exists: true }, statut: { $ne: 'RUPTURE' } }).limit(2).toArray();
-  let promises = [];
+  const conseillers = await db.collection('conseillers').find({ emailCNError: { $exists: true }, statut: { $ne: 'RUPTURE' } }).toArray();
 
   logger.info('Fix des boites mail gandi non crée...');
-  conseillers.forEach(conseiller => {
-    promises.push(new Promise(async (resolve, reject) => {
-      const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
-      const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
-      let login = conseiller?.emailCN?.address ? conseiller?.emailCN?.address.substring(0, conseiller?.emailCN?.address.lastIndexOf('@')) : `${prenom}.${nom}`;
-      const conseillerId = conseiller._id;
-      const password = uuidv4() + 'AZEdsf;+:';
-      const { data } = await getMailBox({ gandi, login });
+  const sleep = ms => new Promise(r => setTimeout(r, ms));
 
-      if (data.length === 0) {
-        createMailbox({ gandi, db, logger, Sentry })({ conseillerId, login, password }).then(async result => {
-          if (result) {
-            fixMailBoxGandi++;
-            idPGSuccessEmailBoxGandi.push(conseiller.idPG);
-            return miseAjourIdentifiant(db, gandi)(conseillerId, login);
-          } else {
-            idPGErrorEmailBoxGandi.push(conseiller.idPG);
-            errorEmailBoxGandi++;
-          }
-        }).catch(error => {
-          logger.error(error);
-          Sentry.captureException(error);
-          reject(error);
-        });
-      } else {
-        okEmailBoxGandi++;
-      }
-      resolve();
-    }));
-  });
-  await Promise.all(promises);
+  for (const conseiller of conseillers) {
+    const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
+    const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
+    let login = conseiller?.emailCN?.address ? conseiller?.emailCN?.address.substring(0, conseiller?.emailCN?.address.lastIndexOf('@')) : `${prenom}.${nom}`;
+    const conseillerId = conseiller._id;
+    const password = uuidv4() + 'AZEdsf;+:';
+    const { data } = await getMailBox({ gandi, login });
+
+    if (data.length === 0) {
+      // eslint-disable-next-line no-loop-func
+      await createMailbox({ gandi, db, logger, Sentry })({ conseillerId, login, password }).then(result => {
+        if (result) {
+          fixMailBoxGandi++;
+          idPGSuccessEmailBoxGandi.push(conseiller.idPG);
+          logger.info(`mise à jour email gandi (script) ${conseiller.idPG}`);
+          return miseAjourIdentifiant(db, gandi)(conseillerId, login);
+        } else {
+          idPGErrorEmailBoxGandi.push(conseiller.idPG);
+          errorEmailBoxGandi++;
+        }
+      }).catch(error => {
+        logger.error(error);
+        Sentry.captureException(error);
+      });
+    } else {
+      okEmailBoxGandi++;
+    }
+
+    await sleep(500);
+  }
   // eslint-disable-next-line max-len
-
   logger.info(`${fixMailBoxGandi} boites mails corrigées, ${okEmailBoxGandi} déjà OK et ${errorEmailBoxGandi} en erreur`);
   logger.info(`Détails pour les ${fixMailBoxGandi} conseiller(s) corrigé(s) => [${idPGSuccessEmailBoxGandi}]`);
   logger.info(`Détails pour les ${errorEmailBoxGandi} conseiller(s) en erreur(s) => [${idPGErrorEmailBoxGandi}]`);
