73ae1732afe1858c3541326ae0850c4a5b7ff3fa
Ornella Ourfi <ornella93@outlook.fr>
2021-12-22 14:43:41 +0100

    ajout d'un script pour remettre à zéro les erreurs de mot de passe oublier

---
+++ src/tools/scripts/majErrorResetPassword.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+'use strict';
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ logger, db }) => {
+  const conseillers = await db.collection('conseillers').find({ 'mattermost.errorResetPassword': true }).toArray();
+  let promises = [];
+  let count = 0;
+
+  logger.info('Mettre tout les mattermost.errorResetPassword à false...');
+  conseillers.forEach(conseiller => {
+    promises.push(new Promise(async resolve => {
+      await db.collection('conseillers').updateOne({ _id: conseiller._id }, { $set: { 'mattermost.errorResetPassword': false } });
+      logger.info(`Le conseiller avec l'idPG: ${conseiller.idPG} a eut un 'mattermost.errorResetPassword' à true`);
+      count++;
+      resolve();
+    }));
+  });
+  await Promise.all(promises);
+  logger.info(`${count} remis à false pour 'mattermost.errorResetPassword'`);
+});
