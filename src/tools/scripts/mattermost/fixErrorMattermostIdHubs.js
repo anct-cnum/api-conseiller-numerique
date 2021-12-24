399b6f3be1138e5a118b9604c26386b3fdffa098
Ornella Ourfi <ornella93@outlook.fr>
2021-12-24 14:43:11 +0100

    correction directement dans script + appel de la fonction joinTeam et joinChanel + un count pour compter les erreurs

--- src/tools/scripts/mattermost/fixErrorMattermostIdHubs.js
+++ src/tools/scripts/mattermost/fixErrorMattermostIdHubs.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 'use strict';
 
+const departements = require('./../../../../data/imports/departements-region.json');
 const { execute } = require('../../utils');
-const { loginAPI, joinFixTeam } = require('../../../utils/mattermost');
+const { loginAPI, joinTeam, joinChannel } = require('../../../utils/mattermost');
+const slugify = require('slugify');
+const { findDepartement } = require('../../../utils/geo');
+const axios = require('axios');
 
 execute(__filename, async ({ app, db, logger, Sentry }) => {
   const mattermost = app.get('mattermost');
@@ -10,6 +14,7 @@ execute(__filename, async ({ app, db, logger, Sentry }) => {
 
   const sleep = ms => new Promise(r => setTimeout(r, ms));
   let count = 0;
+  let countError = 0;
   const conseillers = await db.collection('conseillers').find({
     'mattermost.error': true,
     'mattermost.errorMessage': { $exists: true }
@@ -17,22 +22,70 @@ execute(__filename, async ({ app, db, logger, Sentry }) => {
 
   for (const conseiller of conseillers) {
     try {
-      const result = await joinFixTeam(db, logger, Sentry, mattermost, token)(conseiller);
-      if (result) {
-        db.collection('conseillers').updateOne({ _id: conseiller._id }, {
-          $set: {
-            'mattermost.error': false
+      slugify.extend({ '-': ' ' });
+      slugify.extend({ '\'': ' ' });
+      const departement = departements.find(d => `${d.num_dep}` === conseiller.codeDepartement);
+      const channelName = slugify(departement.dep_name, { replacement: '', lower: true });
+
+      const resultChannel = await axios({
+        method: 'get',
+        url: `${mattermost.endPoint}/api/v4/teams/${mattermost.teamId}/channels/name/${channelName}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      logger.info(resultChannel);
+      const idTeam = mattermost.teamId;
+      const idUser = conseiller.mattermost.id;
+
+      const resultJoinTeam = await joinTeam(mattermost, token, idTeam, idUser);
+      logger.info(resultJoinTeam);
+
+      [resultChannel.data.id, mattermost.themeChannelId, mattermost.resourcesChannelId].forEach(async canalId => {
+        const resultJoinChannel = await axios({
+          method: 'post',
+          url: `${mattermost.endPoint}/api/v4/channels/${canalId}/members`,
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
           },
-          $unset: {
-            'mattermost.errorMessage': ''
+          data: {
+            'user_id': idUser,
           }
         });
-        count++;
-        logger.info(`Conseiller id=${conseiller._id} avec un id mattermost: ${conseiller.mattermost.id} est corrigé par le script fixErrorMattermostIdHubs.js`);
+        logger.info(resultJoinChannel);
+      });
+
+      const structure = await db.collection('structures').findOne({ _id: conseiller.structureId });
+      const regionName = findDepartement(structure.codeDepartement).region_name;
+
+      let hub = await db.collection('hubs').findOne({ region_names: { $elemMatch: { $eq: regionName } } });
+      if (hub === null) {
+        hub = await db.collection('hubs').findOne({ departements: { $elemMatch: { $eq: `${structure.codeDepartement}` } } });
       }
+      if (hub !== null) {
+        joinChannel(mattermost, token, hub.channelId, idUser);
+      }
+      await db.collection('conseillers').updateOne({ _id: conseiller._id }, {
+        $set: {
+          'mattermost.error': false,
+          'mattermost.errorFix': false
+        },
+        $unset: {
+          'mattermost.errorMessage': '',
+          'mattermost.errorMessageFix': ''
+        }
+      });
+      count++;
+      logger.info(`Conseiller id=${conseiller._id} avec un id mattermost: ${idUser} est corrigé par le script fixErrorMattermostIdHubs.js`);
     } catch (e) {
       Sentry.captureException(e);
       logger.error(e);
+      await db.collection('conseillers').updateOne({ _id: conseiller._id },
+        { $set:
+          { 'mattermost.errorFix': true, 'mattermost.errorMessageFix': e.message }
+        });
     }
     // To avoid overload Mattermost API
     await sleep(500);
@@ -41,5 +94,5 @@ execute(__filename, async ({ app, db, logger, Sentry }) => {
   // eslint-disable-next-line max-len
   await db.collection('conseillers').updateMany({ 'mattermost.error': false, 'mattermost.errorMessage': '' }, { $unset: { 'mattermost.errorMessage': '' } });
 
-  logger.info(`[MATTERMOST] ${count} conseillers corrigés`);
+  logger.info(`[MATTERMOST] ${count} conseillers corrigés et ${countError} en erreur(s)`);
 });
