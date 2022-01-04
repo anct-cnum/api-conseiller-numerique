e1c055744c1df929e1492a4e1b9d29ef1454667b
Marc Gavanier <marc.gavanier@gmail.com>
2022-01-04 20:39:08 +0100

    move formatAddress helper in dedicated file

--- src/services/conseillers/geolocalisation/core/geolocalisation.core.js
+++ src/services/conseillers/geolocalisation/core/geolocalisation.core.js
@@ -1,22 +1,4 @@
-const addressGroupSeparator = ' ';
-const addressPartSeparator = ', ';
-
-const isValidAddressPart = addressPart => addressPart !== undefined && addressPart !== null && addressPart.trim() !== '';
-
-const addressGroup = addressParts => addressParts.filter(isValidAddressPart).join(addressGroupSeparator);
-
-const formatAddress = adresse => [
-  addressGroup([
-    adresse.numero_voie,
-    adresse.type_voie,
-    adresse.nom_voie
-  ]),
-  adresse.complement_adresse,
-  addressGroup([
-    adresse.code_postal,
-    adresse.localite
-  ]),
-].filter(isValidAddressPart).join(addressPartSeparator);
+const { formatAddress } = require('../../common/format-address');
 
 const formatStructure = structure => ({
   name: structure.nom,
