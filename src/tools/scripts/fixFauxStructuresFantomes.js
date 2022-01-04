501ac1405ce46d969bdbd455997ed8d39afa6d63
Ornella Ourfi <ornella93@outlook.fr>
2022-01-04 15:50:23 +0100

    script pour corriger les faux 'jamais connecté'

---
+++ src/tools/scripts/fixFauxStructuresFantomes.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+'use strict';
+
+require('dotenv').config();
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ db, logger, exit, Sentry }) => {
+  logger.info('Correction des structures "faux fantôme"');
+
+  let promises = [];
+  let countCorrection = 0;
+  let countOk = 0;
+  let countError = 0;
+
+  await db.collection('users').find(
+    { roles: { $in: ['structure'] }, passwordCreated: false }
+  ).forEach(async user => {
+    promises.push(new Promise(async resolve => {
+      try {
+        const nombreMiseEnRelation = await db.collection('misesEnRelation').countDocuments({
+          'structure.$id': user.entity.oid,
+          'statut': { $in: ['interessee', 'recrutee', 'finalisee']
+          } }
+        );
+        if (nombreMiseEnRelation) {
+          await db.collection('users').updateOne(
+            { _id: user._id },
+            { $set: { passwordCreated: true } }
+          );
+          logger.info(`La structure id=${user.entity.oid} est considéré comme "faux fantôme"`);
+          countCorrection++;
+        } else {
+          countOk++;
+        }
+      } catch (error) {
+        logger.error(error);
+        Sentry.captureException(error);
+        countError++;
+      }
+      resolve();
+    }));
+  });
+  await Promise.all(promises);
+
+  logger.info(`${countCorrection} structures faux fantomes corrigé(s) et ${countOk} structures en "vrai fantomes" et ${countError} en erreur`);
+  exit();
+});
