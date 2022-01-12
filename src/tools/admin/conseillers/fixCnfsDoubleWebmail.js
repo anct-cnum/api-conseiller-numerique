22ec4fb720e54755e0065242690586351a7397bb
Ornella Ourfi <ornella93@outlook.fr>
2022-01-12 12:24:38 +0100

    script pour mettre à jour coop mattermost et la suppression de l'email qu'on ne garde pas

--- src/tools/admin/conseillers/fixCnfsDoubleWebmail.js
+++ src/tools/admin/conseillers/fixCnfsDoubleWebmail.js
@@ -1,32 +1,77 @@
 const { program } = require('commander');
 const { execute } = require('../../utils');
-const { deleteMailbox } = require('../../../utils/mailbox');
-const inquirer = require('inquirer');
+const { loginAPI } = require('../../../utils/mattermost');
 const slugify = require('slugify');
+const axios = require('axios');
 
-const updateEmailCN = (db, gandi) => async (answer, conseiller, id) => {
-  await db.collection('conseillers').updateOne({ idPG: id }, { $set: { 'emailCN.address': `${answer.choix_login_webmail}@${gandi.domain}` } });
-  await db.collection('users').updateOne({ idPG: id }, { $set: { 'name': `${answer.choix_login_webmail}@${gandi.domain}` } });
+const updateEmailCN = (db, gandi) => async (loginOk, conseiller, id) => {
+  await db.collection('conseillers').updateOne({ idPG: id }, { $set: { 'emailCN.address': `${loginOk}@${gandi.domain}` } });
+  await db.collection('users').updateOne({ 'entity.$id': conseiller._id }, { $set: { 'name': `${loginOk}@${gandi.domain}` } });
   await db.collection('misesEnRelation').updateMany(
     { 'conseiller.$id': conseiller._id },
-    { $set: { 'conseillerObj.emailCN.address': `${answer.choix_login_webmail}@${gandi.domain}` }
+    { $set: { 'conseillerObj.emailCN.address': `${loginOk}@${gandi.domain}` }
+    });
+};
+const updateLoginMattermost = (mattermost, gandi, db) => async (conseiller, loginOk) => {
+  const token = await loginAPI({ mattermost });
+  await axios({
+    method: 'put',
+    url: `${mattermost.endPoint}/api/v4/users/${conseiller.mattermost?.id}/patch`,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    data: {
+      'username': loginOk,
+      'email': `${loginOk}@${gandi.domain}`
+    }
+  });
+  await db.collection('conseillers').updateOne({ _id: conseiller._id }, { $set: { 'mattermost.login': loginOk } });
+  await db.collection('misesEnRelation').updateMany(
+    { 'conseiller.$id': conseiller._id },
+    { $set: { 'conseillerObj.mattermost.login': loginOk }
     });
 };
 
-execute(__filename, async ({ db, logger, Sentry, exit, gandi }) => {
+const deleteMailBoxDoublon = (gandi, logger) => async loginSupprime => {
+  const mailbox = await axios({
+    method: 'get',
+    url: `${gandi.endPoint}/mailboxes/${gandi.domain}?login=${loginSupprime}`,
+    headers: {
+      'Authorization': `Apikey ${gandi.token}`
+    }
+  });
+  if (mailbox?.data.length === 1) {
+    const resultDeleteMailbox = await axios({
+      method: 'delete',
+      url: `${gandi.endPoint}/mailboxes/${gandi.domain}/${mailbox.data[0].id}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Apikey ${gandi.token}`
+      }
+    });
+    logger.info(`le statut pour la suppression de ${loginSupprime}@${gandi.domain} est ${resultDeleteMailbox.status}`);
+  }
+};
+
+execute(__filename, async ({ db, logger, Sentry, exit, gandi, mattermost }) => {
 
   program.option('-i, --id <id>', 'id: id PG du conseiller');
+  program.option('-l, --login <login>', 'login: lequel à conserver, il faut choisir entre nouveau(-) ou ancien(.)');
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
+
   let id = ~~program.id;
-  if (id === 0 || !id) {
-    exit('Paramètres invalides. Veuillez préciser un id');
+  const login = program.login;
+
+  if (id === 0 || !id || !['ancien', 'nouveau'].includes(login)) {
+    exit('Paramètres invalides. Veuillez préciser un id et un login en choisissant ancien ou login');
     return;
   }
+  const conseiller = await db.collection('conseillers').findOne({ idPG: id });
+
   const ancien = value => slugify(`${value}`, { replacement: '.', lower: true, strict: true });
   const nouveau = value => slugify(`${value}`, { replacement: '-', lower: true, strict: true });
-
-  const conseiller = await db.collection('conseillers').findOne({ idPG: id });
 
   let prenomOption1 = nouveau(conseiller.prenom);
   let nomOption1 = nouveau(conseiller.nom);
@@ -34,34 +79,18 @@ execute(__filename, async ({ db, logger, Sentry, exit, gandi }) => {
   let nomOption2 = ancien(conseiller.nom);
   const loginOption1 = `${prenomOption1}.${nomOption1}`;
   const loginOption2 = `${prenomOption2}.${nomOption2}`;
+  const loginSupprime = login === 'nouveau' ? loginOption2 : loginOption1;
+  const loginOk = login === 'ancien' ? loginOption2 : loginOption1;
 
-  inquirer
-  .prompt([
-    {
-      name: 'choix_login_webmail',
-      type: 'list',
-      message: 'Quel webmail/login voulez vous conservez ?',
-      choices: [`${loginOption1}`, `${loginOption2}`]
-    },
-    {
-      name: 'confirm_answer',
-      type: 'confirm',
-      message: 'Etes vous sure d\'avoir bien choisi le bon à conserver ?'
-    },
-  ])
-  .then(async answer => {
-    if (answer.confirm_answer) {
-      const loginSupprime = answer.choix_login_webmail === loginOption1 ? loginOption2 : loginOption1;
-      logger.info(`On conserve ${answer.choix_login_webmail}${gandi.domain} et on supprime ${loginSupprime} ...`);
-      await deleteMailbox(gandi, db, logger, Sentry)(conseiller._id, answer.choix_login_webmail);
-      if (`${answer.choix_login_webmail}@${gandi.domain}` !== conseiller.emailCN.address) {
-        await updateEmailCN(db, gandi)(answer, conseiller, id);
-      }
-    }
-    exit();
-  }).catch(error => {
+  try {
+    logger.info(`On conserve ${loginOk}@${gandi.domain} et on supprime ${loginSupprime}@${gandi.domain} ...`);
+    await deleteMailBoxDoublon(gandi, logger)(loginSupprime);
+    await updateEmailCN(db, gandi)(loginOk, conseiller, id);
+    await updateLoginMattermost(mattermost, gandi, db)(conseiller, loginOk);
+  } catch (error) {
     logger.error(error);
     Sentry.captureException(error);
-    exit();
-  });
+  }
+  logger.info(`Le login est bien => ${loginOk}@${gandi.domain} pour le conseiller id=${conseiller._id}`);
+  exit();
 });
