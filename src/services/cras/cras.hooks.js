8a6a9176ee19704a3f842906c022cc1f2b447ce9
Ornella Ourfi <ornella93@outlook.fr>
2022-02-02 09:24:36 +0100

    ajout de la validation pour 'organisme'

--- src/services/cras/cras.hooks.js
+++ src/services/cras/cras.hooks.js
@@ -67,8 +67,8 @@ module.exports = {
           // eslint-disable-next-line max-len
           themes: Joi.array().required().min(1).max(13).items(Joi.string().required().valid('equipement informatique', 'internet', 'courriel', 'smartphone', 'contenus numeriques', 'vocabulaire', 'traitement texte', 'echanger', 'trouver emploi', 'accompagner enfant', 'tpe/pme', 'demarche en ligne', 'autre')).error(new Error('Le thème est invalide')),
           duree: Joi.any().required().error(new Error('La durée est invalide')),
-          accompagnement: Joi.string().required().valid('individuel', 'atelier', 'redirection').allow(null).error(new Error('L\'accompagnement est invalide'))
-
+          accompagnement: Joi.string().required().valid('individuel', 'atelier', 'redirection').allow(null).error(new Error('L\'accompagnement est invalide')),
+          organisme: Joi.string().required().allow(null).error(new Error('L\'accompagnement est invalide'))
         }).validate(context.data.cra);
 
         if (schema.error) {
