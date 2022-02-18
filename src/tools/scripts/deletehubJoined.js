72d1d91f934a9d2e56cb5ea2c028724da248d1a9
Ornella Ourfi <ornella93@outlook.fr>
2022-02-18 15:49:52 +0100

    rectif nommage constante

--- src/tools/scripts/deletehubJoined.js
+++ src/tools/scripts/deletehubJoined.js
@@ -4,13 +4,13 @@
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const countConseillerAfter = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
-  logger.info(`${countConseillerAfter} conseiller qui a hubJoined à true dans mongo (avant updateMany)`);
+  const countConseillerBefore = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
+  logger.info(`${countConseillerBefore} conseiller qui a hubJoined à true dans mongo (avant updateMany)`);
 
   await db.collection('conseillers').updateMany({ 'mattermost.hubJoined': true }, { $unset: { 'mattermost.hubJoined': true } });
   logger.info('suppression de tout les clé mattermost..hubJoined: true ');
 
-  const countConseillerBefore = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
-  logger.info(`${countConseillerBefore} conseiller qui a hubJoined à true dans mongo (Après updateMany)`);
+  const countConseillerAfter = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
+  logger.info(`${countConseillerAfter} conseiller qui a hubJoined à true dans mongo (Après updateMany)`);
 
 });
