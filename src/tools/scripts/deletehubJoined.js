f25ee6f56631ce53583648a2ae84b165f08f929d
Ornella Ourfi <ornella93@outlook.fr>
2022-02-18 13:32:45 +0100

    petit script pour unset les hubJoined: true

---
+++ src/tools/scripts/deletehubJoined.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict';
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ logger, db }) => {
+  const countConseillerAfter = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
+  logger.info(`${countConseillerAfter} conseiller qui a hubJoined à true dans mongo (avant updateMany)`);
+
+  await db.collection('conseillers').updateMany({ 'mattermost.hubJoined': true }, { $unset: { 'mattermost.hubJoined': true } });
+  logger.info('suppression de tout les clé mattermost..hubJoined: true ');
+
+  const countConseillerBefore = await db.collection('conseillers').countDocuments({ 'mattermost.hubJoined': true });
+  logger.info(`${countConseillerBefore} conseiller qui a hubJoined à true dans mongo (Après updateMany)`);
+
+});
