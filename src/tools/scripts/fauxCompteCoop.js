cb0cc42b4a2c994f369ff216937fbe039e19b8d4
Ornella Ourfi <ornella93@outlook.fr>
2022-02-22 12:25:52 +0100

    ajout option pour faire uniquement l'export + option pour la correction

--- src/tools/scripts/fauxCompteCoop.js
+++ src/tools/scripts/fauxCompteCoop.js
@@ -8,8 +8,14 @@ const slugify = require('slugify');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
 const fs = require('fs');
+const { program } = require('commander');
 
 execute(__filename, async ({ logger, db, app, Sentry }) => {
+  program.option('-c, --csv ', 'export des conseillers qui ont un faux compte activer en csv');
+  program.option('-f, --fix ', 'correction des faux comptes activé');
+  program.helpOption('-e', 'HELP command');
+  program.parse(process.argv);
+  const { csv, fix } = program;
   let countConseiller = 0;
   let countNotGandi = 0;
   let countNotMattermost = 0;
@@ -18,14 +24,14 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
   const token = await loginAPI({ mattermost });
   const sleep = ms => new Promise(r => setTimeout(r, ms));
   const passwordcreatedTrue = await db.collection('users').find({ 'roles': { $in: ['conseiller'] }, 'passwordCreated': true }).toArray();
-
-  logger.info(`Generating CSV file...`);
   let csvFile = path.join(__dirname, '../../../data/exports', 'faux_compte_coop_activer.csv');
-
   let file = fs.createWriteStream(csvFile, {
     flags: 'w'
   });
-  file.write('Nom; Prénom; Email Personnelle des conseillers\n');
+  if (csv) {
+    logger.info(`Generating CSV file...`);
+    file.write('Nom; Prénom; Email Personnelle des conseillers\n');
+  }
 
   for (const cnfs of passwordcreatedTrue) {
     try {
@@ -36,29 +42,33 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
         emailCN: { $exists: false }
       });
       if (conseiller) {
-        file.write(`${conseiller?.nom};${conseiller?.prenom};${conseiller?.email}\n`);
-        const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
-        const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
-        const login = `${prenom}.${nom}`;
-        const password = uuidv4() + 'AZEdsf;+:';
-        const conseillerId = conseiller._id;
-        const email = `${login}@${gandi.domain}`;
-        const { data } = await getMailBox({ gandi, login });
-        if (data.length === 0) {
-          await createMailbox({ gandi, db, logger, Sentry })({ conseillerId, login, password });
-          await db.collection('users').updateOne({ 'entity.$id': conseillerId },
-            { $set:
-              { name: `${login}@${gandi.domain}` }
-            });
-          countNotGandi++;
+        if (csv) {
+          file.write(`${conseiller?.nom};${conseiller?.prenom};${conseiller?.email}\n`);
         }
-        const result = await searchUser(mattermost, token, conseiller);
-        if (result.data.length === 0) {
-          await createAccount({ mattermost, conseiller, email, login, nom, prenom, password, db, logger, Sentry });
-          countNotMattermost++;
+        if (fix) {
+          const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
+          const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
+          const login = `${prenom}.${nom}`;
+          const password = uuidv4() + 'AZEdsf;+:';
+          const conseillerId = conseiller._id;
+          const email = `${login}@${gandi.domain}`;
+          const { data } = await getMailBox({ gandi, login });
+          if (data.length === 0) {
+            await createMailbox({ gandi, db, logger, Sentry })({ conseillerId, login, password });
+            await db.collection('users').updateOne({ 'entity.$id': conseillerId },
+              { $set:
+                  { name: `${login}@${gandi.domain}` }
+              });
+            countNotGandi++;
+          }
+          const result = await searchUser(mattermost, token, conseiller);
+          if (result.data.length === 0) {
+            await createAccount({ mattermost, conseiller, email, login, nom, prenom, password, db, logger, Sentry });
+            countNotMattermost++;
+          }
+          countConseiller++;
+          logger.info(`Conseiller ${conseiller.idPG} corrigé par le script de "FauxCompteCoop.js"`);
         }
-        countConseiller++;
-        logger.info(`Conseiller ${conseiller.idPG} corrigé par le script de "FauxCompteCoop.js"`);
       }
     } catch (e) {
       Sentry.captureException(e);
@@ -71,5 +81,7 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
   logger.info(`${countConseiller} conseillers qui n'ont pas activer leurs espace via l'email d'invit COOP`);
   logger.info(`${countNotGandi} email @conseiller-numerique.fr corrigé(s)`);
   logger.info(`${countNotMattermost} compte mattermost corrigé(s)`);
-  file.close();
+  if (csv) {
+    file.close();
+  }
 });
