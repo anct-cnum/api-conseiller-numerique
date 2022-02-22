0b38d8674c468632c93be1c59c12006a78851f05
Ornella Ourfi <ornella93@outlook.fr>
2022-02-22 11:51:06 +0100

    ajout de la partie export des conseillers qui seront corrigé(s)

--- src/tools/scripts/fauxCompteCoop.js
+++ src/tools/scripts/fauxCompteCoop.js
@@ -6,6 +6,8 @@ const { createMailbox, getMailBox } = require('../../utils/mailbox');
 const { createAccount, loginAPI, searchUser } = require('../../utils/mattermost');
 const slugify = require('slugify');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const fs = require('fs');
 
 execute(__filename, async ({ logger, db, app, Sentry }) => {
   let countConseiller = 0;
@@ -17,20 +19,34 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
   const sleep = ms => new Promise(r => setTimeout(r, ms));
   const passwordcreatedTrue = await db.collection('users').find({ 'roles': { $in: ['conseiller'] }, 'passwordCreated': true }).toArray();
 
+  logger.info(`Generating CSV file...`);
+  let csvFile = path.join(__dirname, '../../../data/exports', 'faux_compte_coop_activer.csv');
+
+  let file = fs.createWriteStream(csvFile, {
+    flags: 'w'
+  });
+  file.write('Nom; Prénom; Email Personnelle des conseillers\n');
+
   for (const cnfs of passwordcreatedTrue) {
     try {
-      const conseiller = await db.collection('conseillers').findOne({ _id: cnfs.entity.oid });
-      if (!cnfs.mattermost && !cnfs.emailCN) {
+      const conseiller = await db.collection('conseillers').findOne({
+        _id: cnfs.entity.oid,
+        statut: 'RECRUTE',
+        mattermost: { $exists: false },
+        emailCN: { $exists: false }
+      });
+      if (conseiller) {
+        file.write(`${conseiller?.nom};${conseiller?.prenom};${conseiller?.email}\n`);
         const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
         const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
         const login = `${prenom}.${nom}`;
         const password = uuidv4() + 'AZEdsf;+:';
         const conseillerId = conseiller._id;
-        const email = `${login}@conseiller-numerique.fr`;
+        const email = `${login}@${gandi.domain}`;
         const { data } = await getMailBox({ gandi, login });
         if (data.length === 0) {
           await createMailbox({ gandi, db, logger, Sentry })({ conseillerId, login, password });
-          await db.collection('users').updateOne({ _id: conseillerId },
+          await db.collection('users').updateOne({ 'entity.$id': conseillerId },
             { $set:
               { name: `${login}@${gandi.domain}` }
             });
@@ -42,6 +58,7 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
           countNotMattermost++;
         }
         countConseiller++;
+        logger.info(`Conseiller ${conseiller.idPG} corrigé par le script de "FauxCompteCoop.js"`);
       }
     } catch (e) {
       Sentry.captureException(e);
@@ -51,8 +68,8 @@ execute(__filename, async ({ logger, db, app, Sentry }) => {
     // To avoid overload Gandi API
     await sleep(500);
   }
-  logger.info(`${countConseiller} conseillers qui n'ont pas activer leurs eespace via l'email d'invit COOP`);
+  logger.info(`${countConseiller} conseillers qui n'ont pas activer leurs espace via l'email d'invit COOP`);
   logger.info(`${countNotGandi} email @conseiller-numerique.fr corrigé(s)`);
-  logger.info(`${countNotMattermost} email @conseiller-numerique.fr corrigé(s)`);
-
+  logger.info(`${countNotMattermost} compte mattermost corrigé(s)`);
+  file.close();
 });
