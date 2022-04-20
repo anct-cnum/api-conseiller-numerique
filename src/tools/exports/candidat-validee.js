8056f1a71c5a8679c7c7499540b68093ff43a709
Ornella Ourfi <ornella93@outlook.fr>
2022-04-20 12:41:27 +0200

    ajout de l'info prenom et nom

--- src/tools/exports/candidat-validee.js
+++ src/tools/exports/candidat-validee.js
@@ -32,13 +32,13 @@ execute(__filename, async ({ logger, db, exit }) => {
   });
 
   // eslint-disable-next-line max-len
-  file.write('id du conseiller; Email perso du candidat; date de recrutement prévisionelle; id de la structure; Nom de la structure; SIRET; email de contact\n');
+  file.write('id du conseiller; Nom; Prenom; Email perso du candidat; date de recrutement prévisionelle; id de la structure; Nom de la structure; SIRET; email de contact\n');
   candidatValidee.forEach(relation => {
     promises.push(new Promise(async resolve => {
       const conseiller = await db.collection('conseillers').findOne({ _id: relation.conseiller.oid });
       const structure = await db.collection('structures').findOne({ _id: relation.structure.oid });
       // eslint-disable-next-line max-len
-      file.write(`${conseiller.idPG};${conseiller.email};${dayjs(relation?.dateRecrutement).format('DD/MM/YYYY') ?? 'non renseigné'};${structure.idPG};${structure.nom};${structure?.siret ?? 'non renseigné'};${structure.contact.email}\n`);
+      file.write(`${conseiller.idPG};${conseiller.nom};${conseiller.prenom};${conseiller.email};${dayjs(relation?.dateRecrutement).format('DD/MM/YYYY') ?? 'non renseigné'};${structure.idPG};${structure.nom};${structure?.siret ?? 'non renseigné'};${structure.contact.email}\n`);
       resolve();
     }));
   });
