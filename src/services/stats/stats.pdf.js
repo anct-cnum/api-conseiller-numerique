e2fc1e0f05c150aadc1cdca2ae012fd2e1a84360
Ornella Ourfi <ornella93@outlook.fr>
2022-05-02 17:36:53 +0200

    fix

--- src/services/stats/stats.pdf.js
+++ src/services/stats/stats.pdf.js
@@ -6,67 +6,59 @@ const generatePdf = async (app, res, logger, accessToken, user, finUrl = null) =
 
   let browser = null;
 
-  try {
-    if (app.get('espace_coop_hostname') === 'http://localhost:3000') {
-      //fonctionnement en local
-      browser = await puppeteer.launch({
-        executablePath: app.get('puppeteer_browser')
-      });
-    } else {
-      //fonctionnement sur les autres environnements
-      const browserURL = app.get('browser_wss_link') + '?token=' + app.get('browser_wss_token');
-      browser = await puppeteer.connect({
-        browserWSEndpoint: browserURL,
-      });
-    }
-  } catch (error) {
-    app.get('sentry').captureException(error);
-    logger.error(error);
-    logger.error(`Error pdf temporaire (1 error) : ${error}`);
-    logger.error(`Error pdf temporaire (1 browser) : ${browser}`);
+  if (app.get('espace_coop_hostname') === 'http://localhost:3000') {
+    //fonctionnement en local
+    browser = await puppeteer.launch({
+      executablePath: app.get('puppeteer_browser')
+    });
+  } else {
+    //fonctionnement sur les autres environnements
+    const browserURL = app.get('browser_wss_link') + '?token=' + app.get('browser_wss_token');
+    browser = await puppeteer.connect({
+      browserWSEndpoint: browserURL,
+    });
   }
-
-  try {
-    browser.on('targetchanged', async target => {
-      const targetPage = await target.page();
-      const client = await targetPage.target().createCDPSession();
-      await client.send('Runtime.evaluate', {
-        expression: `localStorage.setItem('user', '{"accessToken":"${accessToken}",` +
+  if (browser !== null) {
+    try {
+      browser.on('targetchanged', async target => {
+        const targetPage = await target.page();
+        const client = await targetPage.target().createCDPSession();
+        await client.send('Runtime.evaluate', {
+          expression: `localStorage.setItem('user', '{"accessToken":"${accessToken}",` +
         `"authentication":{` +
           `"strategy":"local",` +
           `"accessToken":"${accessToken}"},` +
         `"user":${JSON.stringify(user)}}')`
+        });
       });
-    });
-  } catch (error) {
-    app.get('sentry').captureException(error);
-    logger.error(error);
-    logger.error(`Error pdf temporaire (2 error) : ${error}`);
-    logger.error(`Error pdf temporaire (2 browser) : ${browser}`);
+    } catch (error) {
+      await browser?.close();
+      app.get('sentry').captureException(error);
+      logger.error(error);
+    }
+
+    let pdf;
+    try {
+      const page = await browser.newPage();
+      await Promise.all([
+        await page.goto(app.get('espace_coop_hostname') + '/statistiques' + finUrl, { waitUntil: 'networkidle0', timeout: 60000 }),
+        await page.waitForTimeout(500),
+        await page.addStyleTag({ content: '#burgerMenu { display: none} .no-print { display: none }' }),
+        pdf = page.pdf({ format: 'A4', printBackground: true })
+      ]);
+
+      await browser.close();
+
+      res.contentType('application/pdf');
+      pdf.then(buffer => res.send(buffer));
+    } catch (error) {
+      await browser?.close();
+      app.get('sentry').captureException(error);
+      logger.error(error);
+      res.status(500).send(new GeneralError('Une erreur est survenue lors de la création du PDF, veuillez réessayer ultérieurement.').toJSON());
+    }
+    return;
   }
-
-  let pdf;
-  try {
-    const page = await browser.newPage();
-    await Promise.all([
-      await page.goto(app.get('espace_coop_hostname') + '/statistiques' + finUrl, { waitUntil: 'networkidle0', timeout: 60000 }),
-      await page.waitForTimeout(500),
-      await page.addStyleTag({ content: '#burgerMenu { display: none} .no-print { display: none }' }),
-      pdf = page.pdf({ format: 'A4', printBackground: true })
-    ]);
-
-    await browser.close();
-
-    res.contentType('application/pdf');
-    pdf.then(buffer => res.send(buffer));
-  } catch (error) {
-    app.get('sentry').captureException(error);
-    logger.error(error);
-    logger.error(`Error pdf temporaire (3 error) : ${error}`);
-    logger.error(`Error pdf temporaire (3 browser) : ${browser}`);
-    res.status(500).send(new GeneralError('Une erreur est survenue lors de la création du PDF, veuillez réessayer ultérieurement.').toJSON());
-  }
-  return;
 };
 
 module.exports = { generatePdf };
