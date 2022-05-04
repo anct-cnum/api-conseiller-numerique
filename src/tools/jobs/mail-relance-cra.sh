c4540c1fb7899604342d1e0cad340c00c469a3c6
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-05-04 16:53:56 +0100

    modif cron et limit envoi mail

--- src/tools/jobs/mail-relance-cra.sh
+++ src/tools/jobs/mail-relance-cra.sh
@@ -3,6 +3,6 @@
 cd ${APP_HOME}
 
 echo "Sending email to conseillers: START\n"
-node src/tools/scripts/mailRelanceCnFS/envoiMailRelanceM+1,5.js --limit 30
-node src/tools/scripts/mailRelanceCnFS/envoiMailRelanceM+1.js --limit 30
+node src/tools/scripts/mailRelanceCnFS/envoiMailRelanceM+1,5.js --limit 100
+node src/tools/scripts/mailRelanceCnFS/envoiMailRelanceM+1.js --limit 100
 echo "Sending email to conseillers: END\n"
