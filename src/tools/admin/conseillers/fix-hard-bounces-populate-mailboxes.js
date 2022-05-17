49cad8354d2eb9798e40b57e20bd5eb46ab85097
Jean-Yves Stervinou <jy@stervinou.com>
2022-05-17 11:33:31 +0200

    Script pour créer une boite mail à ceux qui ont été invités mais n'ont pas encore activé leur compte en saisissant un mot de passe

---
+++ src/tools/admin/conseillers/fix-hard-bounces-populate-mailboxes.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+/* eslint-disable guard-for-in */
+'use strict';
+
+const { execute } = require('../../utils');
+const slugify = require('slugify');
+const { v4: uuidv4 } = require('uuid');
+const { createMailbox } = require('../../utils/mailbox');
+
+execute(__filename, async ({ app, db, logger, Sentry, exit, gandi }) => {
+  try {
+    logger.info('Recherche des conseillers sans mot de passe');
+    let count = 0;
+    const users = await db.collection('users').find({ roles: { $in: ['conseiller'] }, passwordCreated: false }).toArray();
+
+    for (const idx in users) {
+      const user = users[idx];
+
+      const conseiller = await db.collection('conseillers').findOne({ _id: user.entity.oid });
+      // Creation boite mail du conseiller
+      const nom = slugify(`${conseiller.nom}`, { replacement: '-', lower: true, strict: true });
+      const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
+      const login = `${prenom}.${nom}`;
+      const password = uuidv4(); // Sera choisi par le conseiller via invitation
+      await createMailbox({ gandi, db, logger, Sentry: app.get('sentry') })({ conseillerId: conseiller._id, login, password });
+      count++;
+    }
+    logger.info(`${count} conseillers mises à jours`);
+  } catch (error) {
+    logger.error(error);
+    Sentry.captureException(error);
+    return;
+  }
+  exit();
+});
