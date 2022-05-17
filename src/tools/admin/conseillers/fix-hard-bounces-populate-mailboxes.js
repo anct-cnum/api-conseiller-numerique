2ba7fb191aae1ad8166676d4cc5ffa9a1c72b90e
Jean-Yves Stervinou <jy@stervinou.com>
2022-05-17 22:27:45 +0200

    Sentry

--- src/tools/admin/conseillers/fix-hard-bounces-populate-mailboxes.js
+++ src/tools/admin/conseillers/fix-hard-bounces-populate-mailboxes.js
@@ -22,7 +22,7 @@ execute(__filename, async ({ app, db, logger, Sentry, exit, gandi }) => {
       const prenom = slugify(`${conseiller.prenom}`, { replacement: '-', lower: true, strict: true });
       const login = `${prenom}.${nom}`;
       const password = uuidv4(); // Sera choisi par le conseiller via invitation
-      await createMailbox({ gandi, db, logger, Sentry: app.get('sentry') })({ conseillerId: conseiller._id, login, password });
+      await createMailbox({ gandi, db, logger, Sentry: Sentry })({ conseillerId: conseiller._id, login, password });
       count++;
     }
     logger.info(`${count} conseillers mis à jour`);
