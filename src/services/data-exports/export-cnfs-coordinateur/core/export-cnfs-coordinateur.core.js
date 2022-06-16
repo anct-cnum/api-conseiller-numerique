645cff64b8e6688fc12ef395b01d47b13768637d
morgan <morgan@fitlab.fr>
2022-06-16 15:08:03 +0200

    ajout de certifie dans l'export

--- src/services/data-exports/export-cnfs-coordinateur/core/export-cnfs-coordinateur.core.js
+++ src/services/data-exports/export-cnfs-coordinateur/core/export-cnfs-coordinateur.core.js
@@ -5,13 +5,13 @@ const formatDate = dateFin => dayjs(new Date(dateFin)).format('DD/MM/YYYY');
 const userActifStatus = mattermost => mattermost?.error === false ? 'Oui' : 'Non';
 
 const prettifyAndComplete = () => async statCnfs => {
-  const { mattermost, ...nextStatCnfs } = statCnfs;
+  const { mattermost, certifie, ...nextStatCnfs } = statCnfs;
   return {
     ...nextStatCnfs,
     datePrisePoste: formatDate(nextStatCnfs.datePrisePoste),
     dateFinFormation: formatDate(nextStatCnfs.dateFinFormation),
     isUserActif: userActifStatus(mattermost),
-    certifie: 'Non',
+    certifie: certifie === true ? 'Oui' : 'Non',
   };
 };
 
