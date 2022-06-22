851ea38b4590f04399cff7df4f302c808b45b1ec
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-06-22 16:10:06 +0100

    gestion des cas ayant eu des ruptures

--- src/tools/scripts/fixConseillerRupturePermanence.js
+++ src/tools/scripts/fixConseillerRupturePermanence.js
@@ -4,13 +4,16 @@
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseillers = await db.collection('conseillers').find({ statut: 'RUPTURE' }).toArray();
+  const conseillers = await db.collection('conseillers').find(
+    {
+      $or: [{ statut: 'RUPTURE' }, { ruptures: { $exists: true } }]
+    }).toArray();
   let promises = [];
 
   logger.info('Suppression des conseillers lié à des permanences avec le statut RUPTURE...');
   conseillers.forEach(conseiller => {
     promises.push(new Promise(async resolve => {
-      await db.collection('permanences').updateMany({},
+      await db.collection('permanences').updateMany({ 'structure.oid': { $ne: conseiller.structureId } },
         { $pull: { conseillers: conseiller._id, conseillersItinerants: conseiller._id } }
       );
       resolve();
