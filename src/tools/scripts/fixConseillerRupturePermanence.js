a91caf9f46086fd28cb06b2d6004349077ba2faa
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2022-06-23 09:58:55 +0100

    prise en compte commentaire

--- src/tools/scripts/fixConseillerRupturePermanence.js
+++ src/tools/scripts/fixConseillerRupturePermanence.js
@@ -4,16 +4,20 @@
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseillers = await db.collection('conseillers').find(
-    {
-      $or: [{ statut: 'RUPTURE' }, { ruptures: { $exists: true } }]
-    }).toArray();
+  const conseillers = await db.collection('conseillers').find({ ruptures: { $exists: true } }).toArray();
   let promises = [];
 
-  logger.info('Suppression des conseillers lié à des permanences avec le statut RUPTURE...');
+  logger.info('Suppression des conseillers liés à des permanences avec le statut RUPTURE...');
   conseillers.forEach(conseiller => {
     promises.push(new Promise(async resolve => {
-      await db.collection('permanences').updateMany({ 'structure.oid': { $ne: conseiller.structureId } },
+      await db.collection('permanences').updateMany(
+        {
+          $or: [
+            { 'structure.oid': { $ne: conseiller.structureId } },
+            { 'conseillers': { $elemMatch: { $eq: conseiller._id } } },
+            { 'conseillersItinerants': { $elemMatch: { $eq: conseiller._id } } }
+          ]
+        },
         { $pull: { conseillers: conseiller._id, conseillersItinerants: conseiller._id } }
       );
       resolve();
