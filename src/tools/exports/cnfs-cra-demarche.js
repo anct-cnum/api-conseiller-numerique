435fcb06dfd6862dfe0f468424a184fefafd9f29
Ornella Ourfi <ornella93@outlook.fr>
2022-07-01 17:49:26 +0200

    rectif..

--- src/tools/exports/cnfs-cra-demarche.js
+++ src/tools/exports/cnfs-cra-demarche.js
@@ -17,18 +17,20 @@ execute(__filename, async ({ logger, db }) => {
     flags: 'w'
   });
 
-  file.write('Nom;Prénom;mail du CNFS;Nom Structure;SIRET;Adresse de la SA\n');
+  file.write('Nom;Prénom;mail du CNFS;Nombre de CRA;Nom Structure;SIRET;Adresse de la SA\n');
   conseillersids.forEach(id => {
     promises.push(new Promise(async resolve => {
       const conseiller = await db.collection('conseillers').findOne({ _id: id, statut: 'RECRUTE' });
       if (conseiller) {
         const structure = await db.collection('structures').findOne({ _id: conseiller.structureId });
         // eslint-disable-next-line camelcase
-        const { numero_voie, type_voie, nom_voie, complement_adresse, code_postal, localite } = structure?.insee.etablissement.adresse;
+        const { numero_voie, type_voie, nom_voie, complement_adresse, code_postal, localite } = structure?.insee?.etablissement?.adresse;
         // eslint-disable-next-line camelcase
         const adresse = `${numero_voie ?? ''} ${type_voie ?? ''} ${nom_voie ?? ''} ${complement_adresse ?? ''} ${code_postal ?? ''} ${localite ?? ''}`;
-
-        file.write(`${conseiller.nom};${conseiller.prenom};${conseiller?.emailCN?.adress};${structure.nom};${structure.nom};${structure.siret};${adresse}\n`);
+        const craFiltre = await db.collection('cras').countDocuments({ 'conseiller.$id': id, 'cra.themes': { $in: ['demarche en ligne'] } });
+        const craTotal = await db.collection('cras').countDocuments({ 'conseiller.$id': id });
+        // eslint-disable-next-line max-len
+        file.write(`${conseiller.nom};${conseiller.prenom};${conseiller?.emailCN?.address};${craFiltre} /${craTotal};${structure.nom};${structure.siret};${adresse.replace(/["']/g, '')}\n`);
       }
       resolve();
     }));
