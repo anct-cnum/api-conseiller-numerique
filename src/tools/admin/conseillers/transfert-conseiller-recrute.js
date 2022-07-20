f892aada5ccbff060776a122bd7e58f19911019f
Ornella Ourfi <ornella93@outlook.fr>
2022-07-20 12:04:05 +0200

    suppresion de commentaire

--- src/tools/admin/conseillers/transfert-conseiller-recrute.js
+++ src/tools/admin/conseillers/transfert-conseiller-recrute.js
@@ -3,6 +3,7 @@ const { execute } = require('../../utils');
 const { DBRef, ObjectID } = require('mongodb');
 const utils = require('../../../utils/index');
 require('dotenv').config();
+
 execute(__filename, async ({ db, logger, exit, app }) => {
 
   program.option('-i, --id <id>', 'id: id Mongo du conseiller à transferer');
@@ -20,7 +21,6 @@ execute(__filename, async ({ db, logger, exit, app }) => {
     return;
   }
 
-  // Phase de contrôle :
   const cnfsRecrute = await db.collection('misesEnRelation').findOne({ 'conseiller.$id': idCNFS, 'structure.$id': ancienneSA, 'statut': 'finalisee' });
   if (!cnfsRecrute) {
     exit(`Le Cnfs avec l'id ${idCNFS} n'est pas recruté.`);
@@ -42,17 +42,10 @@ execute(__filename, async ({ db, logger, exit, app }) => {
     exit(`La structure destinataire est seulement autorisé à ${dernierCoselec.nombreConseillersCoselec} et à déjà ${misesEnRelationRecrutees} validé(s)/recrutée(s)`);
     return;
   }
-  // Checker si conseiller bien recruté avec la structure d'origine (statut finalisee)
-  // Checker si la structure de destination existe bien + bien validée Coselec + quota
 
-  // Modification du document conseiller
   await db.collection('conseillers').updateOne({ _id: idCNFS }, { $set: { structureId: nouvelleSA } });
   await db.collection('misesEnRelation').updateMany({ 'conseiller.$id': idCNFS }, { $set: { 'conseillerObj.structureId': nouvelleSA } });
-  // Mise à jour du champ structureId avec le nouveau OID
-  // Mise à jour du cache conseillerObj dans les mises en relation pour ce champ structureId avec le nouveau OID
 
-
-  // Modification de la mise en relation finalisée avec l'ancienne structure
   await db.collection('misesEnRelation').updateOne(
     { 'conseiller.$id': idCNFS, 'structure.$id': ancienneSA },
     { $set: {
@@ -63,14 +56,13 @@ execute(__filename, async ({ db, logger, exit, app }) => {
       }
     }
     });
-  // Mise à jour du statut en 'finalisee_non_disponible'
 
   const misesEnrelationNouvelleSA = await db.collection('misesEnRelation').findOne({ 'conseiller.$id': idCNFS, 'structure.$id': nouvelleSA });
   const transfert = {
     'ancienneStructureId': nouvelleSA,
     'date': new Date()
   };
-  // Vérification de la présence ou non de la mise en relation entre le conseiller et la structure de destination
+
   if (misesEnrelationNouvelleSA) {
     const connection = app.get('mongodb');
     const database = connection.substr(connection.lastIndexOf('/') + 1);
@@ -95,8 +87,7 @@ execute(__filename, async ({ db, logger, exit, app }) => {
       }
       });
   }
-  // Si non présente : la créer aves statut 'finalisee'
-  // Si présente : modifier le statut en 'finalisee'
+
   logger.info(`Le conseiller id: ${idCNFS} a été transférer par la structure: ${ancienneSA} vers la structure: ${nouvelleSA}`);
   exit();
 });
