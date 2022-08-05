705acae6863258b8e9344218fab6202f0b6dcc0e
Jean-Yves Stervinou <jy@stervinou.com>
2022-08-05 16:54:50 +0200

    Lint

--- src/services/structures/get-strucures/utils/get-structure.utils.js
+++ src/services/structures/get-strucures/utils/get-structure.utils.js
@@ -1,16 +1,12 @@
 const doublon = 'DOUBLON';
 const regexpFirstMatchIndex = 1;
 const captureCoselecNumberRegexp = /\/coselec (\d+)\//;
-const latest = -1;
 
 const hasDuplicateStatut = structure =>
   structure.statut === doublon;
 
 const getCoselecNumber = prefet =>
   Number(prefet.fichier.match(captureCoselecNumberRegexp)[regexpFirstMatchIndex]);
-
-const byCoselecNumber = (prefetA, prefetB) =>
-  getCoselecNumber(prefetA) - getCoselecNumber(prefetB);
 
 const isStructureDuplicate = structure =>
   hasDuplicateStatut(structure);
