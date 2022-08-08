30b876a5762f5b788d9a893a1502213fb5243ac2
Ornella Ourfi <ornella93@outlook.fr>
2022-08-08 17:36:56 +0200

    password hash

--- src/tools/data/tasks/fake-data.js
+++ src/tools/data/tasks/fake-data.js
@@ -1,6 +1,7 @@
 const faker = require('@faker-js/faker/locale/fr');
 const { name, internet, helpers, random, datatype } = faker;
 const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
 
 module.exports = async ({ idPG }) => {
   if (idPG) {
@@ -12,11 +13,12 @@ module.exports = async ({ idPG }) => {
   let tel = random.arrayElement(['06', '07', '01', '02', '+336', '+337']);
   let telephone = helpers.replaceSymbolWithNumber(`${tel}########`);
   let token = datatype.uuid();
-  const password = uuidv4();
+  let password = uuidv4();
   const tokenCreatedAt = new Date();
   nom = nom.toLowerCase();
   prenom = prenom.toLowerCase();
   email = email.toLowerCase();
+  password = await bcrypt.hashSync(password);
 
   return {
     nom,
