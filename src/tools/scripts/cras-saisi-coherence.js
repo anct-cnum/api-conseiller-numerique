0599b227154042f43abfe15c42acecec065ae8e9
Ornella Ourfi <ornella93@outlook.fr>
2022-09-12 15:35:20 +0200

    src/tools/scripts/cras-saisi-coherence.js

--- src/tools/scripts/cras-saisi-coherence.js
+++ src/tools/scripts/cras-saisi-coherence.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
 'use strict';
-
+const { program } = require('commander');
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const cras = await db.collection('cras').find({}).toArray();
+  program.option('-f, --fix', 'fix: correction des cra(s) incohérente(s)');
+  program.parse(process.argv);
+
+  const fix = program.fix;
   let promises = [];
   let conseillerCraSaisi = [];
   let count = 0;
+  let countFix = 0;
+
+  const cras = await db.collection('cras').find({}).toArray();
 
   logger.info('Détection + correction de cras à corriger');
   cras.forEach(cra => {
     promises.push(new Promise(async resolve => {
-      if (cra.cra?.nbParticipantsRecurrents > cra.cra?.nbParticipants) {
+      if (cra.cra.nbParticipantsRecurrents > cra.cra.nbParticipants) {
         count++;
-        conseillerCraSaisi.push({
-          _id: cra.conseiller.oid,
-          activite: cra.cra?.activite,
-          nbParticipants: cra.cra?.nbParticipants,
-          nbParticipantsRecurrents: cra.cra?.nbParticipantsRecurrents,
-          action: cra.cra?.activite === 'collectif' ? `changer le nbr de participant de ${cra.cra?.nbParticipants} à ${cra.cra?.nbParticipantsRecurrents}` :
-            `changer le nbr de récurrent de ${cra.cra?.nbParticipantsRecurrents} à 1`
-        });
-        if (['individuel', 'ponctuel'].includes(cra.cra?.activite)) {
+        if (fix) {
+          countFix++;
+          conseillerCraSaisi.push({
+            _id: cra.conseiller.oid,
+            activite: cra.cra.activite,
+            nbParticipants: cra.cra.nbParticipants,
+            nbParticipantsRecurrents: cra.cra.nbParticipantsRecurrents,
+            action: `changer le nbr de récurrent de ${cra.cra?.nbParticipantsRecurrents} à ${cra.cra.nbParticipants}`
+          });
           await db.collection('cras').updateOne(
             { _id: cra._id },
-            { $set: { 'cra.nbParticipantsRecurrents': 1 } }
-          );
-        }
-        if (cra.cra?.activite === 'collectif') {
-          await db.collection('cras').updateOne(
-            { _id: cra._id },
-            { $set: { 'cra.nbParticipants': cra.cra?.nbParticipantsRecurrents } }
+            { $set: { 'cra.nbParticipantsRecurrents': cra.cra.nbParticipants } }
           );
         }
       }
@@ -39,6 +39,6 @@ execute(__filename, async ({ logger, db }) => {
     }));
   });
   logger.info(conseillerCraSaisi);
-  logger.info(`${count} CRA(s) corrigé(s)`);
+  logger.info(`${count} CRA(s) incohérente(s) et ${countFix} CRA(s) corrigé(s)`);
   await Promise.all(promises);
 });
