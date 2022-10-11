b0c02f73bb6232ba621c2b06aad3949c3d2f2cab
Ornella Ourfi <ornella93@outlook.fr>
2022-10-11 17:24:55 +0200

    export pour les cnfs des coordo

--- src/services/conseillers/export-statistiques/repositories/export-statistiques.repository.js
+++ src/services/conseillers/export-statistiques/repositories/export-statistiques.repository.js
@@ -1,8 +1,19 @@
+const { ObjectId } = require('mongodb');
+
 const getConseillerAssociatedWithUser = db => async user =>
   await db.collection('conseillers').findOne({ _id: user.entity.oid });
 
+const getConseiller = db => async userIdSubordonne =>
+  await db.collection('conseillers').findOne({ _id: new ObjectId(userIdSubordonne) });
+
+const getConseillerSubordonee = db => async (user, userIdSubordonne) =>
+  await db.collection('conseillers').findOne({ '_id': user.entity.oid, 'listeSubordonnes.liste': { '$in': [new ObjectId(userIdSubordonne)] } });
+
+
 const exportStatistiquesRepository = db => ({
-  getConseillerAssociatedWithUser: getConseillerAssociatedWithUser(db)
+  getConseillerAssociatedWithUser: getConseillerAssociatedWithUser(db),
+  getConseiller: getConseiller(db),
+  getConseillerSubordonee: getConseillerSubordonee(db)
 });
 
 module.exports = {
