9d6bec0731b81e1243eac6430819112eef0eecd8
morgan <morgan@fitlab.fr>
2022-10-26 14:11:46 +0200

    Ajout d'un index sur la date d'accompagnement du cra

--- src/tools/indexes/tasks/mongoIndexes.js
+++ src/tools/indexes/tasks/mongoIndexes.js
@@ -74,6 +74,7 @@ module.exports = {
       db.collection('cras').createIndex({ 'cra.duree': 1 }),
       db.collection('cras').createIndex({ 'cra.codePostal': 1 }),
       db.collection('cras').createIndex({ 'cra.themes': 1 }),
+      db.collection('cras').createIndex({ 'cra.dateAccompagnement': 1 }),
     ]);
   },
   stats_conseillers_cras: db => {
