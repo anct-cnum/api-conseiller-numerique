78d8fee9be7f6ca06a3ccd70ff9aa8729e5c7f09
Ornella Ourfi <ornella93@outlook.fr>
2022-12-14 14:01:15 +0100

    evolution out-coop lié au potentiel transfert

--- src/tools/admin/conseillers/import-recrutes.js
+++ src/tools/admin/conseillers/import-recrutes.js
@@ -78,6 +78,10 @@ execute(__filename, async ({ feathers, app, db, logger, exit, Sentry }) => {
           const datePrisePoste = conseiller['Date de départ en formation'].replace(/^(.{2})(.{1})(.{2})(.{1})(.{4})$/, '$5-$3-$1');
           const formatDate = date => dayjs(date).format('DD/MM/YYYY');
           const date = date => dayjs(date, 'YYYY-MM-DD').toDate();
+          const dateRupture = conseillerOriginal?.ruptures?.slice(-1)[0]?.dateRupture;
+          const query = conseillerOriginal?.ruptures ? { '$gt': dateRupture } : { '$gte': miseEnRelation.dateRecrutement };
+          const matchCras = { 'conseiller.$id': conseillerOriginal._id, 'cra.dateAccompagnement': query };
+          const countCras = await db.collection('cras').countDocuments(matchCras);
 
           if (!regexDateFormation.test(conseiller['Date de fin de formation']) || !regexDateFormation.test(conseiller['Date de départ en formation'])) {
             logger.error(`Format date invalide (attendu DD/MM/YYYY) pour les dates de formation pour le conseiller avec l'id: ${idPGConseiller}`);
@@ -125,6 +129,10 @@ execute(__filename, async ({ feathers, app, db, logger, exit, Sentry }) => {
           } else if (miseEnRelation === null) {
             logger.error(`Mise en relation introuvable pour la structure avec l'idPG '${structureId}'`);
             Sentry.captureException(`Mise en relation introuvable pour la structure avec l'idPG '${structureId}'`);
+            errors++;
+          } else if (dateRupture && (dateRupture > miseEnRelation.dateRecrutement)) {
+            // eslint-disable-next-line max-len
+            logger.warn(`Un conseiller avec l'id: ${idPGConseiller} a une date de Rupture ${dateRupture} supérieur à la date de recrutement ${miseEnRelation.dateRecrutement}`);
             errors++;
           } else {
             //Maj PG en premier lieu pour éviter la resynchro PG > Mongo (avec email pour tous les doublons potentiels)
@@ -216,7 +224,14 @@ execute(__filename, async ({ feathers, app, db, logger, exit, Sentry }) => {
                 'conseillerObj.userCreated': false
               }
             });
-
+            if (countCras >= 1) {
+              logger.info(`Maj de ${countCras} CRAS pour le conseiller avec l'id: ${idPGConseiller}, cras => ${query}`);
+              await db.collection('cras').updateMany(matchCras, {
+                $set: {
+                  'structure.$id': miseEnRelation.structure.oid,
+                }
+              });
+            }
             // Creation boite mail du conseiller
             const gandi = app.get('gandi');
             const nom = slugify(`${conseillerUpdated.nom}`, { replacement: '-', lower: true, strict: true });
