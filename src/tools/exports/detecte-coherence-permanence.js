1ed78d56265003daa413972f26ba01813b2c8a13
Ornella Ourfi <ornella93@outlook.fr>
2022-12-22 14:05:35 +0100

    retour commentaire + ajout commentaire

--- src/tools/exports/detecte-coherence-permanence.js
+++ src/tools/exports/detecte-coherence-permanence.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const { execute } = require('../utils');
 
 execute(__filename, async ({ logger, db }) => {
-  const conseillers = await db.collection('conseillers').find({ statut: 'RECRUTE' }).toArray();
+  const conseillers = await db.collection('conseillers').find({ statut: 'RECRUTE', idPG: 52407 }).toArray();
+  // const conseillers = await db.collection('conseillers').find({ statut: 'RECRUTE' }).toArray();
   let promises = [];
   let count = 0;
   logger.info(`Generating CSV file...`);
@@ -14,14 +15,14 @@ execute(__filename, async ({ logger, db }) => {
   let file = fs.createWriteStream(csvFile, {
     flags: 'w'
   });
-  file.write('id du conseiller; Nom; Prenom;email @conseiller-numerique.fr;Détails\n');
+  file.write('id du conseiller;Nom;Prenom;email @conseiller-numerique.fr;Détails\n');
   conseillers.forEach(cnfs => {
     promises.push(new Promise(async resolve => {
       // controle cohérence :
       let coherenceHasPermenceTrue = 0; // sur le hasPermanence true en lien avec la collection
       let coherenceHasPermenceFalse = 0; // sur le hasPermanence false en lien avec la collection
-      let coherenceLieuxprincipale = 0; // sur les Permanences "Principale" si ils ont au moins 1 permanence
-      let coherenceLieuxprincipaleItinerence = 0; // sur les Permanences "Principale avec la donnée itinérence
+      let coherenceLieuxPrincipaux = 0; // sur les Permanences "Principale" si ils ont au moins 1 permanence
+      let coherenceLieuxprincipauxItinerence = 0; // sur les Permanences "Principale avec la donnée itinérence
       let coherenceArrayConseillers = 0; // sur les Permanences avec l'id du conseiller mais qui n'est pas présente dans l'array conseillers
       const queryPrincipal = {
         'conseillers': { '$in': [cnfs._id] },
@@ -41,20 +42,19 @@ execute(__filename, async ({ logger, db }) => {
 
       coherenceHasPermenceTrue = (checkCountPermanences === 0 && cnfs?.hasPermanence === true) ? 'OUI' : 'NON';
       coherenceHasPermenceFalse = (checkCountPermanences >= 1 && !cnfs?.hasPermanence) ? 'OUI' : 'NON';
-      coherenceLieuxprincipale = (checkCountPermanences >= 1 && checkLieuPrincipal === 0) ? 'OUI' : 'NON';
-      coherenceLieuxprincipaleItinerence = (checkCountPermanences >= 1 && checkItinerants >= 1) ? 'OUI' : 'NON';
-      coherenceArrayConseillers = (checkConseillers >= 1) ? 'OUI' : 'NON';
+      coherenceLieuxPrincipaux = (checkCountPermanences >= 1 && checkLieuPrincipal === 0) ? 'OUI' : 'NON';
+      coherenceLieuxprincipauxItinerence = (checkCountPermanences >= 1 && checkItinerants >= 1) ? 'OUI' : 'NON';
+      coherenceArrayConseillers = (checkConseillers >= 1) ? 'OUI' : 'NON'; // if OUI => le checkCountPermanences ligne 43 sera OUI aussi
 
       const checkCoherence = [
         coherenceHasPermenceTrue,
         coherenceHasPermenceFalse,
-        coherenceLieuxprincipale,
-        coherenceLieuxprincipaleItinerence,
+        coherenceLieuxPrincipaux,
+        coherenceLieuxprincipauxItinerence,
         coherenceArrayConseillers
       ];
 
       if (checkCoherence.includes('OUI')) {
-        // eslint-disable-next-line max-len
         file.write(`${cnfs.idPG};${cnfs.nom};${cnfs.prenom};${cnfs?.emailCN?.address};${checkCoherence.join('/ ')}\n`);
         count++;
       }
