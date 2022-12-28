299e4729b4046ff99267904863d8ade1b5f749b2
Ornella Ourfi <ornella93@outlook.fr>
2022-12-28 14:51:09 +0100

    retour commentaire..

--- src/tools/scripts/date-formation-rupture.js
+++ src/tools/scripts/date-formation-rupture.js
@@ -28,13 +28,13 @@ execute(__filename, async ({ db, logger, exit }) => {
     _id: { '$in': arrayIdCNRupture },
     dateFinFormation: { '$exists': false }
   }).toArray();
-  const conseillerIdPG = arrayIdPGConseiller.map(e => e.idPG);
+  const ListConseillerIdPG = arrayIdPGConseiller.map(e => e.idPG);
 
   const arrayIdPGSupprime = await db.collection('conseillersSupprimes').find({
     'conseiller._id': { '$in': arrayIdCNRupture },
     'conseiller.dateFinFormation': { '$exists': false }
   }).toArray();
-  const conseilleSupprimeIdPG = arrayIdPGSupprime.map(e => e.conseiller.idPG);
+  const ListConseilleSupprimeIdPG = arrayIdPGSupprime.map(e => e.conseiller.idPG);
 
   await new Promise(() => {
     readCSV(program.csv).then(async fichier => {
@@ -45,21 +45,26 @@ execute(__filename, async ({ db, logger, exit }) => {
           const dateFinFormation = coop['Date de fin de formation'].replace(/^(.{2})(.{1})(.{2})(.{1})(.{4})$/, '$5-$3-$1');
           const idStructure = coop['ID structure'];
           const idConseiller = parseInt(coop['ID conseiller']);
-          if (conseillerIdPG.includes(idConseiller)) {
+          if (ListConseillerIdPG.includes(idConseiller)) {
             await db.collection('conseillers').updateOne({ idPG: idConseiller },
               { $set: {
                 datePrisePoste: date(datePrisePoste),
                 dateFinFormation: date(dateFinFormation),
               } });
-            logger.info('-', emailCN, datePrisePoste, dateFinFormation, idStructure, idConseiller);
+            logger.info(['- ' + emailCN, datePrisePoste, dateFinFormation, idStructure, idConseiller].join(','));
             countConseiller++;
           }
-          if (conseilleSupprimeIdPG.includes(idConseiller)) {
-            logger.info('*', emailCN, datePrisePoste, dateFinFormation, idStructure, idConseiller);
+          if (ListConseilleSupprimeIdPG.includes(idConseiller)) {
+            await db.collection('conseillersSupprimes').updateOne({ 'conseiller.idPG': idConseiller },
+              { $set: {
+                'conseiller.datePrisePoste': date(datePrisePoste),
+                'conseiller.dateFinFormation': date(dateFinFormation),
+              } });
+            logger.info(['* ' + emailCN, datePrisePoste, dateFinFormation, idStructure, idConseiller].join(','));
             countConseillerSupprime++;
           }
         }
-        logger.info(`${countConseiller} / ${conseillerIdPG.length} pour la collection conseillers`);
+        logger.info(`${countConseiller} / ${ListConseillerIdPG.length} pour la collection conseillers`);
         logger.info(`${countConseillerSupprime} / ${arrayIdPGSupprime.length} pour la collection conseillersSupprimes`);
         exit();
       });
