0f53fc704d2da7ba6f341d54dedfcc766f714c96
Ornella Ourfi <ornella93@outlook.fr>
2023-02-01 15:38:06 +0100

    retour commentaire + ajout condition dans le cas où il est déjà recruté

--- src/tools/admin/conseillers/double-recrutement-cnfs.js
+++ src/tools/admin/conseillers/double-recrutement-cnfs.js
@@ -5,26 +5,24 @@ const dayjs = require('dayjs');
 execute(__filename, async ({ db, logger, Sentry, exit }) => {
 
   program.option('-c, --conseiller <conseiller>', 'conseiller: id PG du conseiller');
-  program.option('-s, --structure <sa>', 'id: id PG de la structure');
-  program.option('-d, --date <date>', 'date: date prévisionel d\'embauche DD/MM/AAAA');
-  program.option('-s, --statut <statut>', 'statut: nouvelle ou interessee ou recrutee');
+  program.option('-s, --structure <structure>', 'id: id PG de la structure');
+  program.option('-d, --date <date>', 'date: date prévisionnelle d\'embauche DD/MM/AAAA');
+  program.option('-s, --statut <statut>', 'statut: interessee ou recrutee');
   program.helpOption('-e', 'HELP command');
   program.parse(process.argv);
 
-  const constroleDate = new RegExp(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)((202)[0-9])$/);
+  const controleDate = new RegExp(/^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)((202)[0-9])$/);
   const idPGConseiller = ~~program.conseiller;
   const idPGStructure = ~~program.structure;
   let datePrevisionnelle = program.date;
   let statut = program.statut;
 
-  if ((idPGStructure === 0) || (idPGConseiller === 0) || (statut === 'recrutee' && !constroleDate.test(datePrevisionnelle)) || !statut) {
-    exit('Paramètre invalide. Veuillez préciser un id conseiller & id structure ainsi que la date prévissionelle');
+  // eslint-disable-next-line max-len
+  if ((idPGStructure === 0) || (idPGConseiller === 0) || (statut === 'recrutee' && !controleDate.test(datePrevisionnelle)) || !['recrutee', 'interessee'].includes(statut)) {
+    exit('Paramètres invalides. Veuillez préciser un statut, un id conseiller & id structure ainsi que la date prévisionnelle si statut recrutee');
     return;
   }
-  if (!['nouvelle', 'recrutee', 'interessee'].includes(statut)) {
-    exit(`Le statut ${statut} est invalide. les statuts autorisés sont [nouvelle, recrutee, interessee]`);
-    return;
-  }
+
   try {
 
     const finaliseeRupture = await db.collection('misesEnRelation').findOne({
@@ -37,27 +35,34 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
       exit('Aucune mise en relation avec le statut finalisee_rupture existe !');
       return;
     }
+    const finalisee = await db.collection('misesEnRelation').findOne({
+      'conseillerObj.idPG': idPGConseiller,
+      'statut': 'finalisee'
+    });
+
+    if (finalisee) {
+      exit(`Le conseiller est déjà RECRUTE par la structure ${finalisee?.structureObj.idPG}`);
+      return;
+    }
 
     datePrevisionnelle = dayjs(datePrevisionnelle.replace(/^(.{2})(.{1})(.{2})(.{1})(.{4})$/, '$5-$3-$1'), 'YYYY-MM-DD').toDate();
-    console.log('datePrevisionnelle:', datePrevisionnelle);
-    const {
-      dossierIncompletRupture,
-      dateRupture,
-      emetteurRupture,
-      motifRupture,
-      resendMailCnfsRupture,
-      dateRecrutement,
+
+    let misesEnRelation = {
+      conseiller: finaliseeRupture.conseiller,
+      structure: finaliseeRupture.structure,
       statut,
-      transfert,
-      validateurRupture,
-      _id,
-      ...misesEnRelation } = finaliseeRupture;
+      createdAt: new Date(),
+      conseillerCreatedAt: finaliseeRupture.conseillerObj.createdAt,
+      conseillerObj: finaliseeRupture.conseillerObj,
+      structureObj: finaliseeRupture.structureObj,
+      type: 'MANUEL',
+    };
 
     if (statut === 'recrutee') {
       misesEnRelation.dateRecrutement = datePrevisionnelle;
     }
 
-    await db.collection('misesEnRelation').insertOne({ ...misesEnRelation, statut });
+    await db.collection('misesEnRelation').insertOne(misesEnRelation);
 
   } catch (error) {
     logger.error(error);
@@ -65,7 +70,7 @@ execute(__filename, async ({ db, logger, Sentry, exit }) => {
     return;
   }
 
-  logger.info(`Mises En relation bien créer pour la SA ${idPGStructure} avec le conseiller ${idPGConseiller} avec un statut ${statut}`);
+  logger.info(`Mises En relation bien créée pour la SA ${idPGStructure} avec le conseiller ${idPGConseiller} avec un statut ${statut}`);
   exit();
 });
 
