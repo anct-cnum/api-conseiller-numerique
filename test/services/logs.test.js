34431726640b4c86906fa85af27db835f856ec97
dienamo <46058257+dienamo@users.noreply.github.com>
2023-02-15 15:53:57 +0100

    retrait du fichier de test

--- test/services/logs.test.js
+++
@@ -1,8 +0,0 @@
-const app = require('../../src/app');
-
-describe('\'logs\' service', () => {
-  it('registered the service', () => {
-    const service = app.service('logs');
-    expect(service).toBeTruthy();
-  });
-});
