ff6006994d726b8238706b353873cab3521661aa
morgan <morgan@fitlab.fr>
2023-03-15 10:01:58 +0100

    suppression mail d'activité et archivage de certains script

--- src/tools/jobs/send-email-mensuelle-activite-cras.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-
-cd ${APP_HOME}
-
-echo "Sending email to conseillers: START\n"
-node src/tools/scripts/mailMensuelActivite/mailMensuelActivite.js --limit 4000
-echo "Sending email to conseillers: END\n"
