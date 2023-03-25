32e9546da825e8322d7ef17afaa13d9723258820
Ornella Ourfi <ornella93@outlook.fr>
2023-03-25 11:12:03 +0100

    refacto script, aux lieux du .json c'est en csv

--- src/tools/scripts/AjoutCodeInseeAndFixPermanences.js
+++ src/tools/scripts/AjoutCodeInseeAndFixPermanences.js
@@ -8,6 +8,15 @@ const axios = require('axios');
 const codePostauxFichier = require('../../../data/imports/code-commune.json');
 const { program } = require('commander');
 
+const statCras = async db => {
+  const CrasRestantSansPerm = await db.collection('cras').countDocuments(
+    { 'cra.codeCommune': { '$exists': false }, 'permanence.$id': { '$exists': false } }
+  );
+  const CrasRestantAvecPerm = await db.collection('cras').countDocuments(
+    { 'cra.codeCommune': { '$exists': false }, 'permanence.$id': { '$exists': true } }
+  );
+  return { CrasRestantSansPerm, CrasRestantAvecPerm };
+};
 const updatePermanenceAndCRAS = db => async (matchLocation, _id) => {
   await db.collection('permanences').updateOne({ _id },
     { '$set': {
@@ -15,40 +24,55 @@ const updatePermanenceAndCRAS = db => async (matchLocation, _id) => {
       'adresse.rue': matchLocation.rue,
       'codePostal': matchLocation.codePostal,
       'adresse.ville': matchLocation.ville,
-      'adresse.codeCommuneInsee': matchLocation.codeCommuneInsee,
+      'adresse.codeCommune': matchLocation.codeCommune,
     }
     });
   await db.collection('cras').updateMany({ 'permanence.$id': _id },
     { '$set': {
       'cra.codePostal': matchLocation.codePostal,
       'cra.nomCommune': matchLocation.ville,
-      'cra.codeCommuneInsee': matchLocation.codeCommuneInsee,
+      'cra.codeCommune': matchLocation.codeCommune,
     }
     });
 };
-const statCras = async db => {
-  const CrasRestantSansPerm = await db.collection('cras').countDocuments(
-    { 'cra.codeCommuneInsee': { '$exists': false }, 'permanence.$id': { '$exists': false } }
-  );
-  const CrasRestantAvecPerm = await db.collection('cras').countDocuments(
-    { 'cra.codeCommuneInsee': { '$exists': false }, 'permanence.$id': { '$exists': true } }
-  );
-  return { CrasRestantSansPerm, CrasRestantAvecPerm };
-};
-const formatText = mot => {
-  let m = mot.normalize('NFD').replace(/[\u0300-\u036f]/g, '')?.replace(/[',-]/g, ' ');
-  m = m.replace(/\bSAINT\b/gi, 'ST');
-  m = m.replace(/\bSAINTE\b/gi, 'STE');
-  return m;
-};
-
+const formatText = mot => mot?.normalize('NFD').replace(/[\u0300-\u036f]/g, '')?.replace(/[',-]/g, ' ');
 const resultApi = obj => ({
   'numeroRue': obj?.housenumber ?? '',
-  'rue': obj?.street ?? '',
+  'rue': obj?.street ?? obj?.locality,
   'codePostal': obj?.postcode,
   'ville': obj?.city.toUpperCase(),
-  'codeCommuneInsee': obj?.citycode,
+  'codeCommune': obj?.citycode,
 });
+const exportCsvPermanences = (exportsCSv, lot, logger) => {
+  const exportCsvColonnes = [
+    { label: 'permMatchOK', colonne: 'id permanence;Adresse permanence;matchOK\n' },
+    { label: 'permNotOK', colonne: 'id permanence;Adresse permanence;nombre résultat Api adresse (query lat/lon); Resultat Api Adresse (query lat/lon)\n' },
+    { label: 'diffCityAndCodePostal', colonne: 'total de diff;id permanence;Adresse permanence;Resultat Api Adresse\n' },
+    { label: 'permError', colonne: 'id permanence;message;detail\n' },
+  ];
+  const objectCsv = obj => `${obj.numeroRue} ${obj.rue} ${obj.codePostal} ${obj.ville}`;
+  Object.keys(exportsCSv).forEach(function(key) {
+    let csvFile = path.join(__dirname, '../../../data/exports-historique', `${lot}Lot-${key}.csv`);
+    let file = fs.createWriteStream(csvFile, { flags: 'w' });
+    file.write(exportCsvColonnes.find(i => i.label === key).colonne);
+    exportsCSv[key].forEach(i => {
+      switch (key) {
+        case 'permMatchOK':
+          file.write(`${i._id};${objectCsv(i.adresse)};${objectCsv(i.matchOK)}\n`);
+          break;
+        case 'permNotOK':
+          file.write(`${i._id};${objectCsv(i.adresse)} /${i.adresse.coordinates};${i.resultApi?.total};${i.resultApi.fields.map(i => objectCsv(i))}\n`);
+          break;
+        case 'diffCityAndCodePostal':
+          file.write(`${i.nombreDiff};${i._id};${objectCsv(i.adresse)};${objectCsv(i.matchLocation)}\n`);
+          break;
+        default:
+          file.write(`${i._id};${i.message};${i.detail}\n`);
+      }
+    });
+    logger.info(`${[key]}: ${exportsCSv[key].length}`);
+  });
+};
 
 execute(__filename, async ({ logger, db, exit }) => {
   program.option('-l, --limit <limit>', 'limit: limit');
@@ -61,22 +85,22 @@ execute(__filename, async ({ logger, db, exit }) => {
   const limit = ~~program.limit === 0 ? 1 : ~~program.limit;
   const { partie, lot, acte } = program;
 
-  if (!['cras', 'permanences'].includes(partie)) { // ok
+  if (!['cras', 'permanences'].includes(partie)) {
     exit(`Partie incorrect, veuillez choisir parmi la liste ['cras', 'permanences']`);
     return;
-  } else if (!lot && partie === 'cras') { // ok
+  } else if (!lot && partie === 'cras') {
     exit(`Presciser numero lot pour la partie ${partie} ?`);
     return;
   } else {
 
-    if (partie === 'cras') { // ok
+    if (partie === 'cras') {
       const statAvant = await statCras(db);
       let count = 0;
       logger.info(`${statAvant.CrasRestantAvecPerm} CRAS avec une permanence & ${statAvant.CrasRestantSansPerm} CRAS sans permanence (AVANT la correction) `);
       for (let obj of codePostauxFichier) {
         const correctionCras = await db.collection('cras').updateMany(
-          { 'cra.codePostal': obj.Code_postal, 'cra.nomCommune': obj.Nom_commune, 'cra.codeCommuneInsee': { '$exists': false } },
-          { '$set': { 'cra.codeCommuneInsee': obj.Code_Commune } });
+          { 'cra.codePostal': obj.Code_postal, 'cra.nomCommune': obj.Nom_commune, 'cra.codeCommune': { '$exists': false } },
+          { '$set': { 'cra.codeCommune': obj.Code_Commune } });
         count += correctionCras?.modifiedCount;
       }
       logger.info(`${count} Cras corrigés aux total`);
@@ -86,7 +110,7 @@ execute(__filename, async ({ logger, db, exit }) => {
     if (partie === 'permanences') {
       logger.info(`Partie Permanences , par defaut la verification ${acte ? 'AVEC' : 'SANS'} correction`);
       const permanences =
-      await db.collection('permanences').find({ 'adresse.codeCommuneInsee': { '$exists': false } }).limit(limit).project({ adresse: 1, location: 1 }).toArray();
+      await db.collection('permanences').find({ 'adresse.codeCommune': { '$exists': false } }).limit(limit).project({ adresse: 1, location: 1 }).toArray();
       const exportsCSv = {
         permMatchOK: [],
         permNotOK: [],
@@ -94,25 +118,34 @@ execute(__filename, async ({ logger, db, exit }) => {
         diffCityAndCodePostal: [],
       };
       for (const { adresse, location, _id } of permanences) {
-        const adresseCompile = `${adresse.numeroRue ?? ''} ${adresse.rue} ${adresse.codePostal} ${adresse.ville}`;
-        const urlAPI = `https://api-adresse.data.gouv.fr/search/?q=${encodeURI(adresseCompile)}`;
+        const adresseComplete = `${adresse.numeroRue ?? ''} ${adresse.rue} ${adresse.codePostal} ${adresse.ville}`;
+        const urlAPI = `https://api-adresse.data.gouv.fr/search/?q=${encodeURI(adresseComplete)}`;
         await axios.get(urlAPI, { params: {} }).then(async result => {
-          // verif de la location avec l'adresse de la perm (saisi ou non)
-          const matchLocation = result.data?.features.find(i => String(i?.geometry?.coordinates) === String(location?.coordinates));
+          const matchLocation = result?.data?.features.find(i => String(i?.geometry?.coordinates) === String(location?.coordinates));
+          const district = matchLocation?.properties?.district ? matchLocation?.properties?.district?.replace('e Arrondissement', '') : undefined;
+          const adresseControleDiff = {
+            diffNumber: matchLocation?.properties?.housenumber !== adresse.numeroRue && ![null, ''].includes(adresse.numeroRue),
+            // eslint-disable-next-line max-len
+            diffRue: formatText(matchLocation?.properties?.street ?? matchLocation?.properties?.locality)?.toUpperCase() !== formatText(adresse.rue)?.toUpperCase(),
+            diffCodePostal: matchLocation?.properties?.postcode !== adresse.codePostal,
+            diffville: formatText(district ?? matchLocation?.properties?.city.toUpperCase())?.toUpperCase() !== formatText(adresse.ville)
+          };
           if (!matchLocation) {
             const resultQueryLatLong = await axios.get(`${urlAPI}&lat=${location?.coordinates[0]}&lon=${location?.coordinates[1]}`, { params: {} });
-            exportsCSv.permNotOK.push({ // A rectif plus tard..
-              _id,
+            exportsCSv.permNotOK.push({ _id,
               adresse: { ...adresse, coordinates: location?.coordinates },
               resultApi: {
                 total: resultQueryLatLong.data?.features.length,
                 fields: resultQueryLatLong.data?.features.map(i => resultApi(i.properties))
               } });
             return;
-          } else if (formatText(matchLocation.properties.city.toUpperCase()) !== formatText(adresse.ville) ||
-          matchLocation.properties.postcode !== adresse.codePostal || !matchLocation?.properties?.street) {
-            exportsCSv.diffCityAndCodePostal.push({ _id, adresse,
-              matchLocation: matchLocation?.properties?.street ? resultApi(matchLocation.properties) : matchLocation.properties });
+          } else if (Object.values(adresseControleDiff).includes(true)) {
+            exportsCSv.diffCityAndCodePostal.push({
+              nombreDiff: Object.values(adresseControleDiff).filter(i => i === true).length,
+              _id,
+              adresse,
+              matchLocation: resultApi(matchLocation.properties)
+            });
           } else {
             exportsCSv.permMatchOK.push({ _id, adresse, matchOK: resultApi(matchLocation.properties) });
             if (acte === 'correction') {
@@ -120,16 +153,9 @@ execute(__filename, async ({ logger, db, exit }) => {
             }
           }
         }).catch(error =>
-          exportsCSv.permError.push({ message: error.message, detail: adresseCompile }));
+          exportsCSv.permError.push({ _id, message: error.message, detail: adresseComplete }));
       }
-      Object.keys(exportsCSv).forEach(function(key) {
-        let jsonFile = path.join(__dirname, '../../../data/exports-historique', `${lot}Lot-${key}.json`);
-        let file = fs.createWriteStream(jsonFile, { flags: 'w' });
-        file.write(JSON.stringify([
-          { total: exportsCSv[key].length, [key]: exportsCSv[key] },
-        ]));
-        logger.info(`${[key]}: ${exportsCSv[key].length}`);
-      });
+      await exportCsvPermanences(exportsCSv, lot, logger);
     }
     logger.info(`Fin du lot ${lot} pour la partie ${partie}`);
     exit();
