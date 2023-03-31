66b1b0e3f9638ff5e5dbd8f8f71a404798e2bf77
morgan <morgan@fitlab.fr>
2023-03-31 16:21:41 +0200

    script de refonte des organismes et controle du formulaire

--- src/services/cras/cra/utils/validationCra.js
+++ src/services/cras/cra/utils/validationCra.js
@@ -34,7 +34,7 @@ const validationCra = ObjectCra => {
       redirection: Joi.number().integer().min(0).max(100).error(new Error('Le nombre d\'accompagnements redirigés vers un autre établissement est invalide')),
     }),
     dateAccompagnement: Joi.date().min(new Date('2020-01-01T00:00:00.000Z')).required().error(new Error('La date est invalide')),
-    organisme: Joi.string().required().allow(null).error(new Error('L\'organisme de l\'accompagnement est invalide')),
+    organismes: Joi.array().required().allow(null).error(new Error('Les organismes de l\'accompagnement sont invalides')),
   }).validate(ObjectCra);
 
   return schema.error;
