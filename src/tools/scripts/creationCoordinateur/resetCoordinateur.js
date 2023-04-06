ccfa56a4eddbcec566eb191d58cdb7df443ab29f
Ornella Ourfi <ornella93@outlook.fr>
2023-04-06 18:53:47 +0200

    script purge coordo

---
+++ src/tools/scripts/creationCoordinateur/resetCoordinateur.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+'use strict';
+
+require('dotenv').config();
+
+const { execute } = require('../utils');
+
+execute(__filename, async ({ db, logger, exit }) => {
+  logger.info('Reset/Purge de tout les coordos....');
+  // estCoordinateur
+  await db.collection('conseillers').updateMany(
+    { 'estCoordinateur': true },
+    { $set: { 'estCoordinateur': false } },
+  );
+  await db.collection('misesEnRelation').updateMany(
+    { 'conseillerObj.estCoordinateur': true },
+    { $set: { 'conseillerObj.estCoordinateur': false } },
+  );
+  // listeSubordonnes
+  await db.collection('conseillers').updateMany(
+    { 'listeSubordonnes': { '$exists': true } },
+    { $unset: { 'listeSubordonnes': '' } }
+  );
+  await db.collection('misesEnRelation').updateMany(
+    { 'conseillerObj.listeSubordonnes': { '$exists': true } },
+    { $unset: { 'conseillerObj.listeSubordonnes': '' } }
+  );
+  // Role
+  await db.collection('users').updateMany(
+    { 'roles': { '$in': ['coordinateur_coop'] } },
+    { $pull: { 'roles': 'coordinateur_coop' } }
+  );
+  logger.info('FiN de la purge.');
+  exit();
+});
