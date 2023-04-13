0eaa7a23204a85ea7e4512af8fe18a71b09c7c4b
morgan <morgan@fitlab.fr>
2023-04-13 13:00:33 +0200

    changement de requete

--- src/services/stats/cras/tempsAccompagnements.js
+++ src/services/stats/cras/tempsAccompagnements.js
@@ -1,56 +1,36 @@
 const getStatsTempsAccompagnements = async (db, query) => {
-  let promises = [];
   let tempsAccompagnement = [
     { nom: 'individuel', valeur: 0 },
     { nom: 'collectif', valeur: 0 },
     { nom: 'ponctuel', valeur: 0 }
   ];
 
-  //Gestion des nombres >= 60
-  const tempsAccompagnementTranche = await db.collection('cras').aggregate(
+  const tempsAccompagnementArray = await db.collection('cras').aggregate(
     [
       { $unwind: '$cra.duree' },
-      { $match: { ...query, 'cra.duree': { $ne: ['0-30', '30-60', '60', '90'] } } },
-      { $group: { _id: '$cra.activite', count: { $sum: '$cra.duree' } } },
+      { $match: { ...query } },
+      { $group: { _id: '$cra.activite', count: { $sum: {
+        $switch: {
+          branches:
+          [
+            { case: { $eq: ['$cra.duree', '0-30'] }, then: 30 },
+            { case: { $eq: ['$cra.duree', '30-60'] }, then: 60 },
+            { case: { $eq: ['$cra.duree', '60'] }, then: 60 },
+            { case: { $eq: ['$cra.duree', '90'] }, then: 90 },
+          ],
+          default: '$cra.duree'
+        } } } } },
       { $project: { '_id': 0, 'nom': '$_id', 'valeur': '$count' } }
     ]
   ).toArray();
 
-  //Gestion des categories '0-30' / '30-60' / '60' / '90'
-  await tempsAccompagnementTranche.forEach(async activite => {
-    promises.push(new Promise(async resolve => {
-      tempsAccompagnement.find(accompagnement => accompagnement.nom === activite.nom).valeur += activite.valeur;
-      let dureesString = await db.collection('cras').aggregate(
-        [
-          { $unwind: '$cra.duree' },
-          { $match: { ...query, 'cra.activite': activite.nom, 'cra.duree': { $in: ['0-30', '30-60', '60', '90'] } } },
-          { $group: { _id: '$cra.duree', count: { $sum: 1 } } },
-          { $project: { '_id': 0, 'nom': '$_id', 'valeur': '$count' } }
-        ]
-      ).toArray();
-
-      // Ajout des heures par activité
-      if (dureesString?.length > 0) {
-        await dureesString.forEach(async duree => {
-          let valeurString = 0;
-          if (duree.nom === '0-30') {
-            valeurString = 30 * duree?.valeur;
-          }
-          if (duree.nom === '30-60' || duree.nom === '60') {
-            valeurString = 60 * duree?.valeur;
-          }
-          if (duree.nom === '90') {
-            valeurString = 90 * duree?.valeur;
-          }
-          tempsAccompagnement.find(accompagnement => accompagnement.nom === activite.nom).valeur += valeurString;
-        });
-      }
-      resolve();
-    }));
+  tempsAccompagnement.forEach((temps, i) => {
+    if (tempsAccompagnementArray.find(tpsAcc => tpsAcc.nom === temps.nom)) {
+      tempsAccompagnement[i].valeur = tempsAccompagnementArray.find(tpsAcc => tpsAcc.nom === temps.nom).valeur;
+    }
   });
-  await Promise.all(promises);
 
-  //Temps en minutes
+  //Temps en heure
   let total = 0;
   await tempsAccompagnement.forEach(temps => {
     temps.valeur = temps.valeur === 0 ? 0 : Math.round(temps.valeur / 60);
