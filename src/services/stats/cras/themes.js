aa8d46a025b82727662c216cd8c658f2c5ff1106
morgan <morgan@fitlab.fr>
2023-04-21 10:10:23 +0200

    quick fix sentry string au lieu de number

--- src/services/stats/cras/themes.js
+++ src/services/stats/cras/themes.js
@@ -1,25 +1,25 @@
 const getStatsThemes = async (db, query) => {
 
   let statsThemes = [
-    { nom: 'equipement informatique', valeur: 0 },
-    { nom: 'internet', valeur: 0 },
-    { nom: 'courriel', valeur: 0 },
-    { nom: 'smartphone', valeur: 0 },
-    { nom: 'contenus numeriques', valeur: 0 },
-    { nom: 'vocabulaire', valeur: 0 },
-    { nom: 'traitement texte', valeur: 0 },
-    { nom: 'echanger', valeur: 0 },
-    { nom: 'trouver emploi', valeur: 0 },
-    { nom: 'accompagner enfant', valeur: 0 },
-    { nom: 'tpe/pme', valeur: 0 },
-    { nom: 'demarche en ligne', valeur: 0 },
-    { nom: 'securite', valeur: 0 },
-    { nom: 'fraude et harcelement', valeur: 0 },
-    { nom: 'sante', valeur: 0 },
-    { nom: 'espace-sante', valeur: 0 },
-    { nom: 'budget', valeur: 0 },
-    { nom: 'scolaire', valeur: 0 },
-    { nom: 'diagnostic', valeur: 0 },
+    { nom: 'equipement informatique', valeur: 0, pourcent: 0 },
+    { nom: 'internet', valeur: 0, pourcent: 0 },
+    { nom: 'courriel', valeur: 0, pourcent: 0 },
+    { nom: 'smartphone', valeur: 0, pourcent: 0 },
+    { nom: 'contenus numeriques', valeur: 0, pourcent: 0 },
+    { nom: 'vocabulaire', valeur: 0, pourcent: 0 },
+    { nom: 'traitement texte', valeur: 0, pourcent: 0 },
+    { nom: 'echanger', valeur: 0, pourcent: 0 },
+    { nom: 'trouver emploi', valeur: 0, pourcent: 0 },
+    { nom: 'accompagner enfant', valeur: 0, pourcent: 0 },
+    { nom: 'tpe/pme', valeur: 0, pourcent: 0 },
+    { nom: 'demarche en ligne', valeur: 0, pourcent: 0 },
+    { nom: 'securite', valeur: 0, pourcent: 0 },
+    { nom: 'fraude et harcelement', valeur: 0, pourcent: 0 },
+    { nom: 'sante', valeur: 0, pourcent: 0 },
+    { nom: 'espace-sante', valeur: 0, pourcent: 0 },
+    { nom: 'budget', valeur: 0, pourcent: 0 },
+    { nom: 'scolaire', valeur: 0, pourcent: 0 },
+    { nom: 'diagnostic', valeur: 0, pourcent: 0 },
   ];
 
   let themes = await db.collection('cras').aggregate(
@@ -55,7 +55,7 @@ const getStatsThemes = async (db, query) => {
   );
   if (total > 0) {
     statsThemes.forEach(theme => {
-      theme.pourcent = (theme.valeur > 0 ? theme.valeur * 100 / total : 0).toFixed(1);
+      theme.pourcent = Number((theme.valeur > 0 ? theme.valeur * 100 / total : 0).toFixed(1));
     });
   }
 
