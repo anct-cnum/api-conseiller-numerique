6d3934f01b3194ab098a176bf7845f9339f7089c
Ornella Ourfi <ornella93@outlook.fr>
2023-06-02 15:10:43 +0200

    resolve impact coordo

--- src/services/data-exports/export-cnfs-coordinateur/core/export-cnfs-coordinateur.core.js
+++ src/services/data-exports/export-cnfs-coordinateur/core/export-cnfs-coordinateur.core.js
@@ -7,8 +7,8 @@ const prettifyAndComplete = () => async statCnfs => {
   const { mattermost, certifie, ...nextStatCnfs } = statCnfs;
   return {
     ...nextStatCnfs,
-    datePrisePoste: formatDate(nextStatCnfs.datePrisePoste),
-    dateFinFormation: formatDate(nextStatCnfs.dateFinFormation),
+    datePrisePoste: nextStatCnfs.datePrisePoste ? formatDate(nextStatCnfs.datePrisePoste) : 'Non renseignée',
+    dateFinFormation: nextStatCnfs.dateFinFormation ? formatDate(nextStatCnfs.dateFinFormation) : 'Non renseignée',
     isUserActif: userActifStatus(mattermost),
     certifie: certifie === true ? 'Oui' : 'Non',
   };
