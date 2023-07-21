7952ca6bbaea47abbb1dbd9090eb68059d5051cc
morgan <morgan@fitlab.fr>
2023-07-21 11:24:48 +0200

    suppression de la requete postgres

--- src/tools/scripts/rgpd-suppression-compte.js
+++ src/tools/scripts/rgpd-suppression-compte.js
@@ -33,7 +33,6 @@ const deleteCandidatInactif = db => async candidatInactif => {
 };
 
 const deleteMeRCandidatInactif = db => async candidatInactif => {
-  await pool.query(`DELETE FROM djapp_matching WHERE coach_id = $1`, [candidatInactif.idPG]);
   await db.collection('misesEnRelation').deleteMany({
     'conseiller.$id': candidatInactif._id
   });
