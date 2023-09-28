6e12c9e739eddf82717a629ddb4bc9e02f46255d
Ornella Ourfi <ornella93@outlook.fr>
2023-09-28 15:36:30 +0200

    blocage de la mise à jour stat uniquement pour les cras creer le jour J (le cron passera)

--- src/services/cras/cra/repositories/cra.repository.js
+++ src/services/cras/cra/repositories/cra.repository.js
@@ -25,20 +25,25 @@ const deleteLigneCra = db => async (year, month, id, options) => {
 
 //On met à jour la nouvelle stat correspondante au mois et à l'annee
 const updateLigneCra = db => async (year, month, total, id, options) => {
-  const update = { $push: { [String(year)]: { 'mois': month, 'totalCras': total } } };
+  const listMois = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+  const update = { $push: { [String(year)]: { 'mois': month, 'totalCras': total, 'indication': listMois[month] } } };
   await db.collection('stats_conseillers_cras').updateOne({ '_id': id }, update, options);
 };
 
-const updateStatistiquesCra = db => async (cra, oldDateAccompagnement, conseillerId) => {
+const updateStatistiquesCra = db => async (cra, oldDateAccompagnement, conseillerId, createdAt) => {
+  const dateCreateCra = createdAt;
+  dateCreateCra.setUTCHours(0, 0, 0, 0);
+  let dateNow = new Date();
+  dateNow.setUTCHours(0, 0, 0, 0);
   const newYear = cra.cra.dateAccompagnement.getUTCFullYear();
   const newMonth = cra.cra.dateAccompagnement.getMonth();
   const oldYear = oldDateAccompagnement.getUTCFullYear();
   const oldMonth = oldDateAccompagnement.getMonth();
+  const testCraCreate = dateCreateCra.toString() !== dateNow.toString();
   const testYear = newYear === oldYear;
   const testMonth = newMonth === oldMonth;
   const stats = await getStatsConseillerCras(db)(new ObjectId(conseillerId));
-
-  if (stats && (testYear && !testMonth || !testYear)) {
+  if (stats && (testYear && !testMonth || !testYear) && testCraCreate) {
     const options = { upsert: true };
 
     let oldTotal = stats[String(oldYear)]?.find(stat => stat.mois === oldMonth)?.totalCras;
