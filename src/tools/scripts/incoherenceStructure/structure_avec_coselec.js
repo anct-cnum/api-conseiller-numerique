074ce53e6b37ff7c4013965ff59f94b0b94cb545
morgan <morgan@fitlab.fr>
2023-10-18 10:53:03 +0200

    retour PR

--- src/tools/scripts/incoherenceStructure/structure_avec_coselec.js
+++ src/tools/scripts/incoherenceStructure/structure_avec_coselec.js
@@ -4,6 +4,7 @@
 const { execute } = require('../../utils');
 const path = require('path');
 const fs = require('fs');
+const { getLastCoselec } = require('../../../utils');
 
 const getStructure = async db => await db.collection('structures').find({
   'statut': 'VALIDATION_COSELEC'
@@ -11,11 +12,11 @@ const getStructure = async db => await db.collection('structures').find({
 
 const countMisesEnRelation = db => async idStructure => await db.collection('misesEnRelation').countDocuments({
   'structure.$id': idStructure,
-  'statut': { '$in': ['finalisee', 'recrutee', 'renouvellement_initiee'] }
+  'statut': { '$in': ['finalisee', 'recrutee'] }
 });
 
 execute(__filename, async ({ logger, db, exit }) => {
-  logger.info('Début de la recherche d\'incohérence sur le nombre de conseillerslorsqu\'une structure a un statut VALIDATION_COSELEC');
+  logger.info(`Début de la recherche d'incohérence sur le nombre de conseillers lorsqu'une structure a un statut VALIDATION_COSELEC`);
   const promises = [];
   const writeLine = [];
 
@@ -23,9 +24,10 @@ execute(__filename, async ({ logger, db, exit }) => {
   structures.forEach(structure => {
     promises.push(new Promise(async resolve => {
       await countMisesEnRelation(db)(structure._id).then(countNbConseiller => {
-        if (countNbConseiller !== structure.coselec[structure.coselec.length - 1].nombreConseillersCoselec) {
+        const coselec = getLastCoselec(structure);
+        if (countNbConseiller !== coselec.nombreConseillersCoselec) {
           writeLine.push('La structure avec l\'id ' + String(structure._id) +
-          ' a un nombreConseillersCoselec de ' + structure.coselec[structure.coselec.length - 1].nombreConseillersCoselec +
+          ' a un nombreConseillersCoselec de ' + coselec.nombreConseillersCoselec +
           ' mais comporte ' + countNbConseiller + ' mise(s) en relation\n'
           );
         }
@@ -46,61 +48,6 @@ execute(__filename, async ({ logger, db, exit }) => {
   file.close();
   logger.info('Fin de la génération du fichier texte.');
 
-  logger.info('Fin de la recherche d\'incohérence sur le nombre de conseillerslorsqu\'une structure a un statut VALIDATION_COSELEC');
+  logger.info(`Fin de la recherche d'incohérence sur le nombre de conseillers lorsqu'une structure a un statut VALIDATION_COSELEC`);
   exit();
 });
-
-/*
-
-execute(__filename, async ({ logger, db, exit }) => {
-  logger.info('Début de la recherche d\'incohérence lorsqu\'une structure a un statut autre que VALIDATION_COSELEC');
-
-  const structures = await getStructures(db);
-
-  logger.info(structures.length + ' structures en cours d\'observation...');
-  structures.forEach(structure => {
-    let erreursDetectees = 0;
-    promises.push(new Promise(async resolve => {
-      let line = 'La structure avec l\'id ' + String(structure._id);
-      const resultats = await countStatutsIncorrects(db)(structure._id);
-      if (resultats.length > 0) {
-        line += ' comporte : \n';
-        line += '- ';
-        for (const resultat of resultats) {
-          erreursDetectees++;
-          line += resultat.countStatut + ' statut de valeur ' + String(resultat._id);
-          if (resultats.length > 1) {
-            line += ' | ';
-          }
-        }
-        line += '\n';
-      } else {
-        line += ' comporte : \n';
-        const misesEnRelation = await getMisesEnRelation(db)(structure._id);
-        if (misesEnRelation.length > 0) {
-          for (const miseEnRelation of misesEnRelation) {
-            if (miseEnRelation.conseillerObj.userCreated === true) {
-              erreursDetectees++;
-              line += '- Une mise en relation avec userCreated à true pour le conseiller id ' +
-              String(miseEnRelation.conseillerObj._id) + '\n';
-            }
-          }
-        }
-      }
-
-      const countUsers = await countUserStructure(db)(structure._id);
-      if (countUsers > 0) {
-        erreursDetectees++;
-        line += ' comporte : \n';
-        line += countUsers + ' user(s) encore actif(s) en base';
-      }
-      if (erreursDetectees > 0) {
-        writeLine.push(line);
-        logger.info('Structure : ' + structure._id + ' => ' + erreursDetectees + ' erreur(s)');
-      }
-      resolve();
-    }));
-  });
-
-});
-*/
--- src/tools/scripts/incoherenceStructure/structure_sans_coselec.js
+++ src/tools/scripts/incoherenceStructure/structure_sans_coselec.js
@@ -4,19 +4,20 @@
 const { execute } = require('../../utils');
 const path = require('path');
 const fs = require('fs');
+const { getLastCoselec } = require('../../../utils');
 
 const getStructures = async db => await db.collection('structures').find({
   'statut': { '$ne': 'VALIDATION_COSELEC' }
 }).toArray();
 
 const countUserStructure = db => async idStructure => await db.collection('users').countDocuments({
-  'entity.$id': idStructure
+  'entity.$id': idStructure,
+  'entity.$ref': 'structures'
 });
 
-const countStatutsIncorrects = db => async idStructure => await db.collection('misesEnRelation').aggregate(
+const countStatuts = db => async idStructure => await db.collection('misesEnRelation').aggregate(
   { '$match': {
     'structure.$id': idStructure,
-    'statut': { '$ne': 'finalisee_rupture' }
   } },
   { '$group': {
     '_id': '$statut',
@@ -30,11 +31,11 @@ const countStatutsIncorrects = db => async idStructure => await db.collection('m
 
 const getMisesEnRelation = db => async idStructure => await db.collection('misesEnRelation').find({
   'structure.$id': idStructure,
-  'statut': 'finalisee_rupture'
+  'statut': { '$in': ['finalisee_rupture', 'terminee'] }
 }).toArray();
 
 execute(__filename, async ({ logger, db, exit }) => {
-  logger.info('Début de la recherche d\'incohérence lorsqu\'une structure a un statut autre que VALIDATION_COSELEC');
+  logger.info(`Début de la recherche d'incohérence lorsqu'une structure a un statut autre que VALIDATION_COSELEC`);
   const promises = [];
   const writeLine = [];
   const structures = await getStructures(db);
@@ -43,10 +44,9 @@ execute(__filename, async ({ logger, db, exit }) => {
   structures.forEach(structure => {
     let erreursDetectees = 0;
     promises.push(new Promise(async resolve => {
-      let line = 'La structure avec l\'id ' + String(structure._id);
-      const resultats = await countStatutsIncorrects(db)(structure._id);
+      let line = 'La structure avec l\'id ' + String(structure._id) + ' comporte : \n';
+      const resultats = await countStatuts(db)(structure._id);
       if (resultats.length > 0) {
-        line += ' comporte : \n';
         line += '- ';
         for (const resultat of resultats) {
           erreursDetectees++;
@@ -56,25 +56,30 @@ execute(__filename, async ({ logger, db, exit }) => {
           }
         }
         line += '\n';
-      } else {
-        line += ' comporte : \n';
-        const misesEnRelation = await getMisesEnRelation(db)(structure._id);
-        if (misesEnRelation.length > 0) {
-          for (const miseEnRelation of misesEnRelation) {
-            if (miseEnRelation.conseillerObj.userCreated === true) {
-              erreursDetectees++;
-              line += '- Une mise en relation avec userCreated à true pour le conseiller id ' +
-              String(miseEnRelation.conseillerObj._id) + '\n';
-            }
+      }
+      const misesEnRelation = await getMisesEnRelation(db)(structure._id);
+      if (misesEnRelation.length > 0) {
+        for (const miseEnRelation of misesEnRelation) {
+          if (miseEnRelation.conseillerObj.userCreated === true) {
+            erreursDetectees++;
+            line += '- Une mise en relation avec userCreated à true pour le conseiller id ' +
+            String(miseEnRelation.conseillerObj._id) + '\n';
+          } else if (miseEnRelation.structureObj.userCreated === true) {
+            erreursDetectees++;
+            line += '- Une mise en relation avec userCreated à true pour la structure id ' +
+            String(miseEnRelation.structureObj._id) + '\n';
           }
         }
       }
-
+      const coselec = getLastCoselec(structure);
+      if (coselec && coselec?.nombreConseillersCoselec !== 0) {
+        erreursDetectees++;
+        line += '- Un nombre de conseillers coselec différent de 0 => nombreConseillersCoselec = ' + coselec?.nombreConseillersCoselec + '\n';
+      }
       const countUsers = await countUserStructure(db)(structure._id);
       if (countUsers > 0) {
         erreursDetectees++;
-        line += ' comporte : \n';
-        line += countUsers + ' user(s) encore actif(s) en base';
+        line += '- ' + countUsers + ' user(s) encore actif(s) en base \n';
       }
       if (erreursDetectees > 0) {
         writeLine.push(line);
@@ -96,6 +101,6 @@ execute(__filename, async ({ logger, db, exit }) => {
   file.close();
   logger.info('Fin de la génération du fichier texte.');
 
-  logger.info('Fin de la recherche d\'incohérence lorsqu\'une structure a un statut autre que VALIDATION_COSELEC');
+  logger.info(`Fin de la recherche d'incohérence lorsqu'une structure a un statut autre que VALIDATION_COSELEC`);
   exit();
 });
