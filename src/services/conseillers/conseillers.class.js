8db2dcbab1a571052274426d7fdb98c1bed34dfe
morgan <morgan@fitlab.fr>
2023-10-19 16:49:39 +0200

    erreur sur le req.body

--- src/services/conseillers/conseillers.class.js
+++ src/services/conseillers/conseillers.class.js
@@ -967,7 +967,7 @@ exports.Conseillers = class Conseillers extends Service {
       const userId = decode(accessToken).sub;
       const user = await db.collection('users').findOne({ _id: new ObjectId(userId) });
       const idConseiller = req.params.id;
-      const { dateDisponibilite } = req.body.dateDisponibilite;
+      const { dateDisponibilite } = req.body;
       const mongoDateDisponibilite = new Date(dateDisponibilite);
       const updatedAt = new Date();
 
