7900d3a9606dd5b0a758737478db95bae24497fb
morgan <morgan@fitlab.fr>
2023-11-02 15:18:07 +0100

    optimisation des requêtes

--- src/tools/scripts/nettoyageUserCreationError.js
+++ src/tools/scripts/nettoyageUserCreationError.js
@@ -4,11 +4,7 @@
 const { program } = require('commander');
 const { execute } = require('../utils');
 
-const getStructures = async db => await db.collection('structures').find({
-  userCreated: true, userCreationError: true
-});
-
-const getConseillers = async db => await db.collection('conseillers').find({
+const getElements = async (db, collection) => await db.collection(collection).find({
   userCreated: true, userCreationError: true
 });
 
@@ -17,73 +13,70 @@ const getUserByEntity = db => async (id, role) => await db.collection('users').f
   'roles': { '$in': [role] }
 });
 
-const updateStructure = db => async structure => await db.collection('structures').replaceOne(
-  { '_id': structure._id },
-  structure
+const updateUserCreated = (db, collection) => async id => await db.collection(collection).updateOne(
+  { '_id': id },
+  { '$set': { 'userCreated': false } }
 );
 
-const updateConseiller = db => async conseiller => await db.collection('conseillers').replaceOne(
-  { '_id': conseiller._id },
-  conseiller
+const deleteUserCreationError = (db, collection) => async id => await db.collection(collection).updateOne(
+  { '_id': id },
+  { '$unset': { 'userCreationError': '' } }
 );
 
-program.option('-f, --fix', 'fixer en base les statuts userCreated et userCreationError')
-.option('-c, --collection', 'choisir d\'appliquer les corrections sur les conseillers ou sur les structures');
+program.option('-f, --fix', 'fixer en base les statuts userCreated et userCreationError');
+program.option('-c, --collection <collection>', 'choisir d\'appliquer les corrections sur les conseillers ou sur les structures');
+program.parse(process.argv);
 
 execute(__filename, async ({ exit, logger, db }) => {
   logger.info('Début du script de nettoyage des incohérences sur userCreationError');
   const { fix, collection } = program;
 
+  const elements = await getElements(db, collection);
+
   if (collection === 'structures') {
     logger.info('Début du traitement des structures');
     const promisesStructures = [];
-    const structures = await getStructures(db);
-
-    structures.forEach(structure => {
+    elements.forEach(structure => {
       promisesStructures.push(new Promise(async resolve => {
         const user = await getUserByEntity(db)(structure._id, 'structure');
         if (user) {
           logger.info('La structure _id ' + structure._id + ' a un user, suppression du status userCreationError');
-          delete structure.userCreationError;
+          if (fix) {
+            await deleteUserCreationError(db, collection)(structure._id);
+          }
         } else {
           logger.info('La structure _id ' + structure._id + ' n\'a pas de user, correction du status userCreated');
-          structure.userCreated = false;
-        }
-        if (fix) {
-          await updateStructure(db)(structure);
+          if (fix) {
+            await updateUserCreated(db, collection)(structure._id);
+          }
         }
         resolve();
       }));
     });
-
     await Promise.all(promisesStructures);
     logger.info('Fin du traitement des structures');
-
   } else if (collection === 'conseillers') {
     logger.info('Début du traitement des conseillers');
     const promisesConseillers = [];
-    const conseillers = await getConseillers(db);
-
-    conseillers.forEach(conseiller => {
+    elements.forEach(conseiller => {
       promisesConseillers.push(new Promise(async resolve => {
         const user = await getUserByEntity(db)(conseiller._id, 'conseiller');
         if (user) {
           logger.info('Le conseiller _id ' + conseiller._id + ' a un user, suppression du status userCreationError');
-          delete conseiller.userCreationError;
+          if (fix) {
+            await deleteUserCreationError(db, collection)(conseiller._id);
+          }
         } else {
           logger.info('Le conseiller _id ' + conseiller._id + ' n\'a pas de user, correction du status userCreated');
-          conseiller.userCreated = false;
-        }
-        if (fix) {
-          await updateConseiller(db)(conseiller);
+          if (fix) {
+            await updateUserCreated(db, collection)(conseiller._id);
+          }
         }
         resolve();
       }));
     });
-
     await Promise.all(promisesConseillers);
     logger.info('Fin du traitement des conseillers');
-
   }
 
   logger.info('Fin du script de nettoyage des incohérences sur userCreationError');
