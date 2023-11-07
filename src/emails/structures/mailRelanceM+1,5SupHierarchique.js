266a53492fb98fb5e8f91f9b0985aff340e71a8e
Ornella Ourfi <ornella93@outlook.fr>
2023-11-07 16:05:54 +0100

    re-retour syntaxe

--- src/emails/structures/mailRelanceM+1,5SupHierarchique.js
+++ src/emails/structures/mailRelanceM+1,5SupHierarchique.js
@@ -47,7 +47,7 @@ module.exports = (db, mailer) => {
       return mailer.createMailer().sendEmail(
         conseiller.supHierarchique.email,
         {
-          subject: '[IMPORTANT] Aucun Compte-Rendu d\'Activité complété depuis 45 jours',
+          subject: '[IMPORTANT] Aucun compte-rendu d\'activité complété depuis 45 jours',
           body: await render(conseiller),
         },
       )
