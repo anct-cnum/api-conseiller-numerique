dd62f6b7c90f8aa4f9e58182f5b4ed88d420c25e
Quentin Fontenay <36076581+QuentinFontenay@users.noreply.github.com>
2023-12-05 17:44:47 +0000

    fix: gestion des erreurs mailbox / mattermost

--- src/tools/scripts/reset-password-cnil.js
+++ src/tools/scripts/reset-password-cnil.js
@@ -4,6 +4,7 @@
 const { updateMailboxPassword } = require('../../utils/mailbox');
 const { updateAccountPassword } = require('../../utils/mattermost');
 const { execute, delay } = require('../utils');
+const dayjs = require('dayjs');
 const { v4: uuidv4 } = require('uuid');
 const { program } = require('commander');
 const winston = require('winston');
@@ -16,7 +17,12 @@ execute(__filename, async ({ exit, gandi, mattermost, logger, db, app, Sentry })
   const { limit = 1000, log } = program;
   if (log) {
     logger.info('Création d\'un fichier de log pour les erreurs de réinitialisation de mot de passe...');
-    logger.add(new winston.transports.File({ filename: 'reset-password-cnil.log' }));
+    logger.add(new winston.transports.File(
+      {
+        filename: `reset-password-cnil-${dayjs().format('YYYY-MM-DD_HH-mm-ss')}.log`,
+        level: 'error',
+      }
+    ));
   }
   const users = await db.collection('users').find(
     {
@@ -25,35 +31,28 @@ execute(__filename, async ({ exit, gandi, mattermost, logger, db, app, Sentry })
     }
   ).limit(limit).toArray();
   let promises = [];
-
   logger.info('Réinitialisation des mots de passe de tous les conseillers et candidats...');
   users.forEach(user => {
     promises.push(new Promise(async (resolve, reject) => {
       const password = uuidv4() + 'AZEdsf;+:';
-      const userUpdatedPassword = await app.service('users').patch(user._id,
-        {
-          password,
-          resetPasswordCnil: true,
-        }).catch(err => new Error(err));
-      if (userUpdatedPassword instanceof Error) {
+      try {
+        await app.service('users').patch(user._id,
+          {
+            password,
+            resetPasswordCnil: true,
+          });
+        if (user.roles.includes('conseiller')) {
+          const conseiller = await db.collection('conseillers').findOne({ _id: user.entity.oid });
+          const login = conseiller?.emailCN?.address.substring(0, conseiller?.emailCN?.address.lastIndexOf('@'));
+          await updateMailboxPassword(gandi, conseiller._id, login, password, db, logger, Sentry);
+          await updateAccountPassword(mattermost, db, logger, Sentry)(conseiller, password);
+          await delay(1000);
+        }
+        resolve();
+      } catch {
         logger.error(`Erreur lors de la réinitialisation du mot de passe de ${user._id}`);
         reject();
-        return;
       }
-      if (user.roles.includes('conseiller')) {
-        const conseiller = await db.collection('conseillers').findOne({ _id: user.entity.oid });
-        const login = conseiller?.emailCN?.address.substring(0, conseiller?.emailCN?.address.lastIndexOf('@'));
-        const mailboxUpdated = await updateMailboxPassword(gandi, conseiller._id, login, password, db, logger, Sentry);
-        if (!mailboxUpdated) {
-          logger.error(`Erreur lors de la réinitialisation du mot de passe du compte gandi ${login}`);
-        }
-        const mattermostUpdated = await updateAccountPassword(mattermost, db, logger, Sentry)(conseiller, password);
-        if (!mattermostUpdated) {
-          logger.error(`Erreur lors de la réinitialisation du mot de passe du compte mattermost ${conseiller.mattermost.id}`);
-        }
-        await delay(1000);
-      }
-      resolve();
     }));
   });
   await Promise.all(promises);
