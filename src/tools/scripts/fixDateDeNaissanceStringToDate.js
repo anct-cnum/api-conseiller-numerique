c9aabcad8a0d39e62a23b0624b583e155007415c
morgan <morgan@fitlab.fr>
2024-02-02 14:24:16 +0100

    faire un unset en cas de date invalide

--- src/tools/scripts/fixDateDeNaissanceStringToDate.js
+++ src/tools/scripts/fixDateDeNaissanceStringToDate.js
@@ -14,18 +14,17 @@ execute(__filename, async ({ logger, db, exit }) => {
     logger.info(`${conseillers.length} conseillers à corriger`);
     conseillers.forEach(conseiller => {
       promises.push(new Promise(async resolve => {
+        let query = { $unset: { dateDeNaissance: '' } };
+        if (dayjs(conseiller.dateDeNaissance).isValid()) {
+          query = { $set: { dateDeNaissance: new Date(conseiller.dateDeNaissance) } };
+        }
         await db.collection('conseillers').updateOne(
           { _id: conseiller._id },
-          { $set: {
-            dateDeNaissance:
-              dayjs(conseiller.dateDeNaissance).isValid() ? new Date(conseiller.dateDeNaissance) : ''
-          } }
+          query
         );
         countCorrection += 1;
         resolve();
       }));
-
-      console.log(conseiller.dateDeNaissance);
     });
   } catch (e) {
     logger.error(e);
