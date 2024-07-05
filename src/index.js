d4afb5ff005820e7983f3d6590d166572bf5b9fe
Fabien Mercier <fabien.mercier@octo.com>
2024-07-05 13:47:00 +0200

    Amélioration de la dev expérience en affichant correctement les exceptions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-console */
 const logger = require('./logger');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
+process.on('unhandledRejection', error =>
+  logger.error('%s', error)
 );
 
 server.on('listening', () =>
