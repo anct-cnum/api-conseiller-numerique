bc204234aa90d537ee192c14358d4b9f1ca67b23
ornella <ornella93@outlook.fr>
2024-11-28 17:50:19 +0100

    SA avant 2023 a mettre en ANNULEE

---
+++ src/tools/scripts/candidature-structure-update-annulee.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+/** @format */
+
+'use strict';
+
+const { execute } = require('../utils');
+
+// node src/tools/scripts/candidature-structure-update-annulee.js
+
+execute(__filename, async ({ logger, db }) => {
+  const date = new Date('2023-12-31');
+  await db.collection('structures').updateMany(
+    {
+      'statut': {
+        $in: ['CREEE', 'EXAMEN_COMPLEMENTAIRE_COSELEC'],
+      },
+      'coordinateurCandidature': false,
+      'createdAt': { $lte: date },
+      'coselec.nombreConseillersCoselec': { $nin: [1] },
+    },
+    {
+      $set: { statut: 'ANNULEE' },
+    }
+  );
+  logger.info(`Annulation des candidatures structures avant le ${date}`);
+});
