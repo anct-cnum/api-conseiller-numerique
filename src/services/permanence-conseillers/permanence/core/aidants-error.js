6beded8e3bfa2cdbe9ff6f0b3d19fb0c8b269b7c
ornella <ornella93@outlook.fr>
2025-05-06 09:54:51 +0200

    uninstall de @gouvfr-anct/lieux-de-mediation-numerique + suppression code inutile

--- src/services/permanence-conseillers/permanence/core/aidants-error.js
+++
@@ -1,9 +0,0 @@
-class AidantsError extends Error {
-  constructor() {
-    super('Il devrait y avoir au moins un aidant visible dans une permanence');
-  }
-}
-
-module.exports = {
-  AidantsError
-};
